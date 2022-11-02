adde306f095303f1c1738e89a0b9896ea4d86376
Omri Levy <omri.levy0197@gmail.com>
2022-11-02 17:04:24 +0200

    ci(playwright): added DEBUG=pw:webserver to try and debug the webserver issue

--- sdks/web-sdk/playwright.config.ts
+++ sdks/web-sdk/playwright.config.ts
@@ -103,6 +103,7 @@ const config: PlaywrightTestConfig = {
     command: `pnpm dev`,
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 };
 
