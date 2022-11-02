60d62366b23927c0c9d11b3e82a3103a7928b4fe
Omri Levy <omri.levy0197@gmail.com>
2022-11-02 15:48:43 +0200

    fix(playwright in ci): now opens a web server in ci only using the pnpm start command
    this ensures e2e tests are run against a build and not dev

--- sdks/web-sdk/playwright.config.ts
+++ sdks/web-sdk/playwright.config.ts
@@ -98,10 +98,11 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    reuseExistingServer: !process.env.CI,
+    command: 'pnpm start',
+    port: 3000,
+  },
 };
 
 export default config;
