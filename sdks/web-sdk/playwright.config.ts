e093c4b9436a2353e057c7a2783319d91d6c1bc6
Omri Levy <omri.levy0197@gmail.com>
2022-11-02 16:07:56 +0200

    ci(playwright): updated config to one that previously worked

--- sdks/web-sdk/playwright.config.ts
+++ sdks/web-sdk/playwright.config.ts
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -99,9 +99,10 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
+    command: `pnpm dev`,
+    url: `http://localhost:3000`,
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
-    command: 'pnpm start',
-    port: 3000,
   },
 };
 
