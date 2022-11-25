f0f52ca780cdf4225312e4a84998f4e70090d560
Rudin Alexandr <alexandr.rudin.my@gmail.com>
2022-11-25 14:45:06 +0100

    feat(ocr): start with ocr (#85)
    * feat(ocr): start with ocr
    
    bo: start with ocr
    
    * feat(ocr): add copying to the clipboard functionality
    
    * feat(cor): ocr
    
    Co-authored-by: Alon Peretz <alonp99@gmail.com>

--- apps/backoffice/src/pages/users/components/SubjectContent.tsx
+++ apps/backoffice/src/pages/users/components/SubjectContent.tsx
@@ -6,8 +6,6 @@ import { ActionIcon, Center, Flex, Kbd, Loader, Transition } from '@mantine/core
 import { DetailsGrid } from '../../../molecules/DetailsGrid/DetailsGrid';
 import { WarningAlert } from '../../../components/atoms/WarningAlert/WarningAlert';
 import { DataField } from '../../../components/molecules/DataField/DataField';
-import { ImageViewer } from '../../../components/organisms/ImageViewer/ImageViewer';
-import { MagnifyingGlassButton } from '../../../components/atoms/MagnifyingGlassButton/MagnifyingGlassButton';
 import routerProvider from '@pankod/refine-react-router-v6';
 import { EState } from '../../../mock-service-worker/users/enums';
 import dayjs from 'dayjs';
@@ -25,13 +23,14 @@ import {
 import { IconDotsVertical } from '@tabler/icons';
 import { formatDate, isValidDate } from 'utils';
 import { OcrToggle } from './OcrToggle';
+import { SubjectImageViewer } from './SubjectImageViewer';
 
-const worker = createWorker();
-const ocrInitPromise = async () => {
-  await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-};
+// const worker = createWorker();
+// const ocrInitPromise = async () => {
+//   await worker.load();
+//   await worker.loadLanguage('eng');
+//   await worker.initialize('eng');
+// };
 // For the fromNow method.
 dayjs.extend(relativeTime);
 // @ts-ignore
@@ -130,17 +129,17 @@ export const SubjectContent: FunctionComponent<ISubjectContentProps> = ({ nextId
     })();
   }, [selfieRef.current, docFaceRef.current, selectedUser?.data?.id]);
 
-  useEffect(() => {
-    void (async () => {
-      console.log('OCR calculation');
-      await ocrInitPromise();
+  // useEffect(() => {
+  //   void (async () => {
+  //     console.log('OCR calculation');
+  //     // await ocrInitPromise();
 
-      if (!images[0]?.url) return;
+  //     if (!images[0]?.url) return;
 
-      const { data } = await worker.recognize(images[0].url);
-      setOcrText(data.text);
-    })();
-  }, [images[0]?.url]);
+  //     // const { data } = await worker.recognize(images[0].url);
+  //     // setOcrText(data.text);
+  //   })();
+  // }, [images[0]?.url]);
 
   useEffect(() => {
     setContentMounted(!isFetching);
@@ -420,39 +419,7 @@ export const SubjectContent: FunctionComponent<ISubjectContentProps> = ({ nextId
                 }}
               >
                 <CanAccess resource={'image-viewer'} action={'show'}>
-                  <ImageViewer>
-                    <ImageViewer.ZoomModal />
-                    <ImageViewer.SelectedImage
-                      initialImage={isLoading || !images?.[0]?.url ? '' : images[0].url}
-                      ZoomButton={props => (
-                        <MagnifyingGlassButton
-                          {...props}
-                          sx={{
-                            position: 'absolute',
-                            bottom: '1rem',
-                            right: '1rem',
-                          }}
-                        />
-                      )}
-                    />
-                    <ImageViewer.ImageList>
-                      {!isLoading &&
-                        images?.map(({ url, docType }) => (
-                          <ImageViewer.ImageItem
-                            key={`${url}${docType}`}
-                            src={url}
-                            caption={docType}
-                            alt={docType}
-                            buttonProps={{
-                              sx: {
-                                textTransform: 'capitalize',
-                              },
-                            }}
-                          />
-                        ))}
-                    </ImageViewer.ImageList>
-                  </ImageViewer>
-                  <OcrToggle ocrText={ocrText} />
+                  <SubjectImageViewer isLoading={isLoading} ocrText={ocrText} images={images} />
                 </CanAccess>
               </Stack>
             </Group>
