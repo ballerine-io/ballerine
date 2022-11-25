89ca1be728c3a55d79627879534093338000f331
Omri Levy <omri.levy0197@gmail.com>
2022-11-25 15:47:53 +0200

    revert(backoffice): reverted backoffice to pre-nx configs

--- apps/backoffice/craco.config.cjs
+++ apps/backoffice/craco.config.cjs
@@ -1,7 +1,4 @@
 const { ProvidePlugin } = require('webpack');
-const path = require('path');
-const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = {
   webpack: {
@@ -20,28 +17,6 @@ module.exports = {
         url: require.resolve('url'),
       });
 
-      // Remove guard against importing modules outside of \`src\`.
-      // Needed for workspace projects.
-      config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
-
-      // Add support for importing workspace projects.
-      config.resolve.plugins.push(
-        new TsConfigPathsPlugin({
-          configFile: path.resolve(__dirname, 'tsconfig.json'),
-          extensions: ['.ts', '.tsx', '.js', '.jsx'],
-          mainFields: ['module', 'main'],
-        }),
-      );
-
-      // Replace include option for babel loader with exclude
-      // so babel will handle workspace projects as well.
-      config.module.rules[1].oneOf.forEach(r => {
-        if (r.loader && r.loader.indexOf('babel') !== -1) {
-          r.exclude = /node_modules/;
-          delete r.include;
-        }
-      });
-
       return config;
     },
     plugins: {
@@ -56,12 +31,6 @@ module.exports = {
   eslint: {
     pluginOptions: {
       failOnError: false,
-    },
-  },
-  jest: {
-    configure: config => {
-      config.resolver = '@nrwl/jest/plugins/resolver';
-      return config;
     },
   },
 };
