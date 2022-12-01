9903c05b957041036939ed02562c0915bc0c8d87
Alon Peretz <alonp99@gmail.com>
2022-12-01 13:23:29 +0200

    feat: 1
    1

--- sdks/web-sdk/src/types/BallerineSDK.ts
+++ sdks/web-sdk/src/types/BallerineSDK.ts
@@ -47,7 +47,7 @@ interface FlowsUIConfig {
     loader?: ICSSProperties;
     errorText?: ICSSProperties;
     overlay?: ICSSProperties;
-    documentOptions: IDocumentOptions;
+    documentOptions?: IDocumentOptions;
   };
   flows?: {
     [key: string]: {
