e5c7485475020233a3111ecc60b8ef94c06f99b2
sFiFty <alexandr.rudin.my@gmail.com>
2022-12-01 15:53:43 +0100

    fix(lint): lint issue

--- sdks/web-sdk/src/lib/services/theme-manager/utils.ts
+++ sdks/web-sdk/src/lib/services/theme-manager/utils.ts
@@ -4,7 +4,7 @@ export const injectPrimaryIntoLayoutGradient = (
   layoutStyles: ICSSProperties,
   primaryColor: string,
 ): ICSSProperties => {
-  if (layoutStyles.background?.includes('gradient')) {
+  if (typeof layoutStyles.background === 'string' && layoutStyles.background.includes('gradient')) {
     return {
       ...layoutStyles,
       background: `linear-gradient(180deg, #fff 0%, #fff 75%, ${primaryColor} 250%)`,
