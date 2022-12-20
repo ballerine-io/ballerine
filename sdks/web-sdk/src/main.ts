ed8a7a056cd51152eb15446b8e68c7504fc8b849
Omri Levy <omri.levy0197@gmail.com>
2022-12-20 09:28:26 +0200

    fix(init): no longer growing the steps array each call to the init method
    this handles places like React where each rerender may call init again

--- sdks/web-sdk/src/main.ts
+++ sdks/web-sdk/src/main.ts
@@ -6,6 +6,8 @@ import {
   updateTranslations,
 } from './lib/utils/configuration-manager';
 import { getConfigFromQueryParams } from './lib/utils/get-config-from-query-params';
+import { configuration } from './lib/contexts/configuration';
+import { configuration as defaultConfiguration } from './lib/configuration/configuration';
 //
 export const flows: BallerineSDKFlows = {
   // Use the b_fid query param as the default flowName, fallback to the passed flowName arg.
@@ -67,6 +69,11 @@ export const flows: BallerineSDKFlows = {
         __APP_VERSION__,
         config,
       );
+
+      // Always init with no configuration. This handles multiple calls to init, i.e React re-renders.
+      // Otherwise, the steps array could keep growing.
+      configuration.set(defaultConfiguration);
+
       const { translations, ...restConfig } = config;
       // Extract config from query params
       const {
