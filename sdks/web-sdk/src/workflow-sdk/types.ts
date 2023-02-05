80885e9aa7f39c0a94ed8fc1ef280dbd2ed532ab
Omri Levy <omri.levy0197@gmail.com>
2023-02-05 13:42:35 +0200

    refactor(types): now the event type is only available on the wildcard event listener

--- sdks/web-sdk/src/workflow-sdk/types.ts
+++ sdks/web-sdk/src/workflow-sdk/types.ts
@@ -1,4 +1,4 @@
-import { RenameProperty } from '../types';
+import { AnyRecord, RenameProperty } from '../types';
 import { IWorkflowEventCallbackParams } from './interfaces';
 
 export type WorkflowEvent =
@@ -8,13 +8,33 @@ export type WorkflowEvent =
   | 'local-step'
   | '*'
   | string;
-export type WorkflowEventCallback = (
-  event: Omit<IWorkflowEventCallbackParams, 'type'> &
-    Partial<RenameProperty<Pick<IWorkflowEventCallbackParams, 'type'>, 'type', 'event'>>,
+export type WorkflowEventCallback = (event: Omit<IWorkflowEventCallbackParams, 'type'>) => void;
+
+export type WorkflowWildcardEventCallback = (
+  event: RenameProperty<IWorkflowEventCallbackParams, 'type', 'event'>,
 ) => void;
-export type WorkflowEventListener = (event: WorkflowEvent, cb: WorkflowEventCallback) => void;
+
+export type WorkflowEventListener = <TEvent extends WorkflowEvent>(
+  event: TEvent,
+  cb: TEvent extends '*' ? WorkflowWildcardEventCallback : WorkflowEventCallback,
+) => void;
+
 export type WorkflowSendEvent = (event: Omit<IWorkflowEventCallbackParams, 'state'>) => void;
 
-export type TSubscriber = { event: WorkflowEvent; cb: WorkflowEventCallback };
+export type TSubscriber = {
+  event: WorkflowEvent;
+  cb: (
+    event:
+      | {
+          payload?: AnyRecord;
+          state: AnyRecord;
+        }
+      | {
+          payload?: AnyRecord;
+          state: AnyRecord;
+          event: WorkflowEvent;
+        },
+  ) => void;
+};
 export type TSubscribers = Array<TSubscriber>;
 export type TNotifyParams = RenameProperty<IWorkflowEventCallbackParams, 'type', 'event'>;
