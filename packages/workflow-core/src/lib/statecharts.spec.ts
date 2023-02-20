ec0ed2739ba411891fa837409880a99988484ab2
Alon Peretz <alonp99@gmail.com>
2023-02-20 19:00:48 +0200

    feat(workflow-core): adding workflow core lib

---
+++ packages/workflow-core/src/lib/statecharts.spec.ts
@@ -0,0 +1,65 @@
+import { MachineConfig } from 'xstate';
+import { expect, test } from 'vitest';
+import { WorkflowRunner } from './statecharts';
+
+test('Basic machine sanity test', () => {
+  const simpleMachine: MachineConfig<any, any, any> = {
+    id: 'toggle',
+    initial: 'inactive',
+    states: {
+      inactive: { on: { TOGGLE: 'active' } },
+      active: { on: { TOGGLE: 'inactive' } },
+    },
+  };
+  const service = new WorkflowRunner({
+    workflowDefinition: simpleMachine,
+    context: {},
+  });
+  expect(service.state).toBe('inactive');
+  service.sendEvent({ type: 'TOGGLE' }); // from welcome to intro
+  expect(service.state).toBe('active');
+  service.sendEvent({ type: 'TOGGLE' }); // from intro to take picture
+  expect(service.state).toBe('inactive');
+});
+
+test('Basic workflow sanity test', async () => {
+  const simpleWorkflow: MachineConfig<any, any, any> = {
+    id: 'toggle',
+    initial: 'inactive',
+    states: {
+      inactive: { on: { TOGGLE: 'active' } },
+      active: { on: { TOGGLE: 'inactive' } },
+    },
+  };
+  const service = new WorkflowRunner({
+    workflowDefinition: simpleWorkflow,
+    context: {},
+    extensions: {
+      statePlugins: [],
+      globalPlugins: [
+        {
+          when: 'pre',
+          // import: '@ballerine/plugins/core/validate@0.2.34',
+          // import: '@ballerine/plugins/browser/validate@0.2.34',
+          // import: '@ballerine/plugins/node/validate@0.2.34',
+          action: (context: any, event: any, currentState: any) => {
+            console.log('global pre action');
+            return Promise.resolve();
+          },
+        },
+        {
+          when: 'post',
+          action: (context: any, event: any, currentState: any) => {
+            console.log('global post action');
+            return Promise.resolve();
+          },
+        },
+      ],
+    },
+  });
+  expect(service.state).toBe('inactive');
+  await service.sendEvent({ type: 'TOGGLE' }); // from welcome to intro
+  expect(service.state).toBe('active');
+  await service.sendEvent({ type: 'TOGGLE' }); // from intro to take picture
+  expect(service.state).toBe('inactive');
+});
---
+++ packages/workflow-core/src/lib/statecharts.ts
@@ -0,0 +1,159 @@
+import { createMachine, interpret, MachineConfig, StateMachine } from 'xstate';
+import * as jsonLogic from 'json-logic-js';
+
+import {
+  WorkflowEvent,
+  WorkflowEventWithoutState,
+  WorkflowExtensions,
+} from '..';
+
+interface WorkflowRunnerArgs {
+  workflowDefinition: MachineConfig<any, any, any>;
+  context: any;
+  state?: string;
+  extensions?: WorkflowExtensions;
+}
+
+export class WorkflowRunner {
+  #__subscription: Array<(event: WorkflowEvent) => void> = [];
+  #__workflow: StateMachine<any, any, any>;
+  #__currentStateNode: any;
+  #__currentState: string | undefined | symbol | number | any;
+  #__context: any;
+  #__callback: ((event: WorkflowEvent) => void) | null = null;
+  #__extensions: WorkflowExtensions;
+  #__debugMode: boolean;
+
+  public get workflow() {
+    return this.#__workflow;
+  }
+
+  public get state() {
+    return this.#__currentState;
+  }
+
+  constructor(
+    { workflowDefinition, context = {}, state, extensions }: WorkflowRunnerArgs,
+    debugMode = true
+  ) {
+    this.#__workflow = this.#__extendedWorkflow(workflowDefinition);
+
+    // use initial context or provided context
+    this.#__context = Object.keys(context).length
+      ? context
+      : workflowDefinition.context || {};
+
+    //use initial state or provided state
+    this.#__currentState = state ? state : workflowDefinition.initial;
+
+    // global and state specific extensions
+    this.#__extensions = extensions || { globalPlugins: [], statePlugins: [] };
+    this.#__debugMode = debugMode;
+  }
+
+  #__extendedWorkflow(workflow: any) {
+    const extended = workflow;
+    const onEnter = ['ping'];
+    const onExit = ['pong'];
+
+    for (const state in extended.states) {
+      extended.states[state].entry = onEnter.concat(
+        workflow.states[state].entry || [],
+        onEnter
+      );
+      extended.states[state].exit = onExit.concat(
+        workflow.states[state].exit || [],
+        onExit
+      );
+    }
+
+    const actions = {
+      ping: (...rest: any[]) => {
+        console.log('Global state entry handler');
+      },
+      pong: (...rest: any[]) => {
+        console.log('Global state exit handler');
+      },
+    };
+    const guards = {
+      'json-rule': (ctx: any, { payload }: any, { cond }: any) => {
+        const data = { ...ctx, ...payload };
+        return jsonLogic.apply(
+          cond.name, // Rule
+          data // Data
+        );
+      },
+    };
+
+    return createMachine(
+      { predictableActionArguments: false, ...extended },
+      { actions, guards }
+    );
+  }
+
+  async sendEvent(event: WorkflowEventWithoutState) {
+    const workflow = this.#__workflow.withContext(this.#__context);
+    console.log('Current state:', this.#__currentState);
+
+    const service = interpret(workflow)
+      .start(this.#__currentState)
+      .onTransition((state) => {
+        if (state.changed) {
+          console.log('Transitioned into', state.value);
+
+          if (state.configuration?.[0]?.['type'] == 'final') {
+            console.log('Reached final state');
+          }
+
+          if (this.#__callback) {
+            // TODO: clean it up
+            const name = state.configuration?.[0]?.['key'] ?? '';
+
+            this.#__callback({
+              ...event,
+              state: name,
+            });
+          }
+        }
+
+        this.#__currentStateNode = state;
+        this.#__currentState = state.value;
+      });
+
+    // all sends() will be deferred until the workflow is started
+    service.start();
+
+    for (const ext of this.#__extensions.globalPlugins) {
+      if (ext.when == 'pre') {
+        await ext.action(
+          service.getSnapshot().context,
+          event,
+          this.#__currentStateNode
+        );
+      }
+    }
+    service.send(event);
+    this.#__context = service.getSnapshot().context;
+    if (this.#__debugMode) {
+      console.log('context:', this.#__context);
+    }
+
+    for (const ext of this.#__extensions.globalPlugins) {
+      if (ext.when == 'post') {
+        await ext.action(this.#__context, event, this.#__currentStateNode);
+      }
+    }
+  }
+
+  subscribe(callback: (event: WorkflowEvent) => void) {
+    this.#__callback = callback;
+    // Not currently in use.
+    this.#__subscription.push(callback);
+  }
+
+  getSnapshot() {
+    const service = interpret(this.#__workflow.withContext(this.#__context));
+    service.start(this.#__currentState);
+    return service.getSnapshot();
+  }
+}
