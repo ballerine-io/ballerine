690dbd32df25f659a8365ed2016b208655bebd3c
Alon Peretz <Alonp99@gmail.com>
2023-02-22 10:30:10 +0200

    feat(workflow-core): adding workflow core lib (#134)
    * feat(workflow-core): adding workflow core lib
    
    * feat: d
    
    d
    
    * Refactor core's api for consistency and latest considerations (#140)
    
    * chore(version control): init commit for wip pr
    
    * refactor(workflowoptions): renamed WorkflowDefType to workflowDefinitionType for consistency
    
    now matches workflowDefinition
    
    * refactor(workflowplugin.action): plugin actions now use an options object instead of args
    
    * feat(sendevent): now statePlugins is part of sendEvent like globalPlugins
    
    handles pre and post respectively
    
    * refactor(stateplugins): state plugins now accepts an array of states instead of a string
    
    allows running the same logic on multiple different states
    
    ---------
    
    Co-authored-by: Omri Levy <61207713+Omri-Levy@users.noreply.github.com>

---
+++ packages/workflow-core/src/index.ts
@@ -0,0 +1,70 @@
+import { MachineConfig } from 'xstate';
+import { WorkflowRunner } from './lib/statecharts';
+
+export * from './lib/statecharts';
+
+interface Workflow {
+  subscribe: (callback: (event: WorkflowEvent) => void) => void;
+  sendEvent: (event: Omit<WorkflowEvent, 'state'>) => void;
+  getSnapshot: () => Record<PropertyKey, any>;
+}
+
+export interface WorkflowContext {
+  id: string;
+  state: any;
+  localContextData: any;
+  sessionData: any;
+  lockKey: string;
+}
+
+interface WorkflowPlugin {
+  when: 'pre' | 'post';
+  action: (options: {
+    context: any;
+    event: any;
+    currentState: any;
+  }) => Promise<void>;
+}
+
+interface StatePlugin extends WorkflowPlugin {
+  stateNames: Array<string>;
+}
+
+interface GlobalPlugin extends WorkflowPlugin {
+  stateName: string;
+}
+
+export interface WorkflowEvent {
+  type: string;
+  state: string;
+  payload?: Record<PropertyKey, any>;
+}
+
+export type WorkflowEventWithoutState = Omit<WorkflowEvent, 'state'>;
+
+type StatePlugins = StatePlugin[];
+type GlobalPlugins = GlobalPlugin[];
+
+export interface WorkflowExtensions {
+  statePlugins: StatePlugins;
+  globalPlugins: GlobalPlugins;
+}
+
+export interface WorkflowOptions {
+  workflowDefinitionType: 'statechart-json' | 'bpmn-json';
+  workflowDefinition: MachineConfig<any, any, any>;
+  context?: WorkflowContext;
+  extensions?: WorkflowExtensions;
+}
+
+type TCreateWorkflow = (options: WorkflowOptions) => Workflow;
+
+export const createWorkflow: TCreateWorkflow = ({
+  workflowDefinition,
+  extensions,
+}) =>
+  new WorkflowRunner({
+    workflowDefinition,
+    context: {},
+    extensions,
+  });
