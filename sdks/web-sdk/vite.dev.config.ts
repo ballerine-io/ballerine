73f313199e92f39d07e7fa7382c292cbdf99fcd3
Omri Levy <omri.levy0197@gmail.com>
2023-03-04 17:41:47 +0200

    fix(ci): testing fixes for ci/web-sdk

--- sdks/web-sdk/vite.dev.config.ts
+++ sdks/web-sdk/vite.dev.config.ts
@@ -1,12 +1,15 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import type { UserConfig } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: UserConfig['mode'] }) => {
   console.log('VITE', mode);
   const env = loadEnv(mode, process.cwd(), '');
   return defineConfig({
+    resolve: {
+      preserveSymlinks: true,
+    },
     server: {
       port: 3000,
       host: '127.0.0.1',
