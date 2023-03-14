d2acd91c7131ca61de5fb480896ef257497837de
Alon Peretz <alonp99@gmail.com>
2023-03-14 22:50:43 +0200

    feat(d): d
    d

--- sdks/web-sdk/src/lib/utils/string-utils/resolve-string-template.ts
+++ sdks/web-sdk/src/lib/utils/string-utils/resolve-string-template.ts
@@ -1,7 +1,7 @@
 import { isEmptyObject } from '@ballerine/common';
 
 export const resolveStringTemplate = (str: string, obj?: StringKV) => {
-  // @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
+  // @typescript-eslint/no-unsafe-call
   if (!obj || isEmptyObject(obj)) return str;
   return str.replace(/\${(.*?)}/g, (x, g: string) => obj[g]);
 };
