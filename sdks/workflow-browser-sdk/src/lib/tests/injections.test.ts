199ca22aca70d25471de61238c1d60009a4ed7b9
Victor <32337426+victor-ca@users.noreply.github.com>
2023-04-04 15:12:12 +0300

    chore: reverted failed tests

--- sdks/workflow-browser-sdk/src/lib/tests/injections.test.ts
+++ sdks/workflow-browser-sdk/src/lib/tests/injections.test.ts
@@ -1,11 +1,10 @@
-/* eslint-disable */
 import { beforeEach, describe, expect, it } from 'vitest';
 import { WorkflowBrowserSDK } from '../workflow-browser-sdk';
 import { workflowOptions } from './workflow-options';
 
 let workflowService: WorkflowBrowserSDK;
-let machine: any;
-let actions: any;
+let machine;
+let actions;
 
 beforeEach(() => {
   workflowService = new WorkflowBrowserSDK(workflowOptions);
@@ -27,6 +26,41 @@ describe('injections', () => {
     expect(machine.states.second.config.on.USER_NEXT_STEP).toMatchObject({
       target: 'third',
       actions: ['USER_NEXT_STEP'],
+    });
+  });
+
+  it('should inject plugins', () => {
+    workflowService = new WorkflowBrowserSDK({
+      ...workflowOptions,
+      submitStates: [],
+    });
+    machine = workflowService.getSnapshot().machine;
+
+    // Submit plugin falls back to states with type: 'final'
+    expect(
+      machine.states.last.onEntry?.find(({ type }) => type === 'SUBMIT_BACKEND'),
+    ).toMatchObject({
+      type: 'SUBMIT_BACKEND',
+      exec: undefined,
+    });
+
+    // Injects user defined submitStates
+    workflowService = new WorkflowBrowserSDK(workflowOptions);
+    machine = workflowService.getSnapshot().machine;
+
+    expect(
+      machine.states.fourth.onEntry?.find(({ type }) => type === 'SUBMIT_BACKEND'),
+    ).toMatchObject({
+      type: 'SUBMIT_BACKEND',
+      exec: undefined,
+    });
+
+    // Injects user defined persistStates
+    expect(
+      machine.states.third.onEntry?.find(({ type }) => type === 'SYNC_LOCAL_STORAGE'),
+    ).toMatchObject({
+      type: 'SYNC_LOCAL_STORAGE',
+      exec: undefined,
     });
   });
 });
--- sdks/workflow-node-sdk/src/lib/workflow-node-sdk.test.ts
+++ sdks/workflow-node-sdk/src/lib/workflow-node-sdk.test.ts
@@ -13,7 +13,7 @@ const simpleMachine = {
   },
 };
 
-test('Simple Server Workflow', () => {
+test('Simple Server Workflow', t => {
   console.log('Running create Server Workflow');
 
   const workflow = createWorkflow({
@@ -28,3 +28,74 @@ test('Simple Server Workflow', () => {
   runner.sendEvent({ type: 'TOGGLE' });
   expect(runner.getSnapshot().value).toBe('inactive');
 });
+
+test('Server Workflow persistance MemoryStore', t => {
+  console.log('Server Workflow persistance MemoryStore');
+
+  const userId = '123456';
+  const memoryStore = new MemoryStore();
+  const memoryPersistancePlugin = new MemoryPersistencePlugin({
+    name: 'in-memory-persistance',
+    stateNames: [],
+    when: 'post',
+    store: memoryStore,
+  });
+
+  simpleMachine.context = { ...(simpleMachine.context || {}), entityId: userId };
+
+  // Create workflow, send an events and verify state transition
+  // Runner is loaded with a memory persistance plugin
+  // that saves 'post' transition the memory state of the running workflow
+
+  const workflow = createWorkflow({
+    definitionType: 'statechart-json',
+    definition: simpleMachine,
+    extensions: {
+      statePlugins: [memoryPersistancePlugin],
+    },
+  });
+
+  expect(workflow.getSnapshot().value).toBe('inactive');
+  workflow.sendEvent({ type: 'TOGGLE' });
+  expect(workflow.getSnapshot().value).toBe('active');
+
+  // check memory store, verify it contains the user workflow runtime data
+  const userWorkflows = memoryStore.find(userId);
+  expect(userWorkflows.length).toBe(1);
+
+  const workflowId = userWorkflows[0]!;
+  let workflowData = memoryStore.get(workflowId, userId);
+
+  expect(workflowData).toBeTruthy();
+
+  // create a new workflow runner, load into it the context and set state.
+
+  console.log(workflowData);
+  const restoredWorkflow = createWorkflow({
+    definitionType: 'statechart-json',
+    definition: simpleMachine,
+    workflowContext: { machineContext: workflowData!.context, state: workflowData!.state },
+    extensions: {
+      statePlugins: [memoryPersistancePlugin],
+    },
+  });
+
+  restoredWorkflow.sendEvent({ type: 'TOGGLE' });
+  expect(restoredWorkflow.getSnapshot().value).toBe('inactive');
+});
+
+// test('Server Workflow with logic', (t) => {
+//   console.log('Running create Server Workflow');
+// });
+
+// test('Server Workflow with plugins', (t) => {
+//   console.log('Running create Server Workflow');
+// });
+
+// test('Server Workflow with locking (Workflow scope)', (t) => {
+//   console.log('Running create Server Workflow');
+// });
+
+// test('Server Workflow with locking (User scope)', (t) => {
+//   console.log('Running create Server Workflow');
+// });
