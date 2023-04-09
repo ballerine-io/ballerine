8fc3aa859e32712f35c47fdf90b14a4ec0c2a931
Omri Levy <omri.levy0197@gmail.com>
2023-04-09 16:15:14 +0300

    refactor(headless-example): now the ThankYou component follows the figma design closer

--- examples/headless-example/src/components/ThankYou.svelte
+++ examples/headless-example/src/components/ThankYou.svelte
@@ -2,10 +2,10 @@
 import Card from "@/components/Card.svelte";
 </script>
 
-<Card>
-  <h1 class="font-bold mb-2 text-2xl w-full text-center">Thank You</h1>
-  <img src="/thank-you.svg" alt="clock" class="mx-auto mb-2"/>
-  <p class="max-w-[50ch] p-1">
+<Card class="min-h-[18rem]">
+  <h1 class="font-bold mb-4 text-2xl w-full text-center">Thank You</h1>
+  <img src="/thank-you.svg" alt="clock" class="mx-auto mb-7"/>
+  <p class="max-w-[50ch] text-center p-1">
     Your onboarding request was sent to a manual review. Once a decision was made you’d be able
     to see it here.
   </p>
