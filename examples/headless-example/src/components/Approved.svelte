fd6b77ed1623b5820acab53f65abce3c4fa71ca8
Omri Levy <omri.levy0197@gmail.com>
2023-04-09 16:28:32 +0300

    refactor(headless-example): rejected and approved components now follow the figma design closer

--- examples/headless-example/src/components/Approved.svelte
+++ examples/headless-example/src/components/Approved.svelte
@@ -2,11 +2,13 @@
 import Card from "@/components/Card.svelte";
 </script>
 
-<Card>
+<Card class="min-h-[18rem]">
   <h1 class="font-bold text-center w-full text-2xl">
     Success!
   </h1>
-  <img src="/approved.svg" alt="approved" class="mx-auto mb-2"/>
+  <div class="grid place-content-center h-full max-h-28 mb-2">
+  <img src="/approved.svg" alt="approved" class="w-14 h-14"/>
+  </div>
   <p class="max-w-[50ch] p-1 text-center">
     Your information was verified successfully.
   </p>
