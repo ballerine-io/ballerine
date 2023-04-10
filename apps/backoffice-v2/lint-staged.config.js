a0b3ba026d7a87e9f5667c1c92f96f2ef4004ed8
Omri Levy <omri.levy0197@gmail.com>
2023-04-10 12:53:24 +0300

    fix(.lintstagedrc.json): removed git add - already covered by lint-staged

--- apps/backoffice-v2/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.{ts,tsx,css}': ['prettier . --write'],
-  '*.{ts,tsx}': [
-    'eslint . --cache --fix --ext .tsx --ext .ts',
-    () => 'pnpm tsc',
-    () => 'pnpm jest --passWithNoTests',
-  ],
-};
