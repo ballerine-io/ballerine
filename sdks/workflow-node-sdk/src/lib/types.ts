08559ef33f601767fb2ebfe574a089171f5488aa
Alon Peretz <alonp99@gmail.com>
2023-04-11 13:15:03 +0300

    ddd

--- sdks/workflow-node-sdk/src/lib/types.ts
+++ sdks/workflow-node-sdk/src/lib/types.ts
@@ -4,6 +4,19 @@ import { WorkflowNodeSDK } from './workflow-node-sdk';
 
 export interface WorkflowOptionsNode
   extends Pick<WorkflowOptions, 'definition' | 'definitionType' | 'workflowContext'> {
+  childWorkflows?: [
+    {
+      workflowDefinitionId: string;
+      workflowDefinitionVersion?: number;
+      states: Array<string>;
+      callbackEvent: string;
+      initialContext: Object;
+      // Context to copy from the parent workflow
+      contextToCopy?: Object;
+      // When a child workflow is done, what should be sent back to the parent workflow with the callback event 
+      responseFromContext?: Object;
+    },
+  ];
   extensions?: {
     statePlugins: Array<NodePlugin>;
   };
--- sdks/workflow-node-sdk/src/lib/workflow-node-sdk.ts
+++ sdks/workflow-node-sdk/src/lib/workflow-node-sdk.ts
@@ -1,22 +1,81 @@
 import { createWorkflow } from '@ballerine/workflow-core';
 import { TCreateWorkflowCoreReturn, WorkflowOptionsNode } from './types';
 
+// NOW
+const wokrflow = new WorkflowNodeSDK({definition,context})
+// this happend on each workflow init
+workflow.subscribe('invokeWorkflow', workflowDefID, workflowVersion, workflowContext, callbackEvent, callbackContext)
+
+// Suggestion
+const wokrflowRunner = new WorkflowNodeSDK(options)
+wokrflowRunner.subscribe('invokeWorkflow', workflowDefID, workflowVersion, workflowContext, callbackEvent, callbackContext)
+
+wokrflowRunner.createWorkflow(...)
+wokrflowRunner.createWorkflow(...)
+wokrflowRunner.createWorkflow(...).subscribe
+
+// Child workflow context, on invoke
+// should be pre-added in contextToCopy
+const context = {
+  parentWrofklow: {
+    runtimeId,
+     callbackEvent,
+     // Data from the context we should attach to the callback event
+      callbackContext:
+      
+  }
+};
+
+
+//{ parentMachine: '#runtimeID', event: 'Next', dataFromContext: { userData: { state: true } } }
+
+
+
+
 export class WorkflowNodeSDK {
   #__service: TCreateWorkflowCoreReturn;
 
   constructor(options: WorkflowOptionsNode) {
-    this.#__service = createWorkflow(options);
+    const { invokeWorkflow } = options;
+    if () {
+      // here we should inject a plugin/action that will invoke child workflows
+      // The plugin should be able to expose events to the host saying which child workflow should be invoked
+      throw new Error('childWorkflows not supported in Node SDK');
+    }
+    this.#__service = createWorkflow({ definition, definitionType, extensions, workflowContext });
   }
 
+  // this part should change to the way the browser sdk works (event based)
+  // the workflow service (host) should be able to listen to "ChildWorkflowInvoked" events
+  // and insert create the runtime instance of the child workflow
+  // subscribe(callback: Parameters<TCreateWorkflowCoreReturn['subscribe']>[0]) {
+  //   this.#__service.subscribe(callback);
+  // }
   subscribe(callback: Parameters<TCreateWorkflowCoreReturn['subscribe']>[0]) {
-    this.#__service.subscribe(callback);
+    'invokeWorkflow', workflowDefID..
   }
 
-  sendEvent(event: Parameters<TCreateWorkflowCoreReturn['sendEvent']>[0]) {
-    this.#__service.sendEvent(event);
-  }
+  // sendEvent(event: Parameters<TCreateWorkflowCoreReturn['sendEvent']>[0]) {
+  //   this.#__service.sendEvent(event);
+  // }
 
-  getSnapshot() {
-    return this.#__service.getSnapshot();
+  // getSnapshot() {
+  //   return this.#__service.getSnapshot();
+  // }
+
+  createWorkflow({ definition, definitionType, extensions, workflowContext }: WorkflowOptionsNode) {
+    const service = createWorkflow({ definition, definitionType, extensions, workflowContext });
+
+    return {
+      sendEvent: (event: Parameters<TCreateWorkflowCoreReturn['sendEvent']>[0]) => {
+        service.sendEvent(event);
+      },
+      subscribe: (callback: Parameters<TCreateWorkflowCoreReturn['subscribe']>[0]) => {
+        service.subscribe(callback);
+      },
+      getSnapshot: () => {
+        return service.getSnapshot();
+      },
+    };
   }
 }
