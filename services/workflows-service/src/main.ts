26f265398f3f119d49d1e4478e914ef2364008aa
Omri Levy <omri.levy0197@gmail.com>
2023-04-24 17:50:38 +0300

    fix(workflows-service): added eslint ignore to ts ignore due to helmet module is not resolved

--- services/workflows-service/src/main.ts
+++ services/workflows-service/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { swaggerDocumentOptions, swaggerPath, swaggerSetupOptions } from './swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { PathItemObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import helmet from 'helmet';
 
