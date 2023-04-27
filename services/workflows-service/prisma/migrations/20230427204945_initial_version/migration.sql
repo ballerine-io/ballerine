9c6f2041a0e96433a24ca9837466914ff73363bc
Alon Peretz <Alonp99@gmail.com>
2023-04-27 23:52:20 +0300

    update pnpm lock

--- services/workflows-service/prisma/migrations/20230427204945_initial_version/migration.sql
+++ services/workflows-service/prisma/migrations/20230427204945_initial_version/migration.sql
@@ -1,10 +1,13 @@
 -- CreateEnum
-CREATE TYPE "EndUserState" AS ENUM ('APPROVED', 'REJECTED', 'PROCESSING', 'NEW');
+CREATE TYPE "ApprovalState" AS ENUM ('APPROVED', 'REJECTED', 'PROCESSING', 'NEW');
 
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
     "password" TEXT NOT NULL,
     "roles" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,7 +22,7 @@ CREATE TABLE "EndUser" (
     "correlationId" VARCHAR NOT NULL,
     "verificationId" VARCHAR,
     "endUserType" TEXT,
-    "state" "EndUserState" NOT NULL DEFAULT 'NEW',
+    "approvalState" "ApprovalState" NOT NULL DEFAULT 'NEW',
     "stateReason" VARCHAR,
     "jsonData" JSONB,
     "firstName" VARCHAR,
@@ -31,8 +34,35 @@ CREATE TABLE "EndUser" (
     "additionalInfo" JSONB,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "businessId" TEXT,
 
     CONSTRAINT "EndUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Business" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "registrationNumber" TEXT NOT NULL,
+    "legalForm" TEXT NOT NULL,
+    "countryOfIncorporation" TEXT NOT NULL,
+    "dateOfIncorporation" TIMESTAMP(3),
+    "address" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "website" TEXT,
+    "industry" TEXT NOT NULL,
+    "taxIdentificationNumber" TEXT,
+    "vatNumber" TEXT,
+    "shareholderStructure" JSONB,
+    "numberOfEmployees" INTEGER,
+    "businessPurpose" TEXT,
+    "documents" JSONB NOT NULL,
+    "status" "ApprovalState" NOT NULL DEFAULT 'PROCESSING',
+
+    CONSTRAINT "Business_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -83,10 +113,16 @@ CREATE TABLE "File" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
 
 -- AddForeignKey
-ALTER TABLE "WorkflowRuntimeData" ADD CONSTRAINT "WorkflowRuntimeData_endUserId_fkey" FOREIGN KEY ("endUserId") REFERENCES "EndUser"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "EndUser" ADD CONSTRAINT "EndUser_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkflowRuntimeData" ADD CONSTRAINT "WorkflowRuntimeData_endUserId_fkey" FOREIGN KEY ("endUserId") REFERENCES "EndUser"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "WorkflowRuntimeData" ADD CONSTRAINT "WorkflowRuntimeData_workflowDefinitionId_fkey" FOREIGN KEY ("workflowDefinitionId") REFERENCES "WorkflowDefinition"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
