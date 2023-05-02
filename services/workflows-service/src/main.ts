b5c12490df42472616a5e9354a7bb3a61b9eec00
Omri Levy <omri.levy0197@gmail.com>
2023-05-02 10:53:40 +0300

    refactor(workflows-service): requested pr changes - session domain and session secret

--- services/workflows-service/src/main.ts
+++ services/workflows-service/src/main.ts
@@ -27,13 +27,13 @@ async function main() {
   app.use(
     session({
       name: 'session',
-      secret: process.env.JWT_SECRET_KEY!,
+      secret: process.env.SESSION_SECRET!,
       saveUninitialized: false,
       resave: false,
       rolling: true,
       cookie: {
         httpOnly: true,
-        domain: process.env.NODE_ENV === 'production' ? '.example.com' : undefined,
+        domain: process.env.NODE_ENV === 'production' ? '.ballerine.app' : undefined,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
         maxAge: 1000 * 60 * 60 * 1, // 1 hour(s)
