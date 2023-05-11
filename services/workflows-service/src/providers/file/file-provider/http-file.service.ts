f84c5a65e4b1dcdb1fd296d7f79226df8a0d6f21
blokh <danielblokhi@gmail.com>
2023-05-11 12:27:28 +0300

    feat(file service): finalized file service provider and stracture

--- services/workflows-service/src/providers/file/file-provider/http-file.service.ts
+++ services/workflows-service/src/providers/file/file-provider/http-file.service.ts
@@ -1,20 +1,27 @@
 import {
-  IFileProvider, TFileServiceProvider,
+  IStreamableFileProvider,
   TLocalFilePath,
-  TRemoteFileConfig, TRemoteUri,
-} from "@/providers/file/types";
-import { promises as fsPromises } from "fs";
-import axios from "axios"; // TODO: NEED to push to package
+  TRemoteFileConfig,
+  TRemoteUri,
+} from '@/providers/file/types';
+import { promises as fsPromises } from 'fs';
+import axios, { AxiosResponse } from 'axios';
+import { Readable } from 'stream';
 
-export class HttpFileService implements IFileProvider {
+export class HttpFileService implements IStreamableFileProvider {
   protected client;
-  constructor(...args: any) {
+  constructor(..._args: any) {
     this.client = axios;
   }
 
-  async downloadFile(remoteFileConfig: TRemoteFileConfig, localFilePath: TLocalFilePath): Promise<TLocalFilePath> {
+  async downloadFile(
+    remoteFileConfig: TRemoteFileConfig,
+    localFilePath: TLocalFilePath,
+  ): Promise<TLocalFilePath> {
     try {
-      const response = await this.client.get(remoteFileConfig as TRemoteUri, { responseType: "arraybuffer" });
+      const response = await this.client.get(remoteFileConfig as TRemoteUri, {
+        responseType: 'arraybuffer',
+      });
 
       if (response.status < 200 || response.status >= 300) {
         throw new Error(`Error downloading file: ${response.statusText}`);
@@ -24,7 +31,7 @@ export class HttpFileService implements IFileProvider {
       await fsPromises.writeFile(localFilePath, Buffer.from(fileBuffer));
       return localFilePath;
     } catch (error) {
-      console.error("Error downloading the file:", error);
+      console.error('Error downloading the file:', error);
       throw error;
     }
   }
@@ -34,13 +41,29 @@ export class HttpFileService implements IFileProvider {
       const response = await this.client.head(remoteFileConfig);
       return response.status >= 200 && response.status < 300;
     } catch (error) {
-      console.error("Error checking if remote file exists:", error);
+      console.error('Error checking if remote file exists:', error);
       throw error;
     }
   }
 
   async uploadFile(...any: any): Promise<TRemoteFileConfig> {
-    throw new Error("Unable to use upload to uri client")
+    throw new Error('Unable to use upload to uri client');
   }
 
+  async fetchRemoteFileDownStream(remoteFileConfig: TRemoteFileConfig): Promise<Readable> {
+    const remoteUri = remoteFileConfig as TRemoteUri;
+
+    const response: AxiosResponse<Readable> = await axios.get(remoteUri, {
+      responseType: 'stream',
+    });
+
+    return response.data;
+  }
+
+  async uploadFileStream(
+    fileStream: Readable,
+    remoteFileConfig: TRemoteFileConfig,
+  ): Promise<TRemoteFileConfig> {
+    throw new Error('Unable to use upload to uri client');
+  }
 }
--- services/workflows-service/src/providers/file/file-service.service.ts
+++ services/workflows-service/src/providers/file/file-service.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {TLocalFilePath, TRemoteFileConfig, TFileServiceProvider, IStreamableFileProvider} from './types';
+import {
+  TLocalFilePath,
+  TRemoteFileConfig,
+  TFileServiceProvider,
+  IStreamableFileProvider,
+} from './types';
 import * as tmp from 'tmp';
 
 @Injectable()
@@ -9,40 +14,69 @@ export class FileServiceService {
     fromServiceProvider: TFileServiceProvider,
     fromRemoteFileConfig: TRemoteFileConfig,
     toServiceProvider: TFileServiceProvider,
-    toFileConfig: TRemoteFileConfig
-  ){
-    if (this.isBothServicesSupportStream(fromServiceProvider, toServiceProvider)){
-      return this.copyThruStream(fromServiceProvider as IStreamableFileProvider, fromRemoteFileConfig, toServiceProvider as IStreamableFileProvider, toFileConfig)
+    toFileConfig: TRemoteFileConfig,
+  ) {
+    if (this.isBothServicesSupportStream(fromServiceProvider, toServiceProvider)) {
+      return this.copyThruStream(
+        fromServiceProvider as IStreamableFileProvider,
+        fromRemoteFileConfig,
+        toServiceProvider as IStreamableFileProvider,
+        toFileConfig,
+      );
     }
 
-    return await this.copyThruDownload(fromServiceProvider, fromRemoteFileConfig, toServiceProvider, toFileConfig);
+    return await this.copyThruLocalFile(
+      fromServiceProvider,
+      fromRemoteFileConfig,
+      toServiceProvider,
+      toFileConfig,
+    );
   }
 
+  async copyThruStream(
+    fromServiceProvider: IStreamableFileProvider,
+    fromRemoteFileConfig: TRemoteFileConfig,
+    toServiceProvider: IStreamableFileProvider,
+    toFileConfig: TRemoteFileConfig,
+  ) {
+    const streamableDownstream = await fromServiceProvider.fetchRemoteFileDownStream(
+      fromRemoteFileConfig,
+    );
 
-  async copyThruStream(fromServiceProvider: IStreamableFileProvider, fromRemoteFileConfig: TRemoteFileConfig, toServiceProvider: IStreamableFileProvider, toFileConfig: TRemoteFileConfig) {
-  const streamableDownstream = await fromServiceProvider.fetchRemoteFileDownStream(fromRemoteFileConfig);
-
-  return await toServiceProvider.uploadFileStream(streamableDownstream, toFileConfig);
+    return await toServiceProvider.uploadFileStream(streamableDownstream, toFileConfig);
   }
 
-  async copyThruDownload(fromServiceProvider: TFileServiceProvider, fromRemoteFileConfig: TRemoteFileConfig, toServiceProvider: TFileServiceProvider, toFileConfig: TRemoteFileConfig) {
+  async copyThruLocalFile(
+    fromServiceProvider: TFileServiceProvider,
+    fromRemoteFileConfig: TRemoteFileConfig,
+    toServiceProvider: TFileServiceProvider,
+    toFileConfig: TRemoteFileConfig,
+  ) {
     const tmpFile = tmp.fileSync();
 
-    const localeFilePath = await fromServiceProvider.downloadFile(fromRemoteFileConfig, tmpFile.name);
+    const localeFilePath = await fromServiceProvider.downloadFile(
+      fromRemoteFileConfig,
+      tmpFile.name,
+    );
     const remoteFilePath = await toServiceProvider.uploadFile(localeFilePath, toFileConfig);
 
     return remoteFilePath;
   }
 
-  private isBothServicesSupportStream(fromServiceProvider: TFileServiceProvider, toServiceProvider: TFileServiceProvider) {
-    return 'fetchRemoteFileDownStream' in fromServiceProvider && 'uploadFileStream' in toServiceProvider;
+  private isBothServicesSupportStream(
+    fromServiceProvider: TFileServiceProvider,
+    toServiceProvider: TFileServiceProvider,
+  ) {
+    return (
+      'fetchRemoteFileDownStream' in fromServiceProvider && 'uploadFileStream' in toServiceProvider
+    );
   }
 
   async downloadFile(
     fromServiceProvider: TFileServiceProvider,
     fromRemoteFileConfig: TRemoteFileConfig,
-    toLocaleFilePath: TLocalFilePath
+    toLocaleFilePath: TLocalFilePath,
   ) {
-    return await fromServiceProvider.downloadFile(fromRemoteFileConfig, toLocaleFilePath)
+    return await fromServiceProvider.downloadFile(fromRemoteFileConfig, toLocaleFilePath);
   }
 }
