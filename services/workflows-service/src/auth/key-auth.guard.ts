34df5d0cb3291cf666fea54e3698628c57901f16
Matan Yadaev <matan.yed@gmail.com>
2023-05-16 15:06:20 +0300

    feat: add key auth guard

---
+++ services/workflows-service/src/auth/key-auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class KeyAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const req = context.switchToHttp().getRequest<Request>();
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    const apiKey = authHeader.split(' ')[1];
+
+    if (apiKey !== process.env.API_KEY) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    return true;
+  }
+}
