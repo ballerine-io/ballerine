16c4df84406e21d71398ffea052f9b35fe9b1114
Matan Yadaev <matan.yed@gmail.com>
2023-05-16 15:10:05 +0300

    style: simplify

--- services/workflows-service/src/auth/key-auth.guard.ts
+++ services/workflows-service/src/auth/key-auth.guard.ts
@@ -6,12 +6,7 @@ export class KeyAuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const req = context.switchToHttp().getRequest<Request>();
 
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      throw new UnauthorizedException('Invalid API key');
-    }
-
+    const authHeader = req.headers.authorization || '';
     const apiKey = authHeader.split(' ')[1];
 
     if (apiKey !== process.env.API_KEY) {
