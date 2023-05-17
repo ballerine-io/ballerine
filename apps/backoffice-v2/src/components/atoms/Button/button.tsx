8232e54f33de223ebc2d048f669f2b24619b9528
Omri Levy <omri.levy0197@gmail.com>
2023-05-17 16:41:41 +0300

    feat(backoffice-v2): buttons are now gray when disabled

--- apps/backoffice-v2/src/components/atoms/Button/button.tsx
+++ apps/backoffice-v2/src/components/atoms/Button/button.tsx
@@ -4,7 +4,7 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { ctw } from '../../../utils/ctw/ctw';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:text-primary-foreground disabled:bg-slate-400 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
   {
     variants: {
       variant: {
