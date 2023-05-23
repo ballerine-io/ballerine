081b401282a45d279079dd973b235e582ff64d86
Omri Levy <omri.levy0197@gmail.com>
2023-05-23 23:43:35 +0300

    refactor(backoffice-v2): moved mock service worker logic to each respective domain (workflows etc)

--- apps/backoffice-v2/src/lib/mock-service-worker/handlers.ts
+++ apps/backoffice-v2/src/lib/mock-service-worker/handlers.ts
@@ -1,6 +1,6 @@
 import { individualsController } from '../../individuals/mock-service-worker/individuals.controller';
-import { authController } from './auth/auth.controller';
-import { workflowsController } from './workflows/workflows.controller';
+import { authController } from '../../auth/mock-service-worker/auth/auth.controller';
+import { workflowsController } from '../../workflows/mock-service-worker/workflows/workflows.controller';
 
 export const handlers = [
   // Auth
