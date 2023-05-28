8fd8c1006b97222e52c56df445a3dcb1212c8e47
Alon Peretz <Alonp99@gmail.com>
2023-05-28 18:36:27 +0300

    fixed logout

--- services/workflows-service/src/declarations.d.ts
+++ services/workflows-service/src/declarations.d.ts
@@ -3,15 +3,14 @@ import { UserInfo } from '@/user/user-info';
 declare global {
   // eslint-disable-next-line no-var
   var __rootdir__: string;
+
   namespace Express {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface Request {
-      user: {
-        id: string;
-        email: string;
-        firstName: string;
-        lastName: string;
-      };
+    interface User {
+      id: string;
+      email: string;
+      firstName: string;
+      lastName: string;
     }
   }
 }
