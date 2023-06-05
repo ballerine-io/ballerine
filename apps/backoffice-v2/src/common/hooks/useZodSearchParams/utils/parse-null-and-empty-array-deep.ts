7c39f4d0f07248311129dbbc0fda554f3cd71521
Omri Levy <omri.levy0197@gmail.com>
2023-06-05 09:32:11 +0300

    docs(backoffice-v2): added comments to the stringify/parse search params functions

--- apps/backoffice-v2/src/common/hooks/useZodSearchParams/utils/parse-null-and-empty-array-deep.ts
+++ apps/backoffice-v2/src/common/hooks/useZodSearchParams/utils/parse-null-and-empty-array-deep.ts
@@ -1,3 +1,8 @@
+/**
+ * Replaces the placeholder string of null or empty array with null or an empty array respectively - a workaround for qs dropping null and empty arrays
+ * @example { a: '__null', b: [1, 2, 3], c: '__emptyArray' } -> { a: null, b: [1, 2, 3], c: [] }
+ * @param value
+ */
 export const parseNullAndEmptyArrayDeep = (value: unknown) => {
   if (value === '__emptyArray') return [];
   if (value === '__null') return null;
--- apps/backoffice-v2/src/common/hooks/useZodSearchParams/utils/stringify-null-and-empty-array-deep.ts
+++ apps/backoffice-v2/src/common/hooks/useZodSearchParams/utils/stringify-null-and-empty-array-deep.ts
@@ -1,3 +1,8 @@
+/**
+ * Replaces null or empty array with a placeholder string - a workaround for qs dropping null and empty arrays
+ * @example { a: null, b: [1, 2, 3], c: [] } -> { a: '__null', b: [1, 2, 3], c: '__emptyArray' }
+ * @param value
+ */
 export const stringifyNullAndEmptyArrayDeep = (value: unknown) => {
   if (Array.isArray(value) && !value.length) return '__emptyArray';
   if (value === null) return '__null';
