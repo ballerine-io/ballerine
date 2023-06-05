02764168d17b03b327c4fac0b7258d9156a8da7b
teselil <tzlil@datree.com>
2023-06-05 17:39:37 +0300

    fix: use env files instead of hard coded port

--- services/workflows-websocket-service/src/main.ts
+++ services/workflows-websocket-service/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { env } from '@/env';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
 
-  void app.listen(3500);
-  console.log(`Listening on port 3500`);
+  void app.listen(env.PORT);
+  console.log(`Listening on port ${env.PORT}`);
 
   return app;
 }
