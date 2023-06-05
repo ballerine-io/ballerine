4a56225163cfaf3fdbd4227e8afdf4fd55d7b8fd
Omri Levy <omri.levy0197@gmail.com>
2023-06-05 09:17:15 +0300

    refactor(backoffice-v2): refactored routes to pages

--- apps/backoffice-v2/src/Router/Router.tsx
+++ apps/backoffice-v2/src/Router/Router.tsx
@@ -1,18 +1,18 @@
 import React, { FunctionComponent } from 'react';
 import { env } from '../common/env/env';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { RootError } from '../routes/Root/Root.error';
-import { Root } from '../routes/Root/Root.page';
-import { SignIn } from '../routes/SignIn/SignIn.page';
-import { Entity } from '../routes/Entity/Entity.page';
-import { Entities } from '../routes/Entities/Entities.page';
+import { RootError } from '../pages/Root/Root.error';
+import { Root } from '../pages/Root/Root.page';
+import { SignIn } from '../pages/SignIn/SignIn.page';
+import { Entity } from '../pages/Entity/Entity.page';
+import { Entities } from '../pages/Entities/Entities.page';
 import { RouteError } from '../common/components/atoms/RouteError/RouteError';
-import { CaseManagement } from '../routes/CaseManagement/CaseManagement.page';
-import { rootLoader } from '../routes/Root/Root.loader';
-import { entityLoader } from '../routes/Entity/Entity.loader';
-import { entitiesLoader } from '../routes/Entities/Entities.loader';
-import { localeLoader } from '../routes/Locale/Locale.loader';
-import { Locale } from '../routes/Locale/Locale.page';
+import { CaseManagement } from '../pages/CaseManagement/CaseManagement.page';
+import { rootLoader } from '../pages/Root/Root.loader';
+import { entityLoader } from '../pages/Entity/Entity.loader';
+import { entitiesLoader } from '../pages/Entities/Entities.loader';
+import { localeLoader } from '../pages/Locale/Locale.loader';
+import { Locale } from '../pages/Locale/Locale.page';
 
 const router = createBrowserRouter([
   {
