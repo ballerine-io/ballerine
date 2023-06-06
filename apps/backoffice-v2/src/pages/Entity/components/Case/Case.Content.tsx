861c11e00b7c7fad8a03e7948ac4b2b8540d3419
Alon Peretz <Alonp99@gmail.com>
2023-06-06 11:57:21 +0300

    Dynamic webhook config (#501)
    * feat(dynamic-webhooks): add the ablity to overide workflow def webhook config with runtome config
    
    * adding logs
    
    * fixed tests
    
    * fixed tests
    
    * fixed tests

--- apps/backoffice-v2/src/pages/Entity/components/Case/Case.Content.tsx
+++ apps/backoffice-v2/src/pages/Entity/components/Case/Case.Content.tsx
@@ -19,13 +19,11 @@ export const Content: FunctionComponent<PropsWithChildren> = ({ children }) => {
       <motion.div
         initial={{
           opacity: 0,
-          y: '50px',
         }}
         animate={{
           opacity: 1,
-          y: 0,
         }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.3 }}
         className={`mx-2 flex flex-col space-y-3 p-1 pt-2`}
       >
         {children}
