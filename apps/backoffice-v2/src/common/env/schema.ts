c45c97079c527d4d4976a33aa1b29f6ccebf73d6
blokh <danielblokhi@gmail.com>
2023-06-06 15:34:16 +0300

    fixed minor bugs

--- apps/backoffice-v2/src/common/env/schema.ts
+++ apps/backoffice-v2/src/common/env/schema.ts
@@ -16,5 +16,5 @@ export const EnvSchema = z.object({
     .transform(v => v * 1000)
     .or(z.literal(false))
     .catch(undefined),
-  IMAGE_LOGO_URL: z.string().default('https://blrn-cdn-prod.s3.eu-central-1.amazonaws.com/assets/fido_logo.svg'),
+  IMAGE_LOGO_URL: z.string()
 });
