b946f646a0873ffe45fdd4b976888af9cdaa1bed
blokh <danielblokhi@gmail.com>
2023-06-07 11:46:42 +0300

    updated test runnign strategy

--- services/workflows-service/src/test/db-setup.ts
+++ services/workflows-service/src/test/db-setup.ts
@@ -2,8 +2,22 @@ import { PostgreSqlContainer } from 'testcontainers';
 import console from 'console';
 import { TestGlobal } from '@/test/test-global';
 import { execSync } from 'child_process';
+import os from "os";
 
 module.exports = async () => {
+  if (os.platform() === 'win32') {
+    return await setupLocalTestOnWindowsBase();
+  }
+  await setupTestContainersOnLinuxBase();
+};
+function setupLocalTestOnWindowsBase() {
+  process.env.TEST_DATABASE_SCHEMA_NAME = 'test';
+  process.env.DB_URL = 'postgresql://test:test@localhost:5432/test';
+
+  runPrismaMigrations();
+}
+
+const setupTestContainersOnLinuxBase = async () => {
   const container = await new PostgreSqlContainer().withDatabase('test').start();
   process.env.TEST_DATABASE_SCHEMA_NAME = container.getDatabase();
   process.env.DB_URL = container.getConnectionUri();
@@ -12,7 +26,7 @@ module.exports = async () => {
   runPrismaMigrations();
 
   (globalThis as TestGlobal).__DB_CONTAINER__ = container;
-};
+}
 
 const runPrismaMigrations = () => {
   try {
