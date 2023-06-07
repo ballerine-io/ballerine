cceea83f0eda8e8b2329b3cd0e78b83a42ec2929
blokh <danielblokhi@gmail.com>
2023-06-07 11:55:37 +0300

    removed windows test as it does not support db request

--- services/workflows-service/src/test/db-setup.ts
+++ services/workflows-service/src/test/db-setup.ts
@@ -2,35 +2,21 @@ import { PostgreSqlContainer } from 'testcontainers';
 import console from 'console';
 import { TestGlobal } from '@/test/test-global';
 import { execSync } from 'child_process';
-import os from "os";
 
 module.exports = async () => {
-  if (os.platform() === 'win32') {
-    return await setupLocalTestOnWindowsBase();
-  }
-  await setupTestContainersOnLinuxBase();
-};
-function setupLocalTestOnWindowsBase() {
-  process.env.TEST_DATABASE_SCHEMA_NAME = 'test';
-  process.env.DB_URL = 'postgresql://test:test@localhost:5432/test';
-
-  runPrismaMigrations();
-}
-
-const setupTestContainersOnLinuxBase = async () => {
   const container = await new PostgreSqlContainer().withDatabase('test').start();
   process.env.TEST_DATABASE_SCHEMA_NAME = container.getDatabase();
   process.env.DB_URL = container.getConnectionUri();
   console.log('\nStarting database container on: ' + container.getConnectionUri());
 
-  runPrismaMigrations();
+  await runPrismaMigrations();
 
   (globalThis as TestGlobal).__DB_CONTAINER__ = container;
-}
+};
 
-const runPrismaMigrations = () => {
+const runPrismaMigrations = async () => {
   try {
-    execSync('npx prisma migrate dev --preview-feature', { stdio: 'inherit' });
+    await execSync('npx prisma migrate dev --preview-feature', { stdio: 'inherit' });
   } catch (error) {
     console.error('Prisma migration failed:');
     console.error(error);
