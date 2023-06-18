9810ab0b115f705443a9a2d052b910c85457187c
Alon Peretz <Alonp99@gmail.com>
2023-06-18 16:07:01 +0300

    feat(workflow-def): enable mulitple active workflows

--- services/workflows-service/src/workflow/workflow.service.ts
+++ services/workflows-service/src/workflow/workflow.service.ts
@@ -607,6 +607,7 @@ export class WorkflowService {
     const workflowDefinition = await this.workflowDefinitionRepository.findById(
       workflowDefinitionId,
     );
+    config = merge(workflowDefinition.config, config);
     let validatedConfig: WorkflowConfig;
     try {
       validatedConfig = ConfigSchema.parse(config);
@@ -627,24 +628,16 @@ export class WorkflowService {
 
     let contextToInsert = structuredClone(context);
 
-    if (existingWorkflowRuntimeData) {
-      contextToInsert.documents = updateDocuments(
-        existingWorkflowRuntimeData.context.documents,
-        context.documents,
-      );
-    }
-
-    contextToInsert = await this.__copyFileAndCreate(contextToInsert, entityId);
-
     const entityConnect = {
       [entityType]: {
         connect: { id: entityId },
       },
     };
 
-    let workflowRuntimeData: WorkflowRuntimeData;
+    let workflowRuntimeData: WorkflowRuntimeData, newWorkflowCreated: boolean;
 
-    if (!existingWorkflowRuntimeData) {
+    if (!existingWorkflowRuntimeData || config?.allowMultipleActiveWorkflows) {
+      contextToInsert = await this.__copyFileAndCreate(contextToInsert, entityId);
       workflowRuntimeData = await this.workflowRuntimeDataRepository.create({
         data: {
           ...entityConnect,
@@ -659,7 +652,14 @@ export class WorkflowService {
           },
         },
       });
+      newWorkflowCreated = true;
     } else {
+      contextToInsert.documents = updateDocuments(
+        existingWorkflowRuntimeData.context.documents,
+        context.documents,
+      );
+
+      contextToInsert = await this.__copyFileAndCreate(contextToInsert, entityId);
       workflowRuntimeData = await this.workflowRuntimeDataRepository.updateById(
         existingWorkflowRuntimeData.id,
         {
@@ -673,12 +673,14 @@ export class WorkflowService {
           },
         },
       );
+      newWorkflowCreated = false;
     }
 
     this.logger.log(existingWorkflowRuntimeData ? 'Workflow updated' : 'Workflow created', {
       workflowRuntimeDataId: workflowRuntimeData.id,
       entityId,
       entityType,
+      newWorkflowCreated,
     });
 
     return [
@@ -689,6 +691,7 @@ export class WorkflowService {
       },
     ];
   }
+
   private async __copyFileAndCreate(
     context: DefaultContextSchema,
     entityId: TEntityId,
