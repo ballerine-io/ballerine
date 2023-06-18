dd87e6e77bbae45653572362f29cd35189262a48
blokh <danielblokhi@gmail.com>
2023-06-18 17:43:13 +0300

    added trust proxy key to app

--- services/workflows-websocket-service/src/main.ts
+++ services/workflows-websocket-service/src/main.ts
@@ -6,7 +6,6 @@ import { env } from '@/env';
 async function main() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
-  app.set('trust proxy', 1);
   void app.listen(env.PORT);
   console.log(`Listening on port ${env.PORT}`);
 
