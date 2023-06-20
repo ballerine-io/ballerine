b6901fe1ef4fc7fd4541b59c1d082323c01602e2
Omri Levy <omri.levy0197@gmail.com>
2023-06-20 17:29:06 +0300

    fix(workflows-service): added missing nestjs module dependencies

--- services/workflows-service/src/business/business.module.ts
+++ services/workflows-service/src/business/business.module.ts
@@ -5,16 +5,30 @@ import { BusinessControllerInternal } from './business.controller.internal';
 import { BusinessControllerExternal } from './business.controller.external';
 import { FilterService } from '@/filter/filter.service';
 import { FilterRepository } from '@/filter/filter.repository';
+import { WorkflowDefinitionRepository } from '@/workflow/workflow-definition.repository';
+import { WorkflowRuntimeDataRepository } from '@/workflow/workflow-runtime-data.repository';
 import { WorkflowService } from '@/workflow/workflow.service';
+import { FileRepository } from '@/storage/storage.repository';
+import { FileService } from '@/providers/file/file.service';
+import { StorageService } from '@/storage/storage.service';
+import { WorkflowEventEmitterService } from '@/workflow/workflow-event-emitter.service';
+import { EndUserRepository } from '@/end-user/end-user.repository';
 
 @Module({
   controllers: [BusinessControllerInternal, BusinessControllerExternal],
   providers: [
-    WorkflowService,
     BusinessRepository,
     BusinessService,
     FilterRepository,
     FilterService,
+    FileRepository,
+    FileService,
+    StorageService,
+    EndUserRepository,
+    WorkflowEventEmitterService,
+    WorkflowDefinitionRepository,
+    WorkflowRuntimeDataRepository,
+    WorkflowService,
   ],
 })
 export class BusinessModule {}
--- services/workflows-service/src/end-user/end-user.module.ts
+++ services/workflows-service/src/end-user/end-user.module.ts
@@ -5,10 +5,30 @@ import { EndUserRepository } from './end-user.repository';
 import { EndUserService } from './end-user.service';
 import { FilterService } from '@/filter/filter.service';
 import { FilterRepository } from '@/filter/filter.repository';
+import { WorkflowDefinitionRepository } from '@/workflow/workflow-definition.repository';
+import { WorkflowRuntimeDataRepository } from '@/workflow/workflow-runtime-data.repository';
 import { WorkflowService } from '@/workflow/workflow.service';
+import { BusinessRepository } from '@/business/business.repository';
+import { StorageService } from '@/storage/storage.service';
+import { FileService } from '@/providers/file/file.service';
+import { FileRepository } from '@/storage/storage.repository';
+import { WorkflowEventEmitterService } from '@/workflow/workflow-event-emitter.service';
 
 @Module({
   controllers: [EndUserControllerInternal, EndUserControllerExternal],
-  providers: [WorkflowService, EndUserRepository, EndUserService, FilterService, FilterRepository],
+  providers: [
+    EndUserRepository,
+    EndUserService,
+    FilterService,
+    FilterRepository,
+    FileRepository,
+    FileService,
+    StorageService,
+    WorkflowEventEmitterService,
+    BusinessRepository,
+    WorkflowDefinitionRepository,
+    WorkflowRuntimeDataRepository,
+    WorkflowService,
+  ],
 })
 export class EndUserModule {}
