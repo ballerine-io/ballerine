6b200f9a5d7ccb3da9070266ff86a4e39c6968f9
Alon Peretz <Alonp99@gmail.com>
2023-06-21 22:21:45 +0300

    strucute changes

--- websites/docs/src/config.ts
+++ websites/docs/src/config.ts
@@ -65,21 +65,32 @@ export const SIDEBAR: Sidebar = {
     learn: [
       {
         group: `Getting started`,
-        sections: [{ text: `UI Flows`, link: `ui-flows` }],
-      },
-      {
-        group: `Start Here`,
         sections: [
           { text: `Introduction`, link: `introduction` },
-          {
-            text: `Style Guidelines`,
-            link: `style-guidelines`,
-          },
-          { text: `Contributing`, link: `contributing` },
+          { text: `Gloassry`, link: `ui-flows` },
+          { text: `Install`, link: `ui-flows` },
+          { text: `Deploy`, link: `ui-flows` },
         ],
       },
       {
-        group: `Basics`,
+        group: `Guides`,
+        sections: [
+          {
+            text: `Creating a KYC flow and deploying it`,
+            link: `creating-a-kyc-flow-and-deploying-it`,
+          },
+        ],
+      },
+      {
+        group: `Workflows`,
+        sections: [{ text: `UI Flows`, link: `ui-flows` }],
+      },
+      {
+        group: `Case Managment`,
+        sections: [{ text: `UI Flows`, link: `ui-flows` }],
+      },
+      {
+        group: `UI SDK's`,
         sections: [
           { text: `SDK Events`, link: `sdk-events` },
           { text: `SDK Backend Configuration`, link: `sdk-backend-configuration` },
@@ -97,15 +108,21 @@ export const SIDEBAR: Sidebar = {
           },
         ],
       },
+    
+     
+    
       {
-        group: `Guides`,
+        group: `Contributing`,
         sections: [
           {
-            text: `Creating a KYC flow and deploying it`,
-            link: `creating-a-kyc-flow-and-deploying-it`,
+            text: `Style Guidelines`,
+            link: `style-guidelines`,
           },
+          { text: `Contributing`, link: `contributing` },
         ],
       },
+    
+   
       {
         group: `Examples`,
         sections: [
