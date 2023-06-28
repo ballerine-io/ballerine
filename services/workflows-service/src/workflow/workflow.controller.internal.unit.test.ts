f81feec02bc93c370a5c714ff6a1f104d5507d20
teselil <tzlil@datree.com>
2023-06-28 18:10:16 +0300

    test: fix

--- services/workflows-service/src/workflow/workflow.controller.internal.unit.test.ts
+++ services/workflows-service/src/workflow/workflow.controller.internal.unit.test.ts
@@ -14,6 +14,7 @@ import { BusinessModel } from '@/business/business.model';
 import { AppLoggerService } from '@/common/app-logger/app-logger.service';
 import { commonTestingModules } from '@/test/helpers/nest-app-helper';
 import { Test, TestingModule } from '@nestjs/testing';
+import axios from 'axios';
 
 class FakeWorkflowRuntimeDataRepo extends BaseFakeRepository {
   constructor() {
@@ -121,6 +122,7 @@ describe('WorkflowControllerInternal', () => {
             numb: 'context',
           },
         };
+        axios.get = jest.fn().mockResolvedValue({});
         await workflowRuntimeDataRepo.create({
           data: initialRuntimeData,
         });
