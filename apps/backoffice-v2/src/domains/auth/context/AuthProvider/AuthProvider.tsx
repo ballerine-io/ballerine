cf21689089d24b07f80d16a2afb6aedc53be7606
Omri Levy <omri.levy0197@gmail.com>
2023-07-13 21:34:00 +0300

    feat(authprovider): added a full screen loader

--- apps/backoffice-v2/src/domains/auth/context/AuthProvider/AuthProvider.tsx
+++ apps/backoffice-v2/src/domains/auth/context/AuthProvider/AuthProvider.tsx
@@ -1,7 +1,8 @@
 import { FunctionComponentWithChildren } from '../../../../common/types';
-import { createContext, useMemo } from 'react';
+import React, { createContext, useMemo } from 'react';
 import { env } from '../../../../common/env/env';
 import { useAuthenticatedUserQuery } from '../../hooks/queries/useAuthenticatedUserQuery/useAuthenticatedUserQuery';
+import { FullScreenLoader } from '../../../../common/components/molecules/FullScreenLoader/FullScreenLoader';
 
 export const AuthContext = createContext<{
   redirectAuthenticatedTo?: string;
@@ -46,7 +47,7 @@ export const AuthProvider: FunctionComponentWithChildren<{
   );
 
   // Don't render the children to avoid a flash of wrong state (i.e. authenticated layout).
-  if (isLoading && env.VITE_AUTH_ENABLED) return null;
+  if (isLoading && env.VITE_AUTH_ENABLED) return <FullScreenLoader />;
 
   return <AuthContext.Provider value={contextValues}>{children}</AuthContext.Provider>;
 };
