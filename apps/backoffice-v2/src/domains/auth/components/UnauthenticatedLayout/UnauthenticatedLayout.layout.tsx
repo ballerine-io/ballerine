0cd2be204eedcd9b2769d245d0767ab0793e5a73
Omri Levy <61207713+Omri-Levy@users.noreply.github.com>
2023-07-16 10:30:50 +0300

    Authentication layouts using react-router-dom (#617)
    * refactor(backoffice-v2): no longer using useEffect to redirect based on auth state
    
    now using <Navigate/> and navigating to the last page of the user
    
    * refactor(shouldredirect): wrapped in useMemo (PR comment)
    
    * feat(authprovider): added a full screen loader
    
    * fix(useselectentityfilteronmount): updated dependency array
    
    * feat(backoffice-v2): now returning full screen loader instead of null

--- apps/backoffice-v2/src/domains/auth/components/UnauthenticatedLayout/UnauthenticatedLayout.layout.tsx
+++ apps/backoffice-v2/src/domains/auth/components/UnauthenticatedLayout/UnauthenticatedLayout.layout.tsx
@@ -1,13 +1,29 @@
-import { FunctionComponentWithChildren } from '../../../../common/types';
+import { useUnauthenticatedLayoutLogic } from './hooks/useUnauthenticatedLayoutLogic/useUnauthenticatedLayoutLogic';
+import { Navigate, Outlet } from 'react-router-dom';
+import { FunctionComponent } from 'react';
+import { FullScreenLoader } from '../../../../common/components/molecules/FullScreenLoader/FullScreenLoader';
 
-export const UnauthenticatedLayout: FunctionComponentWithChildren = ({ children }) => {
-  // Should only be uncommented once `useAuthRedirects` is no longer in use in `AuthProvider`
-  // useUnauthenticatedLayout();
+export const UnauthenticatedLayout: FunctionComponent = () => {
+  const { isLoading, shouldRedirect, redirectAuthenticatedTo, state } =
+    useUnauthenticatedLayoutLogic();
+
+  if (isLoading) return <FullScreenLoader />;
+
+  if (shouldRedirect) {
+    return (
+      <Navigate
+        to={redirectAuthenticatedTo}
+        replace
+        state={{
+          from: state?.from,
+        }}
+      />
+    );
+  }
 
   return (
     <main className={`h-full`}>
-      {/*<Outlet />*/}
-      {children}
+      <Outlet />
     </main>
   );
 };
