0c5693f754445c19f332efe6cde12da100922e9c
Illia Rudniev <cheskmr@gmail.com>
2023-07-17 22:38:22 +0300

    Illia rudniev/fix/active agents stats (#697)
    * fix: agents stats now fetching all available users & updated endpoints & ui
    
    * feat: added favicon

--- apps/workflows-dashboard/src/domains/workflows/api/users/users.api.ts
+++ apps/workflows-dashboard/src/domains/workflows/api/users/users.api.ts
@@ -3,7 +3,7 @@ import { GetActiveUsersDto } from '@app/domains/workflows/api/users/users.types'
 import { request } from '@app/lib/request';
 
 export const fetchActiveUsers = async (query: GetActiveUsersDto): Promise<IUser[]> => {
-  const result = await request.get<IUser[]>('/metrics/users/active-users', { params: query });
+  const result = await request.get<IUser[]>('/metrics/users', { params: query });
 
   return result.data;
 };
