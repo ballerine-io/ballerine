dca67f31ea9c27f1292e8da6d1402dd0744cb2b5
Illia Rudniev <cheskmr@gmail.com>
2023-07-17 18:53:39 +0000

    feat: added start of workflow after creation & fixed workflow-core map issue

--- examples/kyb-app/src/domains/workflows/workflows.api.ts
+++ examples/kyb-app/src/domains/workflows/workflows.api.ts
@@ -1,10 +1,18 @@
 import { request } from '@app/common/utils/request';
 import { RunWorkflowDto } from '@app/domains/workflows/types';
 
-export const runWorkflowRequset = async (dto: RunWorkflowDto): Promise<void> => {
-  await request.post('external/workflows/run', {
+export const runAndStartWorkflowRequest = async (dto: RunWorkflowDto): Promise<void> => {
+  const runResult = await request
+    .post('external/workflows/run', {
+      json: {
+        ...dto,
+      },
+    })
+    .json<{ workflowRuntimeId: string }>();
+
+  await request.post(`external/workflows/${runResult.workflowRuntimeId}/send-event`, {
     json: {
-      ...dto,
+      name: 'start',
     },
   });
 };
