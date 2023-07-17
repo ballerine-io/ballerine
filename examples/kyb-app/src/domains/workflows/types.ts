c5da07d12545171b6fc5f5a424cfab0dcad9aa9d
Alon Peretz <Alonp99@gmail.com>
2023-07-18 01:34:56 +0300

    collection flow data

--- examples/kyb-app/src/domains/workflows/types.ts
+++ examples/kyb-app/src/domains/workflows/types.ts
@@ -1,14 +1,43 @@
-import { AnyObject } from '@ballerine/ui';
-
 export interface RunWorkflowDto {
   workflowId: string;
   context: {
     entity: {
-      id: string;
+      id?: string;
       endUserId: string;
       ballerineEntityId: string;
       type: 'business';
-      data: AnyObject;
+      data: {
+        website: string;
+        correlationId?: string;
+        companyName: string;
+        address: {
+          street?: string;
+          postalCode?: string;
+          city?: string;
+          countryCode?: string;
+          country?: string;
+          text?: string;
+        };
+        registrationNumber: string;
+        additionalInfo?: {
+          ubos: [
+            {
+              entity: {
+                type: string; //'individual';
+                data: {
+                  firstName: string;
+                  lastName: string;
+                  email: string;
+                  additionalInfo: {
+                    companyName: string;
+                    customerCompany?: string;
+                  };
+                };
+              };
+            },
+          ];
+        };
+      };
     };
   };
 }
