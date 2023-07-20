0984e944acd2bb65b560862401bdb4a56e43deac
blokh <danielblokhi@gmail.com>
2023-07-20 11:32:52 +0300

    updated parent complete rule

--- services/workflows-service/scripts/workflows/parent-kyb-kyc-session-workflow.ts
+++ services/workflows-service/scripts/workflows/parent-kyb-kyc-session-workflow.ts
@@ -41,7 +41,7 @@ export const parentKybWithSessionWorkflowDefinition = {
               cond: {
                 type: 'jmespath',
                 options: {
-                  rule: 'length(childWorkflows.kyc_email_session_example.*.[vendorResult][]) == length(childWorkflows.kyc_email_session_example.*[])',
+                  rule: 'length(childWorkflows.kyc_email_session_example.*.[result.vendorResult][]) == length(childWorkflows.kyc_email_session_example.*[])',
                 },
               },
             },
