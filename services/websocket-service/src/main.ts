07e44234e79d7956ac1bba3e7b4e61211a1e6629
teselil <tzlil@datree.com>
2023-07-28 16:06:31 +0300

    feat: add validation on the notification type

--- services/websocket-service/src/main.ts
+++ services/websocket-service/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { env } from '@/env';
+import { ValidationPipe } from '@nestjs/common';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.useGlobalPipes(new ValidationPipe());
   void app.listen(env.PORT);
   console.log(`Listening on port ${env.PORT}`);
 
--- services/websocket-service/src/notifications/notify.controller.ts
+++ services/websocket-service/src/notifications/notify.controller.ts
@@ -1,14 +1,28 @@
 import { Controller, Post, Query } from '@nestjs/common';
 import { AppGateway } from '@/app.gateway';
+import { IsDefined, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+enum NotificationType {
+  FILTERS = 'filters',
+  WORKFLOWS = 'workflow',
+  WORKFLOWS_LIST = 'workflows_list',
+}
+
+export class NotifyParams {
+  @IsString()
+  @IsEnum(NotificationType)
+  @IsDefined()
+  @IsNotEmpty()
+  type: NotificationType;
+}
 
 @Controller('notify')
 export class NotifyController {
   constructor(private readonly appGateway: AppGateway) {}
 
   @Post()
-  // todo make it an enum
-  notifyAllConnectedWebsockets(@Query('type') notificationType: string): string {
-    this.appGateway.broadcast('notify', notificationType);
+  notifyAllConnectedWebsockets(@Query() notifyParams: NotifyParams): string {
+    this.appGateway.broadcast('notify', notifyParams.type);
     return '200';
   }
 }
