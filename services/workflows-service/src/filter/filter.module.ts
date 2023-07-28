89bd6e2505f8433b13e20aad36306eb2257d46e8
teselil <tzlil@datree.com>
2023-07-28 15:17:57 +0300

    fix: add provider

--- services/workflows-service/src/filter/filter.module.ts
+++ services/workflows-service/src/filter/filter.module.ts
@@ -3,9 +3,10 @@ import { FilterControllerInternal } from '@/filter/filter.controller.internal';
 import { FilterControllerExternal } from '@/filter/filter.controller.external';
 import { FilterRepository } from '@/filter/filter.repository';
 import { FilterService } from '@/filter/filter.service';
+import { WebsocketNotifierService } from '@/common/websocket/notifier/websocket-notifier.service';
 
 @Module({
   controllers: [FilterControllerInternal, FilterControllerExternal],
-  providers: [FilterRepository, FilterService],
+  providers: [FilterRepository, FilterService, WebsocketNotifierService],
 })
 export class FilterModule {}
