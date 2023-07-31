d73386adc59c7f3bb046602aba9619f03abf9f5d
Omri Levy <omri.levy0197@gmail.com>
2023-07-31 16:00:58 +0300

    feat(workflows-service): added a webhook for workflow completed

--- services/workflows-service/src/events/types.ts
+++ services/workflows-service/src/events/types.ts
@@ -1,3 +1,22 @@
+import { TEventName } from '@/workflow/types';
+import { WorkflowRuntimeData } from '@prisma/client';
+
 export interface ListenerFn {
   (...values: any[]): void;
+}
+
+export interface IWebhookPayload {
+  id: string;
+  eventName: TEventName;
+  apiVersion: string;
+  // ISO string
+  timestamp: string;
+  workflowCreatedAt: WorkflowRuntimeData['createdAt'];
+  workflowResolvedAt: WorkflowRuntimeData['resolvedAt'];
+  workflowDefinitionId: string;
+  workflowRuntimeId: string;
+  ballerineEntityId: string;
+  correlationId: string;
+  environment: string | undefined;
+  data: unknown;
 }
