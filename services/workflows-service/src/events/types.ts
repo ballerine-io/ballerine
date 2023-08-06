ce73e41296747e9bbfce0bb4c967b454e0a837c4
Omri Levy <61207713+Omri-Levy@users.noreply.github.com>
2023-08-06 12:54:00 +0300

    KYB MOMO (#760)
    * feat(workflows-service): added business type to the business entity
    
    * feat(schema.prisma): added index to business type
    
    * fix(seed.ts): fixed bad momo filters
    
    * fix(document schemas): fixed wrong companyName fields
    
    * feat(workflows-service): added a webhook for workflow completed
    
    * chore(seed.ts): fixes for pr comments
    
    * fix(seed.ts): remaining pr comments

--- services/workflows-service/src/events/types.ts
+++ services/workflows-service/src/events/types.ts
@@ -1,3 +1,22 @@
+import { WorkflowRuntimeData } from '@prisma/client';
+import { TEventName } from '@/workflow/types';
+
 export interface ListenerFn {
   (...values: any[]): void;
+}
+
+export interface IWebhookPayload {
+  id: string;
+  eventName: TEventName;
+  apiVersion: string;
+  // ISO string
+  timestamp: string;
+  workflowCreatedAt: WorkflowRuntimeData['createdAt'];
+  workflowResolvedAt: WorkflowRuntimeData['resolvedAt'];
+  workflowDefinitionId: string;
+  workflowRuntimeId: string;
+  ballerineEntityId: string;
+  correlationId: string;
+  environment: string | undefined;
+  data: unknown;
 }
