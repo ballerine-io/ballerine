d770a259c5b7a58d225de88b37c4f45eb6670ad0
Tomer Shvadron <tomer94@gmail.com>
2023-08-09 17:22:14 +0300

    style(map component): changed alert component to skeleton

--- apps/backoffice-v2/src/pages/Entity/components/MapCell/MapCell.tsx
+++ apps/backoffice-v2/src/pages/Entity/components/MapCell/MapCell.tsx
@@ -4,7 +4,7 @@ import { useNominatimQuery } from './hooks/useNominatimQuery/useNominatimQuery';
 import { IMapCellProps } from './interfaces';
 import { Map } from '../../../../common/components/molecules/Map/Map';
 import { ErrorAlert } from '../../../../common/components/atoms/ErrorAlert/ErrorAlert';
-import { Alert } from '../../../../common/components/atoms/Alert/Alert';
+import { Skeleton } from '../../../../common/components/atoms/Skeleton/Skeleton';
 
 export const MapCell: FunctionComponent<IMapCellProps> = ({ value }) => {
   const { data, isLoading, isError } = useNominatimQuery(value);
@@ -12,7 +12,7 @@ export const MapCell: FunctionComponent<IMapCellProps> = ({ value }) => {
   const className = 'mt-6 h-[600px] w-[600px] rounded-md';
 
   if (isLoading) {
-    return <Alert className={className}>Loading map...</Alert>;
+    return <Skeleton className={className} />;
   }
 
   if (isError || !data?.[0]?.lat || !data?.[0]?.lon) {
