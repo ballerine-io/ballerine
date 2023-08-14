012303367e45ec731b388e606b3641227997c181
Illia Rudniev <cheskmr@gmail.com>
2023-08-14 09:51:35 +0000

    chore: removed unused code

--- examples/kyb-app/src/common/components/organisms/DynamicForm/components/atoms/TextInput/types.ts
+++
@@ -1,1 +0,0 @@
-export type TextInputFieldType = 'text' | 'select';
--- examples/kyb-app/src/common/components/organisms/DynamicForm/hooks/useWarnings/utils/convertFieldIdToObjectPath.ts
+++
@@ -1,3 +0,0 @@
-export const convertFieldIdToObjectPath = (id: string) => {
-  return id.replace('root_', '').replaceAll('_', '.');
-};
