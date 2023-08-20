038cf6fd053597fd624763fc1bef09d686d94ce6
Daniel Blokh <35891501+Blokh@users.noreply.github.com>
2023-08-20 18:00:51 +0300

    added user scoping (#871)
    * added user scoping
    
    * added scoping customers

--- services/workflows-service/src/user/user.controller.internal.ts
+++ services/workflows-service/src/user/user.controller.internal.ts
@@ -6,6 +6,8 @@ import { UserCreateDto } from '@/user/dtos/user-create';
 import { Prisma } from '@prisma/client';
 import { AdminAuthGuard } from '@/common/guards/admin-auth.guard';
 import { UseGuards } from '@nestjs/common';
+import { ProjectIds } from '@/common/decorators/project-ids.decorator';
+import { TProjectIds } from '@/types';
 
 @swagger.ApiTags('internal/users')
 @common.Controller('internal/users')
@@ -15,18 +17,21 @@ export class UserControllerInternal {
   @common.Get()
   @swagger.ApiOkResponse({ type: [UserModel] })
   @swagger.ApiForbiddenResponse()
-  async list(): Promise<UserModel[]> {
-    return this.service.list({
-      select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true,
-        phone: true,
-        updatedAt: true,
-        createdAt: true,
+  async list(@ProjectIds() projectIds: TProjectIds): Promise<UserModel[]> {
+    return this.service.list(
+      {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          phone: true,
+          updatedAt: true,
+          createdAt: true,
+        },
       },
-    });
+      projectIds,
+    );
   }
 
   @common.Post()
