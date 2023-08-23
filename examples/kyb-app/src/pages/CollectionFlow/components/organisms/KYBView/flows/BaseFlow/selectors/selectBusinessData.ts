8c879b8e166198285d46ad5c47fbf39b2e7cf69f
blokh <danielblokhi@gmail.com>
2023-08-23 15:24:25 +0300

    added get full country name by code

--- examples/kyb-app/src/pages/CollectionFlow/components/organisms/KYBView/flows/BaseFlow/selectors/selectBusinessData.ts
+++ examples/kyb-app/src/pages/CollectionFlow/components/organisms/KYBView/flows/BaseFlow/selectors/selectBusinessData.ts
@@ -3,6 +3,9 @@ import { WorkflowFlowData } from '@app/domains/workflows/flow-data.type';
 import { buildCompanyAddress } from '@app/pages/CollectionFlow/components/organisms/KYBView/flows/BaseFlow/helpers/serialize-business-data';
 import { traverseObjectAndPickValue } from '@app/utils/traverse-object-and-pick-value';
 import dayjs from 'dayjs';
+import {
+  getFullCountryNameByCode
+} from "@app/pages/CollectionFlow/components/organisms/KYBView/helpers/get-countries-list.ts";
 
 export const selectBusinessData = ({ flowData }: WorkflowFlowData, user: TUser): BusinessData => {
   const dateOfIncorporation = traverseObjectAndPickValue('registrationDate', flowData, null);
@@ -14,7 +17,7 @@ export const selectBusinessData = ({ flowData }: WorkflowFlowData, user: TUser):
     companyName: traverseObjectAndPickValue('companyName', flowData, ''),
     registrationNumber: traverseObjectAndPickValue('registrationNumber', flowData, ''),
     country: '',
-    countryOfIncorporation: traverseObjectAndPickValue('companyCountry', flowData, ''),
+    countryOfIncorporation: getFullCountryNameByCode(traverseObjectAndPickValue('companyCountry', flowData, '')),
     dateOfIncorporation: dateOfIncorporation
       ? dayjs(dateOfIncorporation).toISOString()
       : dateOfIncorporation,
