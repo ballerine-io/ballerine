9de8833b25564da069956039d1b6f0c009d2fb77
Matan Yadaev <matan.yed@gmail.com>
2023-08-24 16:32:23 +0300

    fix: queries

--- services/workflows-service/src/metrics/repository/sql/aggregate-average-assignment-time.sql.ts
+++ services/workflows-service/src/metrics/repository/sql/aggregate-average-assignment-time.sql.ts
@@ -2,16 +2,12 @@ export const aggregateAverageAssignmentTimeQuery = `
 SELECT AVG(time)::varchar as average_time
 FROM (
   SELECT
-    "User".id,
     COALESCE(
       EXTRACT(EPOCH FROM ("WorkflowRuntimeData"."assignedAt" - "WorkflowRuntimeData"."createdAt")) * 1000,
       0
     ) AS time
   FROM
-    "User"
-  LEFT JOIN
     "WorkflowRuntimeData"
-    ON "User".id = "WorkflowRuntimeData"."assigneeId"
   WHERE
     "WorkflowRuntimeData"."createdAt" IS NOT NULL
     AND
