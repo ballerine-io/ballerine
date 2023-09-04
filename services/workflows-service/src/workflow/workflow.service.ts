23be34971a1fea909a385d692e0f25982bd44945
Daniel Blokh <35891501+Blokh@users.noreply.github.com>
2023-09-04 15:28:26 +0300

    Blokh/feat/workflow definition mt (#1030)
    * feat: added feinition for specific customer
    
    * feat: added workflow defintiion clone
    
    * fixed build
    
    * updated isTemplate to isPublic
    
    * removed unnecessary selects
    
    * fix: fix internal controller test
    
    * ran format
    
    * updated prisma migrations
    
    * workflowDefinitionRepository.create

--- services/workflows-service/src/workflow/workflow.service.ts
+++ services/workflows-service/src/workflow/workflow.service.ts
@@ -176,8 +176,8 @@ export class WorkflowService {
     );
 
     return await this.workflowDefinitionRepository.create(
-      // @ts-expect-error - types of workflow definition does not propagate to the prisma creation type
       {
+        // @ts-expect-error - types of workflow definition does not propagate to the prisma creation type
         data: { ...workflowDefinition, name: data.name, projectId: projectId, isPublic: false },
         select,
       },
