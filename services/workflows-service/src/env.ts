e9b17311e32349705c7a0b70223583e8e432e6ce
Matan Yadaev <matan.yed@gmail.com>
2023-09-05 10:44:43 +0300

    Salesforce authentication (#1025)
    * feat: schema changes
    
    * feat: new env variables
    
    * feat: auth with salesforce
    
    * fix: default env
    
    * fix: improve error messages

--- services/workflows-service/src/env.ts
+++ services/workflows-service/src/env.ts
@@ -50,6 +50,14 @@ export const env = createEnv({
       .string()
       .optional()
       .describe('Shared secret for the Unified API. Used for verifying incoming callbacks.'),
+    SALESFORCE_API_VERSION: z
+      .string()
+      .optional()
+      .default('58.0')
+      .describe('Salesforce API version'),
+    SALESFORCE_CONSUMER_KEY: z.string().optional().describe('Salesforce consumer key'),
+    SALESFORCE_CONSUMER_SECRET: z.string().optional().describe('Salesforce consumer secret'),
+    APP_API_URL: z.string().url().describe('The URL of the workflows-service API'),
   },
   client: {},
   /**
