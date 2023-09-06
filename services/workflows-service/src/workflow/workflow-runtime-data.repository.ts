162f81b9ab2caf04730a5fe851c2705203e9902e
Omri Levy <61207713+Omri-Levy@users.noreply.github.com>
2023-09-06 19:11:24 +0300

    Omri levy/fix/cloudfront content (#1050)
    * feat(db): added a type field to the file model
    
    * refactor(workflows-service): moved all file related logic to the file service
    
    now checking for mime type using 'file-type'
    
    * refactor(*): now using file type from the 'file-type' package
    
    * feat(backoffice-v2): updated no tasks found svg
    
    * refactor(workflows-service): renamed instances of to/from to source/target
    
    * refactor(workflows-service): removed instances of the word 'file' from the file service interface
    
    * feat(*): pr comments changes
    
    * refactor(workflows-service): changed instance of type 'png' to type 'image/png' to match mime type
    
    * fix(*): changed instances of old document page type
    
    * refactor(*): updated logic to handle backwards compatibility and handle base64
    
    * revert(*): reverted test vendor
    
    * revert(*): reverted schema json changes
    
    * fix(workflows-service): fixed cycle import from types
    
    * fix(packages/ui): removed redundant components.ts file
    
    * refactor(backoffice-v2): tags are now also optional
    
    * feat: merged with illya mt branch
    
    * refactor(workflows-service): no longer using findFirstOrThrow
    
    * fix(workflows-service): now checking if input is unchecked and connecting project accordingly
    
    * feat(backoffice-v2): added an iframe component with default security related attributes
    
    * feat(workflows-service): now generating a file extension for cloudfront/s3 directory name
    
    * fix(seed.ts): fixed customer name
    
    * refactor(*): changed vendor from veriff to open-corporates
    
    * fix(workflows-service): fixed naming from merge
    
    * fix(*): fixed bad merge changes
    
    * revert(*): reverted auto import change
    
    * fix(*): fixed lockfile
    
    ---------
    
    Co-authored-by: blokh <danielblokhi@gmail.com>
    Co-authored-by: Daniel Blokh <35891501+Blokh@users.noreply.github.com>

--- services/workflows-service/src/workflow/workflow-runtime-data.repository.ts
+++ services/workflows-service/src/workflow/workflow-runtime-data.repository.ts
@@ -23,17 +23,21 @@ export class WorkflowRuntimeDataRepository {
     args: Prisma.SelectSubset<T, Prisma.WorkflowRuntimeDataCreateArgs>,
     projectId: TProjectId,
   ): Promise<WorkflowRuntimeData> {
-    return await this.prisma.workflowRuntimeData.create<T>({
-      ...args,
-      data: {
-        ...args.data,
-        context: {
-          ...((args.data?.context ?? {}) as any),
-          documents: assignIdToDocuments((args.data?.context as any)?.documents),
-        },
+    return await this.prisma.workflowRuntimeData.create<T>(
+      this.scopeService.scopeCreate(
+        {
+          ...args,
+          data: {
+            ...args.data,
+            context: {
+              ...((args.data?.context ?? {}) as any),
+              documents: assignIdToDocuments((args.data?.context as any)?.documents),
+            },
+          },
+        } as any,
         projectId,
-      },
-    } as any);
+      ),
+    );
   }
 
   async findMany<T extends Prisma.WorkflowRuntimeDataFindManyArgs>(
