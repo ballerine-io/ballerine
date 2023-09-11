06b9331c9e70c5f355b71b16ae69fcf3b3998161
Matan Yadaev <matan.yed@gmail.com>
2023-09-11 13:19:09 +0300

    feat: add `updateRecord` method

--- services/workflows-service/src/salesforce/salesforce.service.ts
+++ services/workflows-service/src/salesforce/salesforce.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { env } from '@/env';
 import { SalesforceIntegrationRepository } from '@/salesforce/salesforce-integration.repository';
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
 import { GetAccessTokenSchema, GetUserInfoSchema } from '@/salesforce/salesforce.schema';
 import {
   AUTHORIZE_URL,
@@ -45,6 +45,63 @@ export class SalesforceService {
       refreshToken,
       idUrl,
     });
+  }
+
+  public async refreshAccessToken(refreshToken: string) {
+    const response = await axios.post<unknown>(GET_ACCESS_TOKEN_URL, null, {
+      params: {
+        grant_type: 'refresh_token',
+        client_id: env.SALESFORCE_CONSUMER_KEY,
+        client_secret: env.SALESFORCE_CONSUMER_SECRET,
+        refresh_token: refreshToken,
+      },
+    });
+
+    return GetAccessTokenSchema.parse(response.data);
+  }
+
+  public async updateRecord({
+    projectId,
+    objectName,
+    recordId,
+    data,
+  }: {
+    projectId: string;
+    objectName: string;
+    recordId: string;
+    data: Record<string, unknown>;
+  }): Promise<void> {
+    const salesforceIntegration = await this.findByProjectId(projectId);
+
+    if (!salesforceIntegration) {
+      throw new Error('Salesforce integration not found');
+    }
+
+    const updateEndpoint = `${salesforceIntegration.instanceUrl}/services/data/v${env.SALESFORCE_API_VERSION}/sobjects/${objectName}/${recordId}`;
+
+    try {
+      await axios.patch(updateEndpoint, data, {
+        headers: {
+          Authorization: `Bearer ${salesforceIntegration.accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (error) {
+      if (isAxiosError(error) && error.response?.status === 401) {
+        const { accessToken, issuedAt } = await this.refreshAccessToken(
+          salesforceIntegration.refreshToken,
+        );
+
+        await this.salesforceIntegrationRepository.updateAccessToken(projectId, {
+          accessToken,
+          accessTokenIssuedAt: new Date(issuedAt),
+        });
+
+        return this.updateRecord({ projectId, objectName, recordId, data });
+      }
+
+      throw new Error(`Failed to update record in Salesforce`, { cause: error });
+    }
   }
 
   private async getAccessToken(code: string) {
