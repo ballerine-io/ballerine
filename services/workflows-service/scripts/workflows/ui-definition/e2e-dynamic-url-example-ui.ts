2a9f7947e3ef4ea883c7a10d4675e6059d78f25c
blokh <danielblokhi@gmail.com>
2023-09-12 23:22:06 +0300

    generate seed for workflow

---
+++ services/workflows-service/scripts/workflows/ui-definition/e2e-dynamic-url-example-ui.ts
@@ -0,0 +1,13 @@
+// import { Prisma, PrismaClient } from '@prisma/client';
+//
+// export const kybWithDynamicExternalRequestWorkflowExample =
+//   {} as const satisfies Prisma.UiDefinitionCreateInput;
+// export const generateDynamicDefinitionForE2eTest = async (
+//   prismaClient: PrismaClient,
+//   workflowDefintionId: string,
+//   projectId: string,
+// ) => {
+//   return await prismaClient.workflowDefinition.create({
+//     data: kybWithDynamicExternalRequestWorkflowExample,
+//   });
+// };
