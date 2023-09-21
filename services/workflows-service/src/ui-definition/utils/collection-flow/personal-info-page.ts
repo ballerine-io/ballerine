35fb5d6ea22cf53f17050ef092060c27d5c4dc95
blokh <danielblokhi@gmail.com>
2023-09-21 14:16:36 +0300

    generated defintion for ui defintion

--- services/workflows-service/src/ui-definition/utils/collection-flow/personal-info-page.ts
+++ services/workflows-service/src/ui-definition/utils/collection-flow/personal-info-page.ts
@@ -40,7 +40,7 @@ const personalInfoPage = {
           elements: [
             {
               type: 'input-text',
-              valueDestination: 'context.entity.data.additionalInfo.mainRepresentative.firstName',
+              valueDestination: 'entity.data.additionalInfo.mainRepresentative.firstName',
               option: {
                 label: 'Name',
                 hint: 'First Name',
@@ -48,7 +48,7 @@ const personalInfoPage = {
             },
             {
               type: 'input-text',
-              valueDestination: 'context.entity.data.additionalInfo.mainRepresentative.lastName',
+              valueDestination: 'entity.data.additionalInfo.mainRepresentative.lastName',
               option: {
                 hint: 'Last Name',
               },
@@ -56,7 +56,7 @@ const personalInfoPage = {
             {
               type: 'input-text',
               valueDestination:
-                'context.entity.data.additionalInfo.mainRepresentative.additionalInfo.jobTitle',
+                'entity.data.additionalInfo.mainRepresentative.additionalInfo.jobTitle',
               option: {
                 label: 'Title',
                 hint: 'CEO / Manager / Partner',
@@ -64,7 +64,7 @@ const personalInfoPage = {
             },
             {
               type: 'date',
-              valueDestination: 'context.entity.data.additionalInfo.mainRepresentative.dateOfBirth',
+              valueDestination: 'entity.data.additionalInfo.mainRepresentative.dateOfBirth',
               option: {
                 label: 'Date of Birth',
                 hint: 'DD/MM/YYYY',
@@ -72,7 +72,7 @@ const personalInfoPage = {
             },
             {
               type: 'international-phone-number',
-              valueDestination: 'context.entity.data.additionalInfo.mainRepresentative.phone',
+              valueDestination: 'entity.data.additionalInfo.mainRepresentative.phone',
               option: {
                 label: 'Phone number',
               },
@@ -83,6 +83,12 @@ const personalInfoPage = {
               uiDefinition: {
                 classNames: ['align-right', 'padding-top-10'],
               },
+              availableOn: [
+                {
+                  type: 'json-logic',
+                  value: ['entity.data.additionalInfo.mainRepresentative.phone'],
+                },
+              ],
               option: {
                 text: 'Continue',
               },
@@ -90,6 +96,91 @@ const personalInfoPage = {
           ],
         },
       ],
+      actions: [
+        {
+          dispatchOn: { type: { event: 'onClick', uiElementName: 'nextButton-page-1' } },
+          type: 'definitionPlugin',
+          pluginName: 'update_end_user',
+        },
+        {
+          dispatchOn: { type: { event: 'onClick', uiElementName: 'nextButton-page-1' } },
+          type: 'definitionEvent',
+          event: 'next',
+        },
+      ],
     },
   ],
+};
+
+const processDefintion = {
+  id: 'clickspay_collection_flow',
+  predictableActionArguments: true,
+  initial: 'personal_details',
+  context: {},
+  states: {
+    personal_details: {
+      on: {
+        next: 'company_information',
+      },
+    },
+    company_information: {
+      on: {
+        next: 'address_information',
+        previous: 'personal_details',
+      },
+    },
+    address_information: {
+      on: {
+        next: 'placeholder_1',
+        previous: 'company_information',
+      },
+    },
+  },
+  extensions: {
+    apiPlugins: [
+      {
+        name: 'update_end_user',
+        pluginKind: 'api',
+        url: `{VITE_API_URL}/api/v1/collection-flow/end-user`,
+        method: 'POST',
+        headers: { Authorization: 'Bearer {tokenId}' },
+        request: {
+          transform: [
+            {
+              transformer: 'jmespath',
+              mapping: `{
+              firstName: entity.data.additionalInfo.mainRepresentative.countryOfIncorporation,
+              lastName: entity.data.additionalInfo.mainRepresentative.registrationNumber,
+              additionalInfo: {title: entity.data.additionalInfo.mainRepresentative.additionalInfo.title},
+              phone: entity.data.additionalInfo.mainRepresentative.phone,
+              dateOfBirth: entity.data.additionalInfo.mainRepresentative.dateOfBirth
+              }`,
+            },
+          ],
+        },
+      },
+      {
+        name: 'update_runtime_data',
+        pluginKind: 'api',
+        url: `{VITE_API_URL}/api/v1/collection-flow/{tokenId}`,
+        method: 'PUT',
+        stateNames: ['company_information', 'address_information'],
+        headers: { Authorization: 'Bearer {tokenId}' },
+        request: {
+          transform: [
+            {
+              transformer: 'jmespath',
+              mapping: `{
+              flowId: {tokenId},
+              mainRepresentative: entity.data.additionalInfo.mainRepresentative,
+              documents: documents,
+              ubos: entity.data.additionalInfo.ubos,
+              businessData: entity.data,
+              }`,
+            },
+          ],
+        },
+      },
+    ],
+  },
 };
