0553a96446df1076cdd7163091de0273b2ea4925
Matan Yadaev <matan.yed@gmail.com>
2023-09-28 16:36:08 +0300

    fix: fix subscription schema

--- services/workflows-service/src/workflow/schemas/zod-schemas.ts
+++ services/workflows-service/src/workflow/schemas/zod-schemas.ts
@@ -1,19 +1,19 @@
 import { z } from 'zod';
 
-export const SubscriptionSchema = z
-  .object({
-    type: z.enum(['webhook', 'slack']),
-    url: z.string().url().optional(),
-    authorizationKey: z.string().optional(),
-    authorizationType: z.enum(['basic-auth', 'signature']).optional(),
-    events: z.array(z.string()),
-  })
-  .strict();
+export const SubscriptionSchema = z.discriminatedUnion('type', [
+  z
+    .object({
+      type: z.literal('webhook'),
+      url: z.string().url(),
+      events: z.array(z.string().nonempty()),
+    })
+    .strict(),
+]);
 
 export const ConfigSchema = z
   .object({
     isLegacyReject: z.boolean().optional(),
-    subscriptions: z.array(SubscriptionSchema).optional(),
+    subscriptions: z.array(SubscriptionSchema),
     completedWhenTasksResolved: z.boolean().optional(),
     workflowLevelResolution: z.boolean().optional(),
     allowMultipleActiveWorkflows: z.boolean().optional(),
