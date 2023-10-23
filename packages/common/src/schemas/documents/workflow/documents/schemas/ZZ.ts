4ec9766ea04b191c4136fc65c5c242b5d0123f35
blokh <danielblokhi@gmail.com>
2023-10-23 16:53:05 +0300

    updated sharedholders schema

--- packages/common/src/schemas/documents/workflow/documents/schemas/ZZ.ts
+++ packages/common/src/schemas/documents/workflow/documents/schemas/ZZ.ts
@@ -66,8 +66,8 @@ export const getUniversalDocuments = (): TDocument[] => {
       version: 1,
       propertiesSchema: Type.Object({
         businessName: Type.Optional(Type.String()),
-        directors: Type.Optional(Type.Array(Type.String())),
-        shareholders: Type.Optional(Type.Array(Type.String())),
+        directors: Type.Optional(Type.String()),
+        shareholders: Type.Optional(Type.String()),
         issueDate: OptionalTypePastDate,
       }),
     },
@@ -142,7 +142,7 @@ export const getUniversalDocuments = (): TDocument[] => {
         licenseNumber: OptionalTypeAlphanumericString,
         issueDate: OptionalTypePastDate,
         expiryDate: Type.Optional(Type.String({ format: 'date' })),
-        permittedProductsOrServices: Type.Optional(Type.Array(Type.String())),
+        permittedProductsOrServices: Type.Optional(Type.String()),
       }),
     },
   ];
