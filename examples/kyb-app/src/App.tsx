0d03b0191696456652d07fa2f432db69a1f20d73
Illia Rudniev <cheskmr@gmail.com>
2023-10-24 21:27:06 +0300

    feat: added message for invalid token & fixed handling of incorrect uid to throw 401 & seed fix (#1474)
    Co-authored-by: Alon Peretz <Alonp99@gmail.com>

--- examples/kyb-app/src/App.tsx
+++ examples/kyb-app/src/App.tsx
@@ -1,9 +1,5 @@
 import '@ballerine/ui/dist/style.css';
 import { AppLoadingContainer } from '@app/components/organisms/AppLoadingContainer';
-// import { RouterProvider } from 'react-router-dom';
-// import { router } from '@app/router';
-// import { CustomerProvider } from '@app/components/providers/CustomerProvider';
-// import { LoadingScreen } from '@app/common/components/molecules/LoadingScreen';
 import { useCustomerQuery } from '@app/hooks/useCustomerQuery';
 import { useUISchemasQuery } from '@app/hooks/useUISchemasQuery';
 import { useFlowContextQuery } from '@app/hooks/useFlowContextQuery';
--- examples/kyb-app/src/common/components/molecules/AppErrorScreen/AppErrorScreen.tsx
+++ examples/kyb-app/src/common/components/molecules/AppErrorScreen/AppErrorScreen.tsx
@@ -1,14 +1,14 @@
 interface Props {
   title?: string;
-  subtitle?: string;
+  description?: string | React.ReactNode;
   actionButton?: React.ReactNode;
 }
 
-export const AppErrorScreen = ({ title, subtitle, actionButton }: Props) => {
+export const AppErrorScreen = ({ title, description, actionButton }: Props) => {
   return (
     <div className="flex h-screen w-full flex-col gap-4 p-4">
       <h1 className="text-3xl font-bold">{title}</h1>
-      <h2 className="text-muted-foreground opacity-50">{subtitle}</h2>
+      <div>{description}</div>
       {actionButton ? <div className="flex ">{actionButton}</div> : null}
     </div>
   );
