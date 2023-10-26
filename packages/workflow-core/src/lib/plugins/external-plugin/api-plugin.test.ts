5ab121e8f910b4a8361b48cb36faf2eaab465d73
blokh <danielblokhi@gmail.com>
2023-10-26 21:29:24 +0300

    fixed tests over workflows

--- packages/workflow-core/src/lib/plugins/external-plugin/api-plugin.test.ts
+++ packages/workflow-core/src/lib/plugins/external-plugin/api-plugin.test.ts
@@ -68,8 +68,7 @@ describe('workflow-runner', () => {
     describe('when api plugin tranforms and makes a request to an external api', () => {
       const workflow = createWorkflowRunner(definition, apiPluginsSchemas);
       it('transitions to successAction and persist response to context', async () => {
-        // @ts-expect-error - `sendEvent` is not supposed to receive a string
-        await workflow.sendEvent('CHECK_BUSINESS_SCORE');
+        await workflow.sendEvent({type: 'CHECK_BUSINESS_SCORE'});
 
         expect(workflow.state).toEqual('checkBusinessScoreSuccess');
         expect(
@@ -103,8 +102,7 @@ describe('workflow-runner', () => {
       apiPluginsSchemasCopy[0]!.request.transform[0].mapping = 'dsa: .unknwonvalue.id}';
       const workflow = createWorkflowRunner(definition, apiPluginsSchemasCopy);
       it('returns error for transformation and transition to testManually', async () => {
-        // @ts-expect-error - `sendEvent` is not supposed to receive a string
-        await workflow.sendEvent('CHECK_BUSINESS_SCORE');
+        await workflow.sendEvent({type: 'CHECK_BUSINESS_SCORE'});
 
         expect(workflow.state).toEqual('testManually');
         expect(
@@ -142,8 +140,7 @@ describe('workflow-runner', () => {
         const workflow = createWorkflowRunner(definition, apiPluginsSchemasCopy);
 
         it('returns error for transformation and transition to testManually', async () => {
-          // @ts-expect-error - `sendEvent` is not supposed to receive a string
-          await workflow.sendEvent('CHECK_BUSINESS_SCORE');
+          await workflow.sendEvent({type: 'CHECK_BUSINESS_SCORE'});
 
           expect(workflow.state).toEqual('testManually');
           expect(
@@ -178,8 +175,7 @@ describe('workflow-runner', () => {
         const workflow = createWorkflowRunner(definition, apiPluginsSchemasCopy);
 
         it('transitions to successAction and persist success (response) to context', async () => {
-          // @ts-expect-error - `sendEvent` is not supposed to receive a string
-          await workflow.sendEvent('CHECK_BUSINESS_SCORE');
+          await workflow.sendEvent({type: 'CHECK_BUSINESS_SCORE'});
 
           expect(workflow.state).toEqual('checkBusinessScoreSuccess');
           expect(
