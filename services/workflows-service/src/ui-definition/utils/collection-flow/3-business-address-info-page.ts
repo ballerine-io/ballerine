db8136ab88f87cebab84d5ecdb78e5bd0d58157d
Illia Rudniev <cheskmr@gmail.com>
2023-10-30 20:44:19 +0200

    fix: renamed company to business address & removed logs (#1518)
    Co-authored-by: Alon Peretz <Alonp99@gmail.com>

--- services/workflows-service/src/ui-definition/utils/collection-flow/3-business-address-info-page.ts
+++ services/workflows-service/src/ui-definition/utils/collection-flow/3-business-address-info-page.ts
@@ -150,7 +150,7 @@ export const BusinessAddressInfoPage = {
   type: 'page',
   number: 3,
   stateName: 'business_address_information',
-  name: 'Company Address',
+  name: 'Business Address',
   pageValidation: [
     {
       type: 'json-schema',
@@ -167,7 +167,7 @@ export const BusinessAddressInfoPage = {
             {
               type: 'h1',
               options: {
-                text: 'Address',
+                text: 'Business Address',
               },
             },
             {
