1a4719c99c02918809f386ce180124870309de17
Lior Zamir <liorz@ballerine.com>
2023-11-02 10:58:17 +0200

    feat: replace NODE_ENV with ENVIRONMENT_NAME

--- services/workflows-service/src/common/decorators/use-key-auth-in-dev-guard.decorator.ts
+++ services/workflows-service/src/common/decorators/use-key-auth-in-dev-guard.decorator.ts
@@ -2,7 +2,7 @@ import { env } from '@/env';
 import { UseCustomerAuthGuard } from '@/common/decorators/use-customer-auth-guard.decorator';
 
 export const UseKeyAuthInDevGuard = () => {
-  if (env.ENVIRONMENT_NAME !== 'development' && env.ENVIRONMENT_NAME !== 'local') {
+  if (['development', 'local'].includes(env.ENVIRONMENT_NAME)) {
     return () => {
       return;
     };
