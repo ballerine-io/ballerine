ea27099b712ba883cb4c33b72c4db8945d9ead1c
Alon Peretz <Alonp99@gmail.com>
2023-11-04 23:54:04 +0200

    Add Config Param to Document Change Event in Workflows Service (#1557)
    * feat(workflows-service): add config parameter to document change events
    
    * fix(wf): CR fixes

--- services/workflows-service/src/workflow/workflow-event-emitter.service.ts
+++ services/workflows-service/src/workflow/workflow-event-emitter.service.ts
@@ -2,21 +2,31 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
 import { Injectable } from '@nestjs/common';
 import { ExtractWorkflowEventData, TEventName } from '@/workflow/types';
 
+export type EventConfig = {
+  forceEmit?: boolean;
+};
+
+const DEFAULT_CONFIG = { forceEmit: false };
 @Injectable()
 export class WorkflowEventEmitterService {
   constructor(private eventEmitter: EventEmitter2) {}
 
-  emit<TEvent extends TEventName>(eventName: TEvent, eventData: ExtractWorkflowEventData<TEvent>) {
+  emit<TEvent extends TEventName>(
+    eventName: TEvent,
+    eventData: ExtractWorkflowEventData<TEvent>,
+    config?: EventConfig,
+  ) {
+    config = { ...DEFAULT_CONFIG, ...(config || {}) };
     if (!eventName) {
       throw new Error('Event name is required');
     }
 
-    this.eventEmitter.emit(eventName, eventData);
+    this.eventEmitter.emit(eventName, eventData, config);
   }
 
   on<TEvent extends TEventName>(
     eventName: TEvent,
-    listener: (eventData: ExtractWorkflowEventData<TEvent>) => Promise<void>,
+    listener: (eventData: ExtractWorkflowEventData<TEvent>, config: EventConfig) => Promise<void>,
   ) {
     if (!eventName) {
       throw new Error('Event name is required');
