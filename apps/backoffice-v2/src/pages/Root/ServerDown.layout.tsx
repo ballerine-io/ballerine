42a628a84d147871dc638b36bd47597b30c8b27f
Omri Levy <omri.levy0197@gmail.com>
2023-11-08 14:43:37 +0200

    refactor(backoffice-v2): now displaying a loader for the server down layout when loading

--- apps/backoffice-v2/src/pages/Root/ServerDown.layout.tsx
+++ apps/backoffice-v2/src/pages/Root/ServerDown.layout.tsx
@@ -2,11 +2,13 @@ import { FunctionComponent } from 'react';
 import { useHealthQuery } from './hooks/useHealthQuery/useHealthQuery';
 import { Outlet } from 'react-router-dom';
 import { ErrorAlert } from '../../common/components/atoms/ErrorAlert/ErrorAlert';
+import { FullScreenLoader } from '../../common/components/molecules/FullScreenLoader/FullScreenLoader';
 
 export const ServerDownLayout: FunctionComponent = () => {
   const { isSuccess, isLoading } = useHealthQuery();
 
-  if (isSuccess || isLoading) return <Outlet />;
+  if (isLoading) return <FullScreenLoader />;
+  if (isSuccess) return <Outlet />;
 
   return (
     <main className={`flex h-full flex-col items-center`}>
