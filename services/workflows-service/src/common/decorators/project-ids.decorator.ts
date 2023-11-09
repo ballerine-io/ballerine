c321363f24fe7584a8b65f2eaee6293a7e6d51a5
blokh <danielblokhi@gmail.com>
2023-11-09 12:14:23 +0200

    added the option to query/inser to payload the relevant project Ids for each request

--- services/workflows-service/src/common/decorators/project-ids.decorator.ts
+++ services/workflows-service/src/common/decorators/project-ids.decorator.ts
@@ -1,6 +1,32 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { AuthenticatedEntityWithProjects } from '@/types';
+import { z } from 'zod';
 
+const RequestedProjectIdSchema = z.array(z.string()).optional();
 export const ProjectIds = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest(); // fetch payload project if present
-  return request.user?.projectIds;
+
+  const authenticatedEntity = request.user as AuthenticatedEntityWithProjects;
+  const userAssociatedProjectIds = authenticatedEntity.projectIds!;
+  const requestedProjectIds = RequestedProjectIdSchema.parse(request.query.projectIds);
+
+  const isExcludedInCustomerProjectIds =
+    !!requestedProjectIds &&
+    requestedProjectIds.length > 0 &&
+    !requestedProjectIds.some(projectId => !userAssociatedProjectIds.includes(projectId));
+  const notAdmin = !(authenticatedEntity.type === 'admin');
+
+  if (isExcludedInCustomerProjectIds && notAdmin) {
+    throw new UnauthorizedException(
+      `Requested projectId ${userAssociatedProjectIds.join(',')} is not associated with ${
+        authenticatedEntity.type
+      } ${
+        authenticatedEntity.user?.id ||
+        authenticatedEntity.customer?.id ||
+        userAssociatedProjectIds.join(',')
+      }`,
+    );
+  }
+
+  return requestedProjectIds || request.user?.projectIds;
 });
