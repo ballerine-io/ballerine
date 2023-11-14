9b17c2aaef1f284c0d4478223f49ef18337862ec
Omri Levy <omri.levy0197@gmail.com>
2023-11-14 13:15:37 +0200

    refactor(workflows-dashboard): now using shared tsconfig

--- apps/workflows-dashboard/src/utils/get-workflow-health-status.test.ts
+++ apps/workflows-dashboard/src/utils/get-workflow-health-status.test.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs';
+import * as dayjs from 'dayjs';
 import { IWorkflow } from '@/domains/workflows/api/workflow';
 import { getWorkflowHealthStatus, HealthStatus } from '@/utils/get-workflow-health-status';
 
