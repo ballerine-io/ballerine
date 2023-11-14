3a23613139b3981b6411e68b5b26f18deecd5862
Omri Levy <omri.levy0197@gmail.com>
2023-11-14 18:20:00 +0200

    refactor(packages/ui): now using shared tsconfig

--- packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/TextInputAdapter/TextInputAdapter.tsx
+++ packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/TextInputAdapter/TextInputAdapter.tsx
@@ -3,17 +3,12 @@ import { SelectField } from './components/SelectField';
 import { TextField } from './components/TextField';
 import { TextInputFieldType } from './types';
 import { FieldProps } from '@rjsf/utils';
-import { ComponentType, useMemo } from 'react';
+import { useMemo } from 'react';
 import { RJSFInputAdapter } from '@/components/organisms/DynamicForm/components/RSJVInputAdaters/types';
 
-export const TextInputAdapter:
-  | RJSFInputAdapter<string, unknown>
-  | RJSFInputAdapter<string | number, unknown> = (props: any) => {
+export const TextInputAdapter: RJSFInputAdapter<string, unknown> = props => {
   const InputComponent = useMemo(() => {
-    const fieldsMap: Record<
-      TextInputFieldType,
-      React.ComponentType<FieldProps<string>> | ComponentType<FieldProps<string | number>>
-    > = {
+    const fieldsMap: Record<TextInputFieldType, React.ComponentType<FieldProps<string>>> = {
       select: SelectField,
       text: TextField,
     };
