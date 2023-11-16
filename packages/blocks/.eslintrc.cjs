524fbc0e2b826ed5b08626f14049e765869b45e1
Omri Levy <omri.levy0197@gmail.com>
2023-11-16 10:22:15 +0200

    feat(blocks): now using shared eslint package

--- packages/blocks/.eslintrc.cjs
+++ packages/blocks/.eslintrc.cjs
@@ -1,22 +1,8 @@
-const { parserOptions, ...config } = require('../../packages/config/eslintrc.base.cjs');
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  ...config,
-  parserOptions: {
-    ...parserOptions,
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.eslint.json'],
+  env: {
+    node: true,
   },
-  rules: {
-    ...config.rules,
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-  ignorePatterns: ['.eslintrc.cjs'],
+  extends: ['@ballerine/eslint-config'],
+  ignorePatterns: ['.eslintrc.cjs', 'rollup.config.js'],
 };
