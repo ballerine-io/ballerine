4c9ddd8c8c23eb3f6f12596c74840607d0a1726a
Omri Levy <omri.levy0197@gmail.com>
2023-11-19 18:46:00 +0200

    fix(workflows-dashboard): remaining react-query types

--- apps/workflows-dashboard/src/pages/Workflows/components/organisms/metrics/AgentsActivityStats/hooks/useActiveUsersQuery/useActiveUsersQuery.ts
+++ apps/workflows-dashboard/src/pages/Workflows/components/organisms/metrics/AgentsActivityStats/hooks/useActiveUsersQuery/useActiveUsersQuery.ts
@@ -2,6 +2,8 @@ import { usersKeys } from '@/domains/workflows/api/users';
 import { useQuery } from '@tanstack/react-query';
 
 export const useActiveUsersQuery = () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const { data = [], isLoading } = useQuery({
     ...usersKeys.activeUsers({}),
   });
