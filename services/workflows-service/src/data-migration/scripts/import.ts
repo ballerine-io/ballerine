b808774ceff42d1c2a36e3b97d2b5d4cfc67b842
blokh <danielblokhi@gmail.com>
2023-11-22 18:03:11 +0200

    updated import script

--- services/workflows-service/src/data-migration/scripts/import.ts
+++ services/workflows-service/src/data-migration/scripts/import.ts
@@ -1,25 +1,4 @@
-import { AwsS3FileConfig } from '../../providers/file/file-provider/aws-s3-file.config';
-import { AwsS3FileService } from '../../providers/file/file-provider/aws-s3-file.service';
-import { env } from '../../env';
-import { DATA_MIGRATION_FOLDER_RELATIVE_PATH } from './consts';
-import console from 'console';
+import { execSync } from 'child_process';
 import path from 'path';
 
-const importMigrations = async () => {
-  const dataMigrationConfig = AwsS3FileConfig.fetchClientConfig(process.env, 'DATA_MIGRATION_');
-  const s3FileService = new AwsS3FileService(dataMigrationConfig);
-
-  await s3FileService.downloadFolderContent(
-    { bucketName: env.DATA_MIGRATION_BUCKET_NAME! },
-    path.join(__dirname, DATA_MIGRATION_FOLDER_RELATIVE_PATH),
-  );
-};
-importMigrations()
-  .then(() => {
-    console.log('Data Migration synced');
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error('Error Syncing Migrations: ', err);
-    process.exit(1);
-  });
+execSync(`bash ${path.join(__dirname, '/data-migration-import.sh')}`, { stdio: 'inherit' });
