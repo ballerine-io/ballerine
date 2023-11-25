8ce49196429e5373723841171d7ad5e347f923b5
Lior Zamir <liorz@ballerine.com>
2023-11-26 01:28:53 +0200

    fix: import module

--- services/workflows-service/src/providers/file/file-service.module.ts
+++ services/workflows-service/src/providers/file/file-service.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileService } from '@/providers/file/file.service';
 import { WorkflowControllerExternal } from '@/workflow/workflow.controller.external';
+import { initHttpMoudle } from '@/common/http-service/http-config.service';
 
 @Module({
+  imports: [initHttpMoudle()],
   controllers: [WorkflowControllerExternal],
   providers: [FileService],
   exports: [FileService],
