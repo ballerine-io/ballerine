98ce0fa912f6b24ce32cfff49b6fe8ba949e618f
Lior Zamir <liorz@ballerine.com>
2023-11-29 14:26:39 +0200

    chore: revert import reorder

--- services/workflows-service/src/collection-flow/collection-flow.module.ts
+++ services/workflows-service/src/collection-flow/collection-flow.module.ts
@@ -1,30 +1,19 @@
 import { PasswordService } from '@/auth/password/password.service';
 import { BusinessRepository } from '@/business/business.repository';
 import { BusinessService } from '@/business/business.service';
-import { CollectionFlowService } from '@/collection-flow/collection-flow.service';
-import { CollectionFlowBusinessController } from '@/collection-flow/controllers/collection-flow.business.controller';
 import { ColectionFlowController } from '@/collection-flow/controllers/collection-flow.controller';
-import { CollectionFlowEndUserController } from '@/collection-flow/controllers/collection-flow.end-user.controller';
-import { CollectionFlowFilesController } from '@/collection-flow/controllers/collection-flow.files.controller';
+import { CollectionFlowService } from '@/collection-flow/collection-flow.service';
 import { WorkflowAdapterManager } from '@/collection-flow/workflow-adapter.manager';
 import { AppLoggerModule } from '@/common/app-logger/app-logger.module';
 import { EntityRepository } from '@/common/entity/entity.repository';
-import { TokenAuthModule } from '@/common/guards/token-guard/token-auth.module';
-import { CustomerModule } from '@/customer/customer.module';
-import { CustomerRepository } from '@/customer/customer.repository';
-import { CustomerService } from '@/customer/customer.service';
 import { EndUserRepository } from '@/end-user/end-user.repository';
 import { EndUserService } from '@/end-user/end-user.service';
 import { FilterRepository } from '@/filter/filter.repository';
 import { FilterService } from '@/filter/filter.service';
 import { ProjectModule } from '@/project/project.module';
 import { FileService } from '@/providers/file/file.service';
-import { SalesforceIntegrationRepository } from '@/salesforce/salesforce-integration.repository';
-import { SalesforceService } from '@/salesforce/salesforce.service';
 import { FileRepository } from '@/storage/storage.repository';
 import { StorageService } from '@/storage/storage.service';
-import { UiDefinitionModule } from '@/ui-definition/ui-definition.module';
-import { UiDefinitionService } from '@/ui-definition/ui-definition.service';
 import { UserRepository } from '@/user/user.repository';
 import { UserService } from '@/user/user.service';
 import { HookCallbackHandlerService } from '@/workflow/hook-callback-handler.service';
@@ -34,6 +23,17 @@ import { WorkflowRuntimeDataRepository } from '@/workflow/workflow-runtime-data.
 import { WorkflowService } from '@/workflow/workflow.service';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { CustomerModule } from '@/customer/customer.module';
+import { UiDefinitionService } from '@/ui-definition/ui-definition.service';
+import { UiDefinitionModule } from '@/ui-definition/ui-definition.module';
+import { TokenAuthModule } from '@/common/guards/token-guard/token-auth.module';
+import { CollectionFlowFilesController } from '@/collection-flow/controllers/collection-flow.files.controller';
+import { CollectionFlowBusinessController } from '@/collection-flow/controllers/collection-flow.business.controller';
+import { CustomerService } from '@/customer/customer.service';
+import { CustomerRepository } from '@/customer/customer.repository';
+import { SalesforceService } from '@/salesforce/salesforce.service';
+import { SalesforceIntegrationRepository } from '@/salesforce/salesforce-integration.repository';
+import { CollectionFlowEndUserController } from '@/collection-flow/controllers/collection-flow.end-user.controller';
 
 @Module({
   imports: [
