36a09ef7a91971a0812b93ffe43945440ef880f2
Omri Levy <omri.levy0197@gmail.com>
2023-11-30 17:29:03 +0200

    refactor(backoffice-v2): removed commented out OCR button

--- apps/backoffice-v2/src/pages/Entity/components/Case/Case.Documents.Toolbar.tsx
+++ apps/backoffice-v2/src/pages/Entity/components/Case/Case.Documents.Toolbar.tsx
@@ -10,10 +10,7 @@ export const DocumentsToolbar: FunctionComponent<{
   hideOpenExternalButton?: boolean;
   onRotateDocument: () => void;
   onOpenDocumentInNewTab: (id: string) => void;
-  // isRotatedOrTransformed: boolean;
   shouldDownload: boolean;
-  // isCropping: boolean;
-  // isLoadingOCR: boolean;
   fileToDownloadBase64: string;
 }> = ({
   image,
@@ -53,39 +50,6 @@ export const DocumentsToolbar: FunctionComponent<{
           >
             <FileText className={`rotate-90 p-0.5`} />
           </button>
-          {/* Commented out until OCR is fixed with rotation, pinch, pan, and zoom. */}
-          {/*<button*/}
-          {/*  className={ctw(*/}
-          {/*    'btn btn-circle btn-ghost btn-sm bg-base-300/70 focus:outline-primary',*/}
-          {/*    {*/}
-          {/*      hidden: !isCropping,*/}
-          {/*    },*/}
-          {/*  )}*/}
-          {/*  disabled={shouldDownload}*/}
-          {/*  onClick={onCancelCrop}*/}
-          {/*>*/}
-          {/*  <XMarkSvg className={`p-0.5`} />*/}
-          {/*</button>*/}
-          {/*<div*/}
-          {/*  title={*/}
-          {/*    isRotatedOrTransformed*/}
-          {/*      ? `Cannot OCR rotated, zoomed, panned, or pinched documents`*/}
-          {/*      : undefined*/}
-          {/*  }*/}
-          {/*>*/}
-          {/*  <button*/}
-          {/*    type={`button`}*/}
-          {/*    className={ctw(*/}
-          {/*      `btn btn-circle btn-ghost btn-sm bg-base-300/70 text-[0.688rem] focus:outline-primary`,*/}
-          {/*      { loading: isLoadingOCR },*/}
-          {/*    )}*/}
-          {/*    onClick={onOcr}*/}
-          {/*    disabled={isRotatedOrTransformed || shouldDownload}*/}
-          {/*  >*/}
-          {/*    {isCropping && !isLoadingOCR && <CheckSvg className={`p-0.5`} />}*/}
-          {/*    {!isCropping && !isLoadingOCR && <span className={`p-0.5`}>OCR</span>}*/}
-          {/*  </button>*/}
-          {/*</div>*/}
         </>
       )}
       <a
