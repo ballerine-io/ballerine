2368a2cbcc284fd13e8a2eb9c5907b7d775a2a7a
Illia Rudniev <cheskmr@gmail.com>
2023-12-06 15:19:41 +0200

    fix: fixed bug where DateInput started losing focus when entering year (#1764)
    * fix: fixed bug where DateInput started losing focus when entering year
    
    * fix: removed comment & lock fix
    
    * fix: fixed incorrect paper import & code refactor
    
    * fix: removed variable

--- packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/DateInputAdater/DateInputAdater.tsx
+++ packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/DateInputAdater/DateInputAdater.tsx
@@ -1,17 +1,45 @@
-import { DatePickerInput } from '@/components/molecules';
+import { DatePickerChangeEvent, DatePickerInput } from '@/components/molecules';
 import { RJSFInputAdapter } from '@/components/organisms/DynamicForm/components/RSJVInputAdaters/types';
 import { useCallback } from 'react';
 
-export const DateInputAdater: RJSFInputAdapter = ({ id, formData, disabled, onBlur, onChange }) => {
+export const isValidDate = (dateString: string): boolean => {
+  const date = new Date(dateString);
+  if (date.getFullYear() < 1000) return false;
+
+  return true;
+};
+
+export const DateInputAdater: RJSFInputAdapter<string | null> = ({
+  id,
+  formData,
+  disabled,
+  onBlur,
+  onChange,
+}) => {
   const handleBlur = useCallback(() => {
     // @ts-ignore
     onBlur && onBlur(id, formData);
   }, [id, onBlur, formData]);
 
+  const handleChange = useCallback(
+    (event: DatePickerChangeEvent) => {
+      const dateValue = event.target.value;
+      if (dateValue === null) return onChange(null);
+
+      // every onChange call in context of DateInput forces re-render of component which eventually dicsonnects user focus from input
+      // in cases when date considered as valid e.g 05/12/0100 but doesnt make sense onChange call should be skipped
+      // validating date with helper below
+      if (!isValidDate(dateValue)) return;
+
+      onChange(dateValue);
+    },
+    [onChange],
+  );
+
   return (
     <DatePickerInput
       value={formData ? formData : undefined}
-      onChange={event => void onChange(String(event.target.value))}
+      onChange={handleChange}
       disabled={disabled}
       onBlur={handleBlur}
     />
