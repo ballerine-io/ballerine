b307bf249028eb4640fb6b37f7285fbe8894aeb8
Illia Rudniev <cheskmr@gmail.com>
2023-12-06 15:51:05 +0200

    feat: active breadcrumb items now using bold text & cleaned code

--- apps/kyb-app/src/components/atoms/Stepper/Stepper.tsx
+++ apps/kyb-app/src/components/atoms/Stepper/Stepper.tsx
@@ -1,15 +1,10 @@
-import {
-  StepperContext,
-  StepperLabelsMap,
-  StepperIndicatorMap,
-} from '@/components/atoms/Stepper/types';
+import { StepperContext, StepperIndicatorMap } from '@/components/atoms/Stepper/types';
 import { AnyChildren } from '@ballerine/ui';
 import { useMemo } from 'react';
 import { Provider } from './stepper.context';
 import { Idle } from './components/atoms/indicators/Idle';
 import { Warning } from './components/atoms/indicators/Warning';
 import { Completed } from './components/atoms/indicators/Completed';
-import { BaseLabel } from '@/components/atoms/Stepper/components/atoms/BaseLabel/BaseLabel';
 
 const baseIndicators: StepperIndicatorMap = {
   idle: Idle,
@@ -17,31 +12,20 @@ const baseIndicators: StepperIndicatorMap = {
   completed: Completed,
 };
 
-const baseLabelsMap: StepperLabelsMap = {
-  idle: ({ text }) => <BaseLabel variant="idle" text={text} />,
-  warning: ({ text }) => <BaseLabel variant="warning" text={text} />,
-  completed: ({ text }) => <BaseLabel variant="completed" text={text} />,
-};
-
 interface Props {
   children: AnyChildren;
   indicators?: StepperIndicatorMap;
-  labels?: StepperLabelsMap;
 }
 
-export const Stepper = ({ children, indicators, labels }: Props) => {
+export const Stepper = ({ children, indicators }: Props) => {
   const context = useMemo(
     (): StepperContext => ({
       indicatorsMap: {
         ...baseIndicators,
         ...indicators,
       },
-      labelsMap: {
-        ...baseLabelsMap,
-        ...labels,
-      },
     }),
-    [indicators, labels],
+    [indicators],
   );
 
   return (
--- apps/kyb-app/src/components/atoms/Stepper/types.ts
+++ apps/kyb-app/src/components/atoms/Stepper/types.ts
@@ -1,9 +1,7 @@
 export type StepStatus = 'warning' | 'completed' | 'idle';
 
 export type StepperIndicatorMap = Record<StepStatus, React.ComponentType>;
-export type StepperLabelsMap = Record<StepStatus, React.ComponentType<{ text: string }>>;
 
 export interface StepperContext {
   indicatorsMap: StepperIndicatorMap;
-  labelsMap: StepperLabelsMap;
 }
