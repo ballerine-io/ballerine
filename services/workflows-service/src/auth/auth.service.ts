0595e5081b14811c2155971d4b3f242aba1f33cc
Daniel Blokh <35891501+Blokh@users.noreply.github.com>
2023-12-10 16:10:54 +0200

    added user status logic (#1792)

--- services/workflows-service/src/auth/auth.service.ts
+++ services/workflows-service/src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserStatus } from '@prisma/client';
 import { UserInfo } from '../user/user-info';
 import { UserService } from '../user/user.service';
 import { PasswordService } from './password/password.service';
@@ -12,7 +13,10 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<UserInfo | null> {
     const user = await this.userService.getByEmailUnscoped(email);
+
     if (user && (await this.passwordService.compare(password, user.password))) {
+      if (user?.status !== UserStatus.Active) throw new UnauthorizedException('Unauthorized');
+
       const { id, firstName, lastName, roles } = user;
       const roleList = roles as string[];
       return { id, email, firstName, lastName, roles: roleList };
