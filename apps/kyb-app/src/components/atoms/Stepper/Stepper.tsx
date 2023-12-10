fbb0d248f98f37b8747af11971b9f7deec0b673f
Illia Rudniev <cheskmr@gmail.com>
2023-12-10 20:12:17 +0200

    fix: cleaned code

--- apps/kyb-app/src/components/atoms/Stepper/Stepper.tsx
+++ apps/kyb-app/src/components/atoms/Stepper/Stepper.tsx
@@ -1,36 +1,9 @@
-import { StepperContext, StepperIndicatorMap } from '@/components/atoms/Stepper/types';
 import { AnyChildren } from '@ballerine/ui';
-import { useMemo } from 'react';
-import { Provider } from './stepper.context';
-import { Idle } from './components/atoms/indicators/Idle';
-import { Warning } from './components/atoms/indicators/Warning';
-import { Completed } from './components/atoms/indicators/Completed';
-
-const baseIndicators: StepperIndicatorMap = {
-  idle: Idle,
-  warning: Warning,
-  completed: Completed,
-};
 
 interface Props {
   children: AnyChildren;
-  indicators?: StepperIndicatorMap;
 }
 
-export const Stepper = ({ children, indicators }: Props) => {
-  const context = useMemo(
-    (): StepperContext => ({
-      indicatorsMap: {
-        ...baseIndicators,
-        ...indicators,
-      },
-    }),
-    [indicators],
-  );
-
-  return (
-    <Provider value={context}>
-      <div className="flex h-full w-full">{children}</div>
-    </Provider>
-  );
+export const Stepper = ({ children }: Props) => {
+  return <div className="flex h-full w-full">{children}</div>;
 };
--- apps/kyb-app/src/components/atoms/Stepper/components/atoms/Breadcrumbs/Breadcrumbs.Label.tsx
+++ apps/kyb-app/src/components/atoms/Stepper/components/atoms/Breadcrumbs/Breadcrumbs.Label.tsx
@@ -1,12 +1,15 @@
-import { BaseLabel } from '@/components/atoms/Stepper/components/atoms/BaseLabel/BaseLabel';
 import { BreadcrumbsLabelProps } from '@/components/atoms/Stepper/components/atoms/Breadcrumbs/types';
 import { ctw } from '@ballerine/ui';
 
 export const Label = ({ active, text, state }: BreadcrumbsLabelProps) => {
   return (
-    <BaseLabel
-      className={ctw({ 'font-bold': active, 'opacity-50': state === 'completed' })}
-      text={text}
-    />
+    <span
+      className={ctw('text-sm leading-4', {
+        'font-bold': active,
+        'opacity-50': state === 'completed',
+      })}
+    >
+      {text}
+    </span>
   );
 };
