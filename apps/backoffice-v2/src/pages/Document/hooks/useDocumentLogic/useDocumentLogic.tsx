e6235843d1555477ffe8527194b89cc9d8b48d79
Tomer Shvadron <tomers@ballerine.com>
2023-12-12 17:51:20 +0200

    Update document on opened tab (#1810)
    * feat: update document on opened tab
    
    * feat: pR comments fixes
    
    * feat: more PR comments fixes
    
    * feat: some more PR comments fixes
    
    * feat: please god last PR comments fixes

---
+++ apps/backoffice-v2/src/pages/Document/hooks/useDocumentLogic/useDocumentLogic.tsx
@@ -0,0 +1,111 @@
+import { toTitleCase } from 'string-ts';
+import { BroadcastChannel } from 'broadcast-channel';
+import { useCallback, useLayoutEffect, useMemo } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+
+import { valueOrNA } from '@/common/utils/value-or-na/value-or-na';
+import { useFilterId } from '@/common/hooks/useFilterId/useFilterId';
+import { useWorkflowQuery } from '@/domains/workflows/hooks/queries/useWorkflowQuery/useWorkflowQuery';
+import { useStorageFilesQuery } from '@/domains/storage/hooks/queries/useStorageFilesQuery/useStorageFilesQuery';
+import { BroadcastChannelEvent, BroadcastChannel } from '@/common/enums';
+
+export const useDocumentLogic = () => {
+  const { state } = useLocation();
+  const filterId = useFilterId();
+  const navigate = useNavigate();
+  const { entityId, documentId, locale = 'en' } = useParams();
+
+  const broadcastChannel = useMemo(
+    () =>
+      new BroadcastChannel(BroadcastChannel.OPEN_DOCUMENT_IN_NEW_TAB, {
+        webWorkerSupport: false,
+      }),
+    [],
+  );
+
+  const handler = useCallback(
+    ({
+      type,
+      data: { entityId, documentId },
+    }: {
+      type: keyof typeof BroadcastChannelEvent;
+      data: { entityId: string; documentId: string };
+    }) => {
+      if (type === BroadcastChannelEvent.OPEN_DOCUMENT_IN_NEW_TAB) {
+        navigate(
+          `/${locale}/case-management/entities/${entityId}/document/${documentId}?filterId=${filterId}`,
+          {
+            replace: true,
+            state: {
+              from: state?.from,
+            },
+          },
+        );
+      }
+
+      broadcastChannel.postMessage({ type: BroadcastChannelEvent.OPEN_DOCUMENT_IN_NEW_TAB_ACK });
+    },
+    [broadcastChannel, filterId, locale, navigate, state?.from],
+  );
+
+  useLayoutEffect(() => {
+    broadcastChannel.addEventListener('message', handler);
+
+    return () => {
+      broadcastChannel.removeEventListener('message', handler);
+    };
+  }, [broadcastChannel, handler]);
+
+  const { data: workflow, isLoading: isLoadingWorkflow } = useWorkflowQuery({
+    workflowId: entityId,
+    filterId,
+  });
+
+  const document = useMemo(
+    () =>
+      workflow?.context?.documents.find(doc =>
+        doc.pages.some(({ ballerineFileId }) => ballerineFileId === documentId),
+      ),
+    [documentId, workflow?.context?.documents],
+  );
+
+  const page = useMemo(
+    () =>
+      workflow?.context?.documents
+        ?.flatMap(({ pages }) => pages)
+        ?.find(({ ballerineFileId }) => ballerineFileId === documentId),
+    [documentId, workflow?.context?.documents],
+  );
+
+  const fileIds = useMemo(
+    () =>
+      workflow?.context?.documents
+        ?.flatMap(({ pages }) => pages?.map(({ ballerineFileId }) => ballerineFileId))
+        .filter(id => id === documentId),
+    [documentId, workflow?.context?.documents],
+  );
+
+  const storageFilesQueryResult = useStorageFilesQuery(fileIds);
+
+  const title = useMemo(
+    () =>
+      `${valueOrNA(toTitleCase(document?.category ?? ''))} - ${valueOrNA(
+        toTitleCase(document?.type ?? ''),
+      )}${page?.metadata?.side ? ` - ${page?.metadata?.side}` : ''}`,
+    [document, page],
+  );
+
+  const isLoading = isLoadingWorkflow || storageFilesQueryResult.some(file => file.isLoading);
+
+  return {
+    isLoading,
+    documents: [
+      {
+        title,
+        fileType: page?.type,
+        id: fileIds?.[0] as string,
+        imageUrl: storageFilesQueryResult?.[0]?.data as string,
+      },
+    ],
+  };
+};
---
+++ apps/backoffice-v2/src/pages/Entity/components/Case/hooks/useDocumentsToolbarLogic/useDocumentsToolbarLogic.tsx
@@ -0,0 +1,66 @@
+import { useCallback, useLayoutEffect, useMemo, useRef } from 'react';
+
+import { useEntity } from '@/pages/Entity/hooks/useEntity/useEntity';
+import { BroadcastChannel } from 'broadcast-channel';
+import { BroadcastChannelEvent, BroadcastChannel } from '@/common/enums';
+
+interface IUseDocumentsToolbarProps {
+  imageId: string;
+  hideOpenExternalButton?: boolean;
+  onOpenDocumentInNewTab: (id: string) => void;
+}
+
+export const useDocumentsToolbarLogic = ({
+  imageId,
+  hideOpenExternalButton,
+  onOpenDocumentInNewTab,
+}: IUseDocumentsToolbarProps) => {
+  const { workflow } = useEntity();
+
+  const broadcastChannel = useMemo(
+    () =>
+      new BroadcastChannel(BroadcastChannel.OPEN_DOCUMENT_IN_NEW_TAB, {
+        webWorkerSupport: false,
+      }),
+    [],
+  );
+
+  const isDocumentTabOpen = useRef(false);
+
+  const handler = useCallback(({ type }: { type: keyof typeof BroadcastChannelEvent }) => {
+    if (type === BroadcastChannelEvent.OPEN_DOCUMENT_IN_NEW_TAB_ACK) {
+      isDocumentTabOpen.current = true;
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    if (hideOpenExternalButton) {
+      return;
+    }
+
+    broadcastChannel.addEventListener('message', handler);
+
+    return () => {
+      broadcastChannel.removeEventListener('message', handler);
+    };
+  }, [broadcastChannel, handler, hideOpenExternalButton]);
+
+  const onOpenInNewTabClick = useCallback(() => {
+    broadcastChannel.postMessage({
+      type: BroadcastChannelEvent.OPEN_DOCUMENT_IN_NEW_TAB,
+      data: { entityId: workflow?.id, documentId: imageId },
+    });
+
+    setTimeout(() => {
+      if (isDocumentTabOpen.current) {
+        isDocumentTabOpen.current = false;
+
+        return;
+      }
+
+      onOpenDocumentInNewTab(imageId);
+    }, 100);
+  }, [broadcastChannel, imageId, onOpenDocumentInNewTab, workflow?.id]);
+
+  return { onOpenInNewTabClick };
+};
