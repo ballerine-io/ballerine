9d66652d68ac317ea30eed2730ed4abff315444f
Omri Levy <omri.levy0197@gmail.com>
2023-12-12 22:31:19 +0200

    feat(backoffice-v2): kyc resgitry info now uses blocks package

--- apps/backoffice-v2/src/pages/Entity/hooks/useTasks/useTasks.tsx
+++ apps/backoffice-v2/src/pages/Entity/hooks/useTasks/useTasks.tsx
@@ -216,7 +216,7 @@ export const useTasks = ({
           ...websiteMonitoringBlock,
           ...entityInfoBlock,
           ...registryInfoBlock,
-          // ...kybRegistryInfoBlock,
+          ...kybRegistryInfoBlock,
           // ...companySanctionsBlock,
           // ...directorsUserProvidedBlock,
           // ...directorsRegistryProvidedBlock,
