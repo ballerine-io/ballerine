54317c1a47eea35c68db8aa4780f969ad7e4d19a
Daniel Blokh <35891501+Blokh@users.noreply.github.com>
2023-12-19 17:32:20 +0200

    fixed kyb example (#1856)

--- services/workflows-service/scripts/seed.ts
+++ services/workflows-service/scripts/seed.ts
@@ -15,13 +15,11 @@ import { generateDynamicDefinitionForE2eTest } from './workflows/e2e-dynamic-url
 import { generateKycForE2eTest } from './workflows/kyc-dynamic-process-example';
 import { generateKybDefintion } from './workflows';
 import { generateKycSessionDefinition } from './workflows/kyc-email-process-example';
-import { generateParentKybWithSessionKycs } from './workflows/parent-kyb-kyc-session-workflow';
 import { env } from '../src/env';
 import { generateKybKycWorkflowDefinition } from './workflows/kyb-kyc-workflow-definition';
 import { generateBaseTaskLevelStates } from './workflows/generate-base-task-level-states';
 import { generateBaseCaseLevelStates } from './workflows/generate-base-case-level-states';
 import type { InputJsonValue } from '../src/types';
-import { generateDynamicUiWorkflow } from './workflows/dynamic-ui-workflow';
 import { generateWebsiteMonitoringExample } from './workflows/website-monitoring-workflow';
 import { generateCollectionKybWorkflow } from './workflows/generate-collection-kyb-workflow';
 import { generateInitialCollectionFlowExample } from './workflows/runtime/generate-initial-collection-flow-example';
