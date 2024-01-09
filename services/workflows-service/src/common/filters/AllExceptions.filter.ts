925f9cab73ed79e490bbc5c732a8efe2022533a7
Lior Zamir <liorz@ballerine.com>
2024-01-09 20:04:55 +0200

    fix: bad request message should be pass on message

--- services/workflows-service/src/common/filters/AllExceptions.filter.ts
+++ services/workflows-service/src/common/filters/AllExceptions.filter.ts
@@ -28,12 +28,22 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
       this.logError(request, serverError);
     }
 
+    const errorRes = serverError.getResponse();
+
+    let errorMessage: unknown;
+
+    if (typeof errorRes === 'string') {
+      errorMessage = errorRes;
+    } else if (typeof errorRes === 'object' && 'message' in errorRes && errorRes.message) {
+      errorMessage = errorRes.message;
+    }
+
     response
       .status(serverError.getStatus())
       .setHeader('Content-Type', 'application/json')
       .json({
         errorCode: String(HttpStatusCode[serverError.getStatus()]),
-        message: serverError.message,
+        message: errorMessage,
         statusCode: serverError.getStatus(),
         timestamp: new Date().toISOString(),
         path: request.url,
