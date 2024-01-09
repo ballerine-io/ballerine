475ba71998e4aec2c6d9820a09d5584f8188ca85
Illia Rudniev <cheskmr@gmail.com>
2024-01-09 16:24:08 +0200

    feat: added version to report header & bump (#1906)
    Co-authored-by: Alon Peretz <Alonp99@gmail.com>

--- packages/react-pdf-toolkit/src/templates/report/components/Header/Header.tsx
+++ packages/react-pdf-toolkit/src/templates/report/components/Header/Header.tsx
@@ -1,17 +1,18 @@
+import { Badge } from '@/components/Badge';
+import { tw } from '@/theme';
 import { Image, Text, View } from '@react-pdf/renderer';
 import ballerinelogo from '../../../../assets/ballerine-logo-report.png';
-import { tw } from '@/theme';
-import { Badge } from '@/components/Badge';
 
 export interface HeaderProps {
   title: string;
   status: 'published';
+  version?: number;
 }
 
-export const Header = ({ title }: HeaderProps) => {
+export const Header = ({ title, version }: HeaderProps) => {
   return (
     <View style={tw('flex flex-col')}>
-      <View style={tw('flex items-start mb-8')}>
+      <View style={tw('flex flex-row justify-between items-center mb-8')}>
         <Image
           style={{
             width: '140px',
@@ -19,6 +20,7 @@ export const Header = ({ title }: HeaderProps) => {
           }}
           src={ballerinelogo}
         />
+        {version && <Text style={tw('text-xs')}>{`Version ${version}`}</Text>}
       </View>
       <View style={tw('flex flex-col gap-10')}>
         <Text style={tw('text-3xl leading-none font-semibold')}>{title}</Text>
