022a2a443a43f062e8cfbe99ad46073bcb2520c8
Illia Rudniev <cheskmr@gmail.com>
2024-01-14 15:48:27 +0200

    fix: removed object mutation from error transformer

--- apps/backoffice-v2/src/pages/Entities/components/CaseCreation/components/CaseCreationForm/utils/transformErrors.ts
+++ apps/backoffice-v2/src/pages/Entities/components/CaseCreation/components/CaseCreationForm/utils/transformErrors.ts
@@ -3,46 +3,47 @@ import { RJSFValidationError } from '@rjsf/utils';
 
 export const transformErrors = (errors: RJSFValidationError[]): RJSFValidationError[] => {
   return errors.map(error => {
-    console.log('error', error);
-    if (error.name === 'minLength' || error.name === 'required') {
-      error.message = 'This field is required.';
+    const errorCopy = structuredClone(error);
+
+    if (errorCopy.name === 'minLength' || errorCopy.name === 'required') {
+      errorCopy.message = 'This field is required.';
     }
 
     if (
-      error.name === 'enum' &&
-      Array.isArray((error.params as AnyObject).allowedValues as any[]) &&
-      ((error.params as AnyObject).allowedValues as boolean[]).includes(true)
+      errorCopy.name === 'enum' &&
+      Array.isArray((errorCopy.params as AnyObject).allowedValues as any[]) &&
+      ((errorCopy.params as AnyObject).allowedValues as boolean[]).includes(true)
     ) {
-      error.message = 'Must be checked.';
+      errorCopy.message = 'Must be checked.';
     }
 
-    if (error.name === 'oneOf') {
-      error.message = 'Value must be selected from list.';
+    if (errorCopy.name === 'oneOf') {
+      errorCopy.message = 'Value must be selected from list.';
     }
 
     // Removing oneOf constant specific errors(they are generated from each item in oneOf array of schema)
-    if (error.message?.includes('constant')) {
-      error.message = '';
+    if (errorCopy.message?.includes('constant')) {
+      errorCopy.message = '';
     }
 
-    if (error.params?.type === 'number') {
-      error.message = 'This field is required';
+    if (errorCopy.params?.type === 'number') {
+      errorCopy.message = 'This field is required';
     }
 
-    if (error.params?.type === 'object') {
-      error.message = 'This field is required';
+    if (errorCopy.params?.type === 'object') {
+      errorCopy.message = 'This field is required';
     }
 
-    if (error.name === 'minItems') {
-      error.message = `Plead add at least ${error?.params.limit} item${
-        error?.params.limit > 1 ? 's' : ''
+    if (errorCopy.name === 'minItems') {
+      errorCopy.message = `Plead add at least ${errorCopy?.params.limit} item${
+        errorCopy?.params.limit > 1 ? 's' : ''
       }.`;
     }
 
-    if (error.params?.format === 'email') {
-      error.message = 'Please provide valid email address.';
+    if (errorCopy.params?.format === 'email') {
+      errorCopy.message = 'Please provide valid email address.';
     }
 
-    return error;
+    return errorCopy;
   });
 };
