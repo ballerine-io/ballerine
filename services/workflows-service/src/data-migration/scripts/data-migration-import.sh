0a505fc7c24ef26e3f9177bbb183606198213b1b
blokh <danielblokhi@gmail.com>
2024-02-01 16:52:44 +0200

    fixed data migration import

--- services/workflows-service/src/data-migration/scripts/data-migration-import.sh
+++ services/workflows-service/src/data-migration/scripts/data-migration-import.sh
@@ -9,4 +9,14 @@ https://api.github.com/repos/ballerine-io/wf-data-migration/zipball -o /tmp/wf-d
 rm -rf ./prisma/data-migrations &&
 mkdir -p ./prisma/data-migrations &&
 unzip -q /tmp/wf-data-migration.zip '*wf-data-migration*.ts' -d /tmp/ &&
-mv /tmp/*wf-data-migration*/* ./prisma/data-migrations/
+mv /tmp/*wf-data-migration*/common/* ./prisma/data-migrations/ &&
+
+if [[ "$ENVIRONMENT_NAME" == "staging" || "$ENVIRONMENT_NAME" == "sandbox" || "$ENVIRONMENT_NAME" == "sb" ]]; then
+    mv /tmp/*wf-data-migration*/sb/* ./prisma/data-migrations/
+elif [[ "$ENVIRONMENT_NAME" == "prod" || "$ENVIRONMENT_NAME" == "production" ]]; then
+    mv /tmp/*wf-data-migration*/prod/* ./prisma/data-migrations/
+elif [[ "$ENVIRONMENT_NAME" == "dev" || "$ENVIRONMENT_NAME" == "development" ]]; then
+    mv /tmp/*wf-data-migration*/dev/* ./prisma/data-migrations/
+else
+    echo "Unknown environment: $ENVIRONMENT_NAME. No data migrations moved."
+fi
