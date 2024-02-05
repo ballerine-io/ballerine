ef78edbe246616aa12c0292264ab41b88898d0eb
Illia Rudniev <cheskmr@gmail.com>
2024-02-05 14:59:53 +0200

    fix: replaced host with wildcard

--- apps/kyb-app/src/hooks/useFlowTracking/useFlowTracking.ts
+++ apps/kyb-app/src/hooks/useFlowTracking/useFlowTracking.ts
@@ -2,17 +2,11 @@ import { useCallback } from 'react';
 
 export const useFlowTracking = () => {
   const trackExit = useCallback(() => {
-    window.parent.postMessage(
-      'ballerine.collection-flow.back-button-pressed',
-      import.meta.env.VITE_HOST_URL,
-    );
+    window.parent.postMessage('ballerine.collection-flow.back-button-pressed', '*');
   }, []);
 
   const trackFinish = useCallback(() => {
-    window.parent.postMessage(
-      'ballerine.collection-flow.finish-button-pressed',
-      import.meta.env.VITE_HOST_URL,
-    );
+    window.parent.postMessage('ballerine.collection-flow.finish-button-pressed', '*');
   }, []);
 
   return {
