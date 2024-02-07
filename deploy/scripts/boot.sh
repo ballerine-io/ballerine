0a056d5b3e1d335dfe442729999fd6a46f2545f0
pratapalakshmi <137189067+pratapalakshmi@users.noreply.github.com>
2024-02-07 14:20:51 +0530

    Update boot.sh

--- deploy/scripts/boot.sh
+++ deploy/scripts/boot.sh
@@ -2,6 +2,43 @@
 
 set -o errexit
 
+DOMAIN_NAME=$1
+
+if [[ -z "$DOMAIN_NAME" ]]; then
+  echo "please provide domain_name if you want to host ballerine."
+fi
+
+source_env_variables(){
+  cat <<<"
+  DOMAIN_NAME="${DOMAIN_NAME}"
+  VITE_API_URL="http://${DOMAIN_NAME:-localhost}:3000/api/v1/"
+  VITE_KYB_DEFINITION_ID="kyb_parent_kyc_session_example"
+  BACKOFFICE_PORT=5137
+  HEADLESS_SVC_PORT=5173
+  WORKFLOW_SVC_PORT=3000
+  BCRYPT_SALT=10
+  API_KEY="secret"
+  NODE_ENV="development"
+  COMPOSE_PROJECT_NAME=ballerine-x
+  DB_PORT=5432
+  DB_USER=admin
+  DB_PASSWORD=admin
+  SESSION_SECRET=secret
+  SESSION_EXPIRATION_IN_MINUTES=60
+  BACKOFFICE_CORS_ORIGIN="http://${DOMAIN_NAME:-localhost}:${BACKOFFICE_PORT}"
+  WORKFLOW_DASHBOARD_CORS_ORIGIN="http://${DOMAIN_NAME:-localhost}:${WORKFLOW_DASHBOARD_PORT}"
+  KYB_EXAMPLE_CORS_ORIGIN="http://${DOMAIN_NAME:-localhost}:${KYB_APP_PORT}"
+  WORKFLOW_DASHBOARD_PORT=5200
+  WEBSOCKET_SVC_PORT=3500
+  KYB_APP_PORT=5201
+  WEBHOOK_SECRET=webhook_secret
+  APP_API_URL="https://alon.ballerine.dev"
+  EMAIL_API_URL="https://api.sendgrid.com/v3/mail/send"
+  UNIFIED_API_URL="https://unified-api-test.eu.ballerine.app"
+  ENVIRONMENT_NAME="development"
+  "> /home/ubuntu/ballerine/deploy/.env
+}
+
 install_docker(){
 
   sudo apt update;
@@ -13,9 +50,10 @@ install_docker(){
 }
 
 deploy_ballerine(){
-	mkdir -p $HOME/ballerine; cd $HOME/ballerine
-  curl https://raw.githubusercontent.com/ballerine-io/ballerine/dev/deploy/docker-compose.yml -o docker-compose.yml;
-  docker-compose up -d --force-recreate
+  git clone https://github.com/ballerine-io/ballerine.git -b deploy/ballerine/deploy/aws/instances ;
+  rm /home/ubuntu/ballerine/deploy/.env
+  source_env_variables
+  cd ballerine/deploy ; sudo docker-compose -f docker-compose-build.yml up -d --force-recreate
 }
 
 install_docker
