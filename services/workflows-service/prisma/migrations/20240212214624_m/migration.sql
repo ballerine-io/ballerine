73585fd6c9ae47d998bec806fbe9b414b1cf735a
Alon Peretz <Alonp99@gmail.com>
2024-02-13 00:47:26 +0200

    draft1

--- services/workflows-service/prisma/migrations/20240212214624_m/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[projectId,transactionCorrelationId]` on the table `TransactionRecord` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "TransactionRecord_projectId_transactionCorrelationId_key" ON "TransactionRecord"("projectId", "transactionCorrelationId");
--- services/workflows-service/prisma/migrations/20240212224721_transaction_adjustments/migration.sql
+++ services/workflows-service/prisma/migrations/20240212224721_transaction_adjustments/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - The `paymentMethod` column on the `TransactionRecord` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[projectId,transactionCorrelationId]` on the table `TransactionRecord` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- CreateEnum
@@ -17,3 +18,6 @@ ADD COLUMN     "paymentMethod" TEXT;
 
 -- CreateIndex
 CREATE INDEX "TransactionRecord_paymentMethod_idx" ON "TransactionRecord"("paymentMethod");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TransactionRecord_projectId_transactionCorrelationId_key" ON "TransactionRecord"("projectId", "transactionCorrelationId");
