30e198381dbdd0c77a5a3db2d1c71314a03e0978
Lior Zamir <liorz@ballerine.com>
2024-02-19 18:58:42 +0200

    fix: db schema

---
+++ services/workflows-service/prisma/migrations/20240219165832_/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `bankCountry` on the `Counterparty` table. All the data in the column will be lost.
+  - You are about to drop the column `sortCode` on the `Counterparty` table. All the data in the column will be lost.
+  - Made the column `inlineRule` on table `AlertDefinition` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "AlertDefinition" ALTER COLUMN "inlineRule" SET NOT NULL,
+ALTER COLUMN "modifiedBy" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Counterparty" DROP COLUMN "bankCountry",
+DROP COLUMN "sortCode";
