776e97caed3c07f5954eddde7f774d1610e8a3c2
Lior Zamir <liorz@ballerine.com>
2024-03-11 01:16:04 +0200

    fix(server.ts): Change port variable case from lowercase port to uppercase PORT
    The port variable in the server.ts file was changed from lowercase "port" to uppercase "PORT". This change was made to improve the semantics of the code and make it more readable.
    
    feat(server.ts): Add support for process.env.PORT environment variable
    
    In the server.ts file, support for the process.env.PORT environment variable was added. This allows the application to be run on a configurable port specified by the environment variable. This change was made to provide flexibility in running the application on different ports based on the deployment environment.

---
+++ services/workflows-service/prisma/migrations/20240310231549_fix_transaction_monitoring_schema/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `businessId` on the `Alert` table. All the data in the column will be lost.
+  - You are about to drop the column `endUserId` on the `Alert` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Alert" DROP CONSTRAINT "Alert_businessId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Alert" DROP CONSTRAINT "Alert_endUserId_fkey";
+
+-- DropIndex
+DROP INDEX "Alert_businessId_idx";
+
+-- DropIndex
+DROP INDEX "Alert_endUserId_idx";
+
+-- DropIndex
+DROP INDEX "EndUsersOnBusinesses_businessId_idx";
+
+-- AlterTable
+ALTER TABLE "Alert" DROP COLUMN "businessId",
+DROP COLUMN "endUserId";
