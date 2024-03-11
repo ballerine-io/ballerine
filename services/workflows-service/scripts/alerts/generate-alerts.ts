def46cef4f0c2568526bcb1a4e9f9221c694d6cc
Matan Yadaev <matan.yed@gmail.com>
2024-03-11 15:58:57 +0200

    Expose `generateAlertDefinitions` function (#2184)
    * refactor: expose `generateAlertDefinitions` function
    
    * feat: add migration

--- services/workflows-service/scripts/alerts/generate-alerts.ts
+++ services/workflows-service/scripts/alerts/generate-alerts.ts
@@ -12,7 +12,7 @@ import {
 } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import { AggregateType } from '../../src/data-analytics/consts';
-import { InputJsonValue } from '@/types';
+import { InputJsonValue, PrismaTransaction } from '@/types';
 
 const tags = [
   ...new Set([
@@ -25,115 +25,185 @@ const tags = [
   ]),
 ];
 
-export const getRuleDefinitions = () => {
-  const _PAY_HCA_CC = {
-    id: 'PAY_HCA_CC',
-    fnName: 'evaluateTransactionsAgainstDynamicRules',
-    subjects: ['businessId'],
-    options: {
-      groupByBusiness: true,
-      havingAggregate: AggregateType.SUM,
+export const ALERT_INLINE_RULES = [
+  {
+    defaultSeverity: AlertSeverity.medium,
+    inlineRule: {
+      id: 'PAY_HCA_CC',
+      fnName: 'evaluateTransactionsAgainstDynamicRules',
+      subjects: ['businessId'],
+      options: {
+        groupByBusiness: true,
+        havingAggregate: AggregateType.SUM,
 
-      direction: 'inbound',
-      excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
+        direction: 'inbound',
+        excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
 
-      paymentMethods: [PaymentMethod.credit_card],
-      excludePaymentMethods: false,
+        paymentMethods: [PaymentMethod.credit_card],
+        excludePaymentMethods: false,
 
-      timeAmount: 7,
-      timeUnit: 'days',
+        timeAmount: 7,
+        timeUnit: 'days',
 
-      amountThreshold: 1000,
-    } as TransactionsAgainstDynamicRulesType,
-  } as const satisfies InlineRule;
+        amountThreshold: 1000,
+      } as TransactionsAgainstDynamicRulesType,
+    },
+  },
 
   // Rule ID: PAY_HCA_APM
   // Description: High Cumulative Amount - inbound - Customer (APM)
   // Condition: Sum of incoming transactions over a set period of time is greater than a limit of APM.
-  const _PAY_HCA_APM = {
-    id: 'PAY_HCA_APM',
-    fnName: 'evaluateTransactionsAgainstDynamicRules',
-    subjects: ['businessId'],
-    options: {
-      groupByBusiness: true,
-      havingAggregate: AggregateType.SUM,
+  {
+    defaultSeverity: AlertSeverity.medium,
+    inlineRule: {
+      id: 'PAY_HCA_APM',
+      fnName: 'evaluateTransactionsAgainstDynamicRules',
+      subjects: ['businessId'],
+      options: {
+        groupByBusiness: true,
+        havingAggregate: AggregateType.SUM,
 
-      direction: 'inbound',
-      excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
+        direction: 'inbound',
+        excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
 
-      paymentMethods: [PaymentMethod.credit_card],
-      excludePaymentMethods: true,
+        paymentMethods: [PaymentMethod.credit_card],
+        excludePaymentMethods: true,
 
-      timeAmount: 7,
-      timeUnit: 'days',
+        timeAmount: 7,
+        timeUnit: 'days',
 
-      amountThreshold: 1000,
-    } as TransactionsAgainstDynamicRulesType,
-  } as const satisfies InlineRule;
+        amountThreshold: 1000,
+      } as TransactionsAgainstDynamicRulesType,
+    },
+  },
 
   // Rule ID: STRUC_CC
   // Description: Structuring - inbound - Customer (Credit Card)
   // Condition: Significant number of low value incoming transactions just below a threshold of credit card.
-  const _STRUC_CC = {
-    id: 'STRUC_CC',
-    fnName: 'evaluateTransactionsAgainstDynamicRules',
-    subjects: ['businessId'],
-    options: {
-      groupByBusiness: true,
-      havingAggregate: AggregateType.COUNT,
+  {
+    defaultSeverity: AlertSeverity.medium,
+    inlineRule: {
+      id: 'STRUC_CC',
+      fnName: 'evaluateTransactionsAgainstDynamicRules',
+      subjects: ['businessId'],
+      options: {
+        groupByBusiness: true,
+        havingAggregate: AggregateType.COUNT,
 
-      direction: 'inbound',
-      excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
+        direction: 'inbound',
+        excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
 
-      paymentMethods: [PaymentMethod.credit_card],
-      excludePaymentMethods: false,
+        paymentMethods: [PaymentMethod.credit_card],
+        excludePaymentMethods: false,
 
-      timeAmount: 7,
-      timeUnit: 'days',
+        timeAmount: 7,
+        timeUnit: 'days',
 
-      amountThreshold: 5,
-      amountBetween: { min: 500, max: 1000 },
-    } as TransactionsAgainstDynamicRulesType,
-  } as const satisfies InlineRule;
+        amountThreshold: 5,
+        amountBetween: { min: 500, max: 1000 },
+      } as TransactionsAgainstDynamicRulesType,
+    },
+  },
 
   // Rule ID: STRUC_APM
   // Description: Structuring - inbound - Customer (APM)
   // Condition: Significant number of low value incoming transactions just below a threshold of APM.
-  const _STRUC_APM = {
-    id: 'STRUC_APM',
-    fnName: 'evaluateTransactionsAgainstDynamicRules',
-    subjects: ['businessId'],
-    options: {
-      groupByBusiness: true,
-      havingAggregate: AggregateType.COUNT,
+  {
+    defaultSeverity: AlertSeverity.medium,
+    inlineRule: {
+      id: 'STRUC_APM',
+      fnName: 'evaluateTransactionsAgainstDynamicRules',
+      subjects: ['businessId'],
+      options: {
+        groupByBusiness: true,
+        havingAggregate: AggregateType.COUNT,
 
-      direction: 'inbound',
-      excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
+        direction: 'inbound',
+        excludedCounterpartyIds: ['9999999999999999', '999999******9999'],
 
-      paymentMethods: [PaymentMethod.credit_card],
-      excludePaymentMethods: false,
+        paymentMethods: [PaymentMethod.credit_card],
+        excludePaymentMethods: false,
 
-      timeAmount: 7,
-      timeUnit: 'days',
+        timeAmount: 7,
+        timeUnit: 'days',
 
-      amountBetween: { min: 500, max: 1000 },
+        amountBetween: { min: 500, max: 1000 },
 
-      amountThreshold: 5,
-    } as TransactionsAgainstDynamicRulesType,
-  } as const satisfies InlineRule;
+        amountThreshold: 5,
+      } as TransactionsAgainstDynamicRulesType,
+    },
+  },
+] as const satisfies readonly { inlineRule: InlineRule; defaultSeverity: AlertSeverity }[];
 
-  const rules = [_PAY_HCA_CC, _PAY_HCA_APM, _STRUC_CC, _STRUC_APM];
+export const generateAlertDefinitions = async (
+  prisma: PrismaClient | PrismaTransaction,
+  {
+    createdBy = 'SYSTEM',
+    project,
+  }: {
+    createdBy?: string;
+    project: Project;
+  },
+) =>
+  Promise.all(
+    ALERT_INLINE_RULES.map(({ inlineRule, defaultSeverity }) =>
+      prisma.alertDefinition.create({
+        include: {
+          alert: true,
+        },
+        data: {
+          type: faker.helpers.arrayElement(Object.values(AlertType)) as AlertType,
+          name: faker.lorem.words(3),
+          enabled: faker.datatype.boolean(),
+          description: faker.lorem.sentence(),
+          rulesetId: `set-${inlineRule.id}`,
+          defaultSeverity,
+          ruleId: inlineRule.id,
+          createdBy: createdBy,
+          modifiedBy: createdBy,
+          dedupeStrategies: {
+            strategy: {},
+            cooldownTimeframeInMinutes: faker.datatype.number({ min: 60, max: 3600 }),
+          },
+          config: { config: {} },
+          inlineRule,
+          tags: [faker.helpers.arrayElement(tags), faker.helpers.arrayElement(tags)],
+          additionalInfo: {},
+          projectId: project.id,
+        },
+      }),
+    ),
+  );
 
-  const mergedRules: Record<string, InlineRule> = {};
+const generateFakeAlert = ({
+  severity,
+  ids,
+}: {
+  severity: AlertSeverity;
+  ids: string[];
+}): Omit<Prisma.AlertCreateManyAlertDefinitionInput, 'projectId'> => {
+  const businessIdCounterpartyOriginatorIdOrBoth = faker.helpers.arrayElement(
+    ids.map(id => ({ counterpartyId: id })),
+  );
 
-  rules.forEach(rule => {
-    mergedRules[rule.id] = rule;
-  });
-
-  return mergedRules;
+  return {
+    dataTimestamp: faker.date.past(),
+    state: faker.helpers.arrayElement(Object.values(AlertState)),
+    status: faker.helpers.arrayElement(Object.values(AlertStatus)),
+    tags: [faker.random.word(), faker.random.word(), faker.random.word()],
+    executionDetails: JSON.parse(faker.datatype.json()) as InputJsonValue,
+    severity,
+    ...businessIdCounterpartyOriginatorIdOrBoth,
+    // TODO: Assign assigneeId value to a valid user id
+    // TODO: Assign counterpart value to a valid user id
+    // businessId: faker.datatype.uuid(),
+    // endUserId: faker.datatype.uuid(),
+    // assigneeId: faker.datatype.uuid(),
+    // workflowRuntimeDataId: faker.datatype.uuid()
+  };
 };
 
-export const generateFakeAlertDefinition = async (
+export const generateFakeAlertsAndDefinitions = async (
   prisma: PrismaClient,
   {
     project,
@@ -144,89 +214,29 @@ export const generateFakeAlertDefinition = async (
     ids: string[];
   },
 ) => {
-  const generateFakeAlert = ({
-    defaultSeverity,
-    ids,
-  }: {
-    defaultSeverity: AlertSeverity;
-    ids: string[];
-  }) => {
-    const businessIdCounterpartyOriginatorIdOrBoth = faker.helpers.arrayElement(
-      ids.map(id => ({ counterpartyId: id })),
-    );
-
-    return {
-      dataTimestamp: faker.date.past(),
-      state: faker.helpers.arrayElement(Object.values(AlertState)),
-      status: faker.helpers.arrayElement(Object.values(AlertStatus)),
-      tags: [faker.random.word(), faker.random.word(), faker.random.word()],
-      executionDetails: JSON.parse(faker.datatype.json()) as InputJsonValue,
-      severity: defaultSeverity,
-      ...businessIdCounterpartyOriginatorIdOrBoth,
-      // TODO: Assign assigneeId value to a valid user id
-      // TODO: Assign counterpart value to a valid user id
-      // businessId: faker.datatype.uuid(),
-      // endUserId: faker.datatype.uuid(),
-      // assigneeId: faker.datatype.uuid(),
-      // workflowRuntimeDataId: faker.datatype.uuid()
-    } satisfies Omit<Prisma.AlertCreateManyAlertDefinitionInput, 'projectId'>;
-  };
-
-  const rules = Object.values(getRuleDefinitions());
+  const alertDefinitions = await generateAlertDefinitions(prisma, {
+    project,
+    createdBy: faker.internet.userName(),
+  });
 
   await Promise.all(
-    rules.map(async rule => {
-      const defaultSeverity = faker.helpers.arrayElement(Object.values(AlertSeverity));
-
-      // Create alerts
-      const alerts = Array.from(
-        {
-          length: faker.datatype.number({ min: 3, max: 5 }),
-        },
-        () => {
-          return {
+    alertDefinitions.map(alertDefinition =>
+      prisma.alert.createMany({
+        data: Array.from(
+          {
+            length: faker.datatype.number({ min: 3, max: 5 }),
+          },
+          () => ({
+            alertDefinitionId: alertDefinition.id,
             projectId: project.id,
             ...generateFakeAlert({
-              defaultSeverity,
               ids,
+              severity: faker.helpers.arrayElement(Object.values(AlertSeverity)),
             }),
-          };
-        },
-      );
-
-      const createdBy = faker.internet.userName();
-
-      await prisma.alertDefinition.create({
-        include: {
-          alert: true,
-        },
-        data: {
-          type: faker.helpers.arrayElement(Object.values(AlertType)) as AlertType,
-          name: faker.lorem.words(3),
-          enabled: faker.datatype.boolean(),
-          description: faker.lorem.sentence(),
-          rulesetId: `set-${rule.id}`,
-          defaultSeverity,
-          ruleId: rule.id,
-          createdBy: createdBy,
-          modifiedBy: createdBy,
-          dedupeStrategies: {
-            strategy: {},
-            cooldownTimeframeInMinutes: faker.datatype.number({ min: 60, max: 3600 }),
-          },
-          config: { config: {} },
-          inlineRule: rule,
-          tags: [faker.helpers.arrayElement(tags), faker.helpers.arrayElement(tags)],
-          additionalInfo: {},
-          projectId: project.id,
-          alert: {
-            createMany: {
-              data: alerts,
-              skipDuplicates: true,
-            },
-          },
-        },
-      });
-    }),
+          }),
+        ),
+        skipDuplicates: true,
+      }),
+    ),
   );
 };
