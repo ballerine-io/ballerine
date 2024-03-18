9caeb60a0ab09e3972d534aab0409fd318d7dce5
Matan Yadaev <matan.yed@gmail.com>
2024-03-18 10:29:45 +0200

    fix: use object property syntax

--- packages/workflow-core/src/lib/logger.ts
+++ packages/workflow-core/src/lib/logger.ts
@@ -1,10 +1,10 @@
 import { AnyRecord } from '@ballerine/common';
 
 export type LoggerInterface = {
-  log(message: string, logData?: AnyRecord): void;
-  error(message: string, logData?: AnyRecord): void;
-  warn(message: string, logData?: AnyRecord): void;
-  debug(message: string, logData?: AnyRecord): void;
+  log: (message: string, logData?: AnyRecord) => void;
+  error: (message: string, logData?: AnyRecord) => void;
+  warn: (message: string, logData?: AnyRecord) => void;
+  debug: (message: string, logData?: AnyRecord) => void;
 };
 
 /* eslint-disable no-console */
