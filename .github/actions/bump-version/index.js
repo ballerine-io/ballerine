83f3b7570008a171ca6a58e7a0dd272a49cec0a5
Lior Zamir <liorz@ballerine.com>
2024-03-27 23:18:20 +0200

    fix: add bump action

---
+++ .github/actions/bump-version/index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+
+async function main() {
+  const lastTag = core.getInput("tag");
+
+  let [component, version] = lastTag.split("@v");
+  version = version.split(".").map((i) => Number.parseInt(i));
+  version[2]++;
+
+  core.setOutput("version", `${component}@v${version.join(".")}`);
+  core.setOutput("tag", `${version.join(".")}`);
+}
+
+main();
