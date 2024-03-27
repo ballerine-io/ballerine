43f80951b0569b1199d46e3dcd8e5b6e70d50681
Lior Zamir <liorz@ballerine.com>
2024-03-27 15:17:50 +0200

    fix: use testcontainer with specific port

--- services/workflows-service/src/test/db-setup.ts
+++ services/workflows-service/src/test/db-setup.ts
@@ -1,17 +1,29 @@
 import { PostgreSqlContainer } from 'testcontainers';
+
 import console from 'console';
 import { TestGlobal } from '@/test/test-global';
 import { execSync } from 'child_process';
+
+const DATANASE_NAME = 'test';
 
 module.exports = async () => {
   if (process.env.SKIP_DB_SETUP_TEARDOWN) return;
 
   const container = await new PostgreSqlContainer('sibedge/postgres-plv8:15.3-3.1.7')
-    .withDatabase('test')
+    .withDatabase(DATANASE_NAME)
+    .withExposedPorts({ host: 5444, container: 5432 })
+    .withHealthCheck({
+      test: ['CMD', 'pg_isready -d test_postgress -U test_user -p 5432'],
+      interval: 10000, // 10 seconds
+      startPeriod: 5000, // 5 seconds
+      retries: 5,
+    })
     .start();
+
   process.env.TEST_DATABASE_SCHEMA_NAME = container.getDatabase();
   process.env.DB_URL = container.getConnectionUri();
-  console.log('\nStarting database container on: ' + container.getConnectionUri());
+
+  console.log('\nStarting database container on: ' + process.env.DB_URL);
 
   runPrismaMigrations();
 
