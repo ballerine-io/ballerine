7364257ed879fd00e8d1050f4296b30a7a7adb26
Lior Zamir <liorz@ballerine.com>
2024-03-28 09:22:14 +0200

    fix: verbose tags

--- .github/actions/bump-version/index.js
+++ .github/actions/bump-version/index.js
@@ -3,9 +3,11 @@ const core = require("@actions/core");
 async function main() {
   const lastTag = core.getInput("tag");
 
+  console.log("Last tag: ", lastTag);
+  
   let [component, version] = lastTag.split("@v");
-  version = version.split(".").map((i) => Number.parseInt(i));
-  version[2]++;
+  let minorVersion = version.split(".").map((i) => Number.parseInt(i));
+  minorVersion[2]++;
 
   core.setOutput("version", `${component}@v${version.join(".")}`);
   core.setOutput("tag", `${version.join(".")}`);
