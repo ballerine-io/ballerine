4a56ffbe4389716827879c637a147ffc6c72529e
Illia Rudniev <cheskmr@gmail.com>
2024-03-31 12:04:23 +0300

    Bal 1740 - E2E Test Ids (#2249)
    * feat: added test ids to inputs & dynamic form adapters & updated storybook
    
    * feat: added missing test ids & ui bump
    
    * fix: added test id to form & renamed story
    
    * fix: added names to json forms in seed & added test id to json form
    
    * fix: fixed kyb tests & renamed data-test-id -> data-testid
    
    * fix: added WithTestId ts util & refactored props
    
    * chore(gh-actions): test
    
    ---------
    
    Co-authored-by: Alon Peretz <Alonp99@gmail.com>

--- packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/TextInputAdapter/components/SelectField/SelectField.tsx
+++ packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/TextInputAdapter/components/SelectField/SelectField.tsx
@@ -1,3 +1,4 @@
+import { WithTestId } from '@/common';
 import { DropdownInput, DropdownOption } from '@/components/molecules';
 import { TOneOfItem } from '@/components/organisms/DynamicForm/types/one-of';
 import { FieldProps } from '@rjsf/utils';
@@ -10,8 +11,9 @@ export const SelectField = ({
   formData,
   uiSchema,
   disabled,
+  testId,
   onBlur,
-}: FieldProps<string>) => {
+}: WithTestId<FieldProps<string>>) => {
   const options = useMemo((): DropdownOption[] => {
     if (!Array.isArray(schema.oneOf)) return [];
 
@@ -37,6 +39,7 @@ export const SelectField = ({
       options={options}
       value={formData}
       disabled={disabled}
+      testId={testId}
       onChange={onChange}
       onBlur={handleBlur}
     />
