f185b0b8a8249cc36c3213c11c18001cdb74cfa2
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-04-10 21:54:20 +0530

    Changes: To handle the fallback

--- apps/backoffice-v2/src/common/components/atoms/Home/Home.tsx
+++ apps/backoffice-v2/src/common/components/atoms/Home/Home.tsx
@@ -1,23 +1,26 @@
 import React, { FunctionComponent, useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { t } from 'i18next';
 import { useAuthenticatedUserQuery } from '../../../../domains/auth/hooks/queries/useAuthenticatedUserQuery/useAuthenticatedUserQuery';
 import { UserAvatar } from '../UserAvatar/UserAvatar';
 
 export const Home: FunctionComponent = () => {
-  const { t } = useTranslation();
   const { data: session } = useAuthenticatedUserQuery();
   const { firstName, fullName, avatarUrl } = session?.user || {};
 
   return (
     <div className={`flex flex-col p-10`}>
       <div className={`flex items-center`}>
-        {session?.user && (
-          <UserAvatar fullName={fullName} className={`mr-2 d-6`} avatarUrl={avatarUrl} />
+        {fullName && (
+          <UserAvatar
+            fullName={fullName}
+            className={`mr-2 d-6`}
+            avatarUrl={!avatarUrl ? undefined : avatarUrl}
+          />
         )}
-        <h3 className={`flex text-2xl font-semibold max-w-[45ch]`}>
+        <h3 className={`flex max-w-[45ch] text-2xl font-semibold`}>
           {t(`home.greeting`)}
-          {session?.user && ` ${firstName}`}
+          {firstName && ` ${firstName}`}
         </h3>
       </div>
       <Outlet />
