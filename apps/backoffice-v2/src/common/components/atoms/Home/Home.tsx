2c9abcfd3a61cb59d1038c38790e86cc58947780
Omri Levy <omri.levy0197@gmail.com>
2024-04-10 19:56:19 +0300

    refactor(backoffice-v2): updated based on dev changes

--- apps/backoffice-v2/src/common/components/atoms/Home/Home.tsx
+++ apps/backoffice-v2/src/common/components/atoms/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Outlet } from 'react-router-dom';
 import { t } from 'i18next';
 import { useAuthenticatedUserQuery } from '../../../../domains/auth/hooks/queries/useAuthenticatedUserQuery/useAuthenticatedUserQuery';
@@ -11,14 +11,12 @@ export const Home: FunctionComponent = () => {
   return (
     <div className={`flex flex-col p-10`}>
       <div className={`flex items-center`}>
-        {fullName && (
-          <UserAvatar
-            fullName={fullName}
-            className={`mr-2 d-6`}
-            avatarUrl={!avatarUrl ? undefined : avatarUrl}
-          />
-        )}
-        <h3 className={`flex max-w-[45ch] text-2xl font-semibold`}>
+        <UserAvatar
+          fullName={fullName ?? ''}
+          className={`mr-2 d-6`}
+          avatarUrl={avatarUrl ?? undefined}
+        />
+        <h3 className={`flex max-w-[45ch] break-all text-2xl font-semibold`}>
           {t(`home.greeting`)}
           {firstName && ` ${firstName}`}
         </h3>
