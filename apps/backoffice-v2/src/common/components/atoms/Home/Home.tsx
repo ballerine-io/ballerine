fdabd6d16b447a0622d7089e752557f51cd88d3e
Shashank Vishwakarma <shashank000s4@gmail.com>
2024-04-11 18:10:17 +0530

    Added: Tabs component

--- apps/backoffice-v2/src/common/components/atoms/Home/Home.tsx
+++ apps/backoffice-v2/src/common/components/atoms/Home/Home.tsx
@@ -1,15 +1,20 @@
 import React, { FunctionComponent } from 'react';
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useParams } from 'react-router-dom';
 import { t } from 'i18next';
 import { useAuthenticatedUserQuery } from '../../../../domains/auth/hooks/queries/useAuthenticatedUserQuery/useAuthenticatedUserQuery';
 import { UserAvatar } from '../UserAvatar/UserAvatar';
+import { Tabs } from '@/common/components/organisms/Tabs/Tabs';
+import { TabsList } from '@/common/components/organisms/Tabs/Tabs.List';
+import { TabsTrigger } from '@/common/components/organisms/Tabs/Tabs.Trigger';
+import { TabsContent } from '@/common/components/organisms/Tabs/Tabs.Content';
 
 export const Home: FunctionComponent = () => {
   const { data: session } = useAuthenticatedUserQuery();
   const { firstName, fullName, avatarUrl } = session?.user || {};
+  const { locale } = useParams();
 
   return (
-    <div className={`flex flex-col p-10`}>
+    <div className={`flex flex-col gap-10 p-10`}>
       <div className={`flex items-center`}>
         <UserAvatar
           fullName={fullName ?? ''}
@@ -21,7 +26,24 @@ export const Home: FunctionComponent = () => {
           {firstName && ` ${firstName}`}
         </h3>
       </div>
-      <Outlet />
+      <div>
+        <Tabs defaultValue="statistics">
+          <TabsList>
+            <TabsTrigger asChild={true} value="statistics">
+              <NavLink to={`/${locale}/statistics`}>Statistics</NavLink>
+            </TabsTrigger>
+            <TabsTrigger asChild={true} value="workflows">
+              <NavLink to={`/${locale}/workflows`}>Workflows</NavLink>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="statistics">
+            <Outlet />
+          </TabsContent>
+          <TabsContent value="workflows">
+            <Outlet />
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 };
