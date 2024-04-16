9fda68e3df81bfbae67ce3feee9de7c0cb38dae2
Tomer Shvadron <tomers@ballerine.com>
2024-04-16 11:51:37 +0300

    feat: transaction details

--- apps/backoffice-v2/src/common/components/atoms/icons/index.tsx
+++ apps/backoffice-v2/src/common/components/atoms/icons/index.tsx
@@ -596,6 +596,32 @@ export const UnassignedAvatarSvg = (props: SVGProps<SVGSVGElement>) => (
   </svg>
 );
 
+export const CopySvg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#clip0_12422_26452)">
+      <path
+        d="M2.66659 10.6668C1.93325 10.6668 1.33325 10.0668 1.33325 9.3335V2.66683C1.33325 1.9335 1.93325 1.3335 2.66659 1.3335H9.33325C10.0666 1.3335 10.6666 1.9335 10.6666 2.66683M6.66658 5.3335H13.3333C14.0696 5.3335 14.6666 5.93045 14.6666 6.66683V13.3335C14.6666 14.0699 14.0696 14.6668 13.3333 14.6668H6.66658C5.93021 14.6668 5.33325 14.0699 5.33325 13.3335V6.66683C5.33325 5.93045 5.93021 5.3335 6.66658 5.3335Z"
+        stroke="#787981"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_12422_26452">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
 export const WarningFilledSvg: FunctionComponent<ComponentProps<'svg'>> = props => {
   return (
     <svg
