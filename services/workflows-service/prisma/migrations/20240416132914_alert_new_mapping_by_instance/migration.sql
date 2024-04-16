db40c917c91790dbe9cebad609cca2a5ac72c9a8
Lior Zamir <liorz@ballerine.com>
2024-04-16 16:30:14 +0300

    fix: fix typo

--- services/workflows-service/prisma/migrations/20240416132914_alert_new_mapping_by_instance/migration.sql
+++ services/workflows-service/prisma/migrations/20240416132914_alert_new_mapping_by_instance/migration.sql
@@ -3,7 +3,7 @@
 
   - You are about to drop the column `businessId` on the `Alert` table. All the data in the column will be lost.
   - You are about to drop the column `counterpartyId` on the `Alert` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[ongoingMarchentAlertId]` on the table `Alert` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[ongoingMerchantAlertId]` on the table `Alert` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[transactionAlertId]` on the table `Alert` will be added. If there are existing duplicate values, this will fail.
 
 */
@@ -19,18 +19,18 @@ DROP INDEX "Alert_counterpartyId_idx";
 -- AlterTable
 ALTER TABLE "Alert" DROP COLUMN "businessId",
 DROP COLUMN "counterpartyId",
-ADD COLUMN     "ongoingMarchentAlertId" TEXT,
+ADD COLUMN     "ongoingMerchantAlertId" TEXT,
 ADD COLUMN     "transactionAlertId" TEXT;
 
 -- CreateTable
-CREATE TABLE "OngoingMarchentAlert" (
+CREATE TABLE "OngoingMerchantAlert" (
     "id" TEXT NOT NULL,
     "businessId" TEXT,
     "businessReportId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "OngoingMarchentAlert_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "OngoingMerchantAlert_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -44,25 +44,25 @@ CREATE TABLE "TransactionAlert" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "OngoingMarchentAlert_businessReportId_key" ON "OngoingMarchentAlert"("businessReportId");
+CREATE UNIQUE INDEX "OngoingMerchantAlert_businessReportId_key" ON "OngoingMerchantAlert"("businessReportId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Alert_ongoingMarchentAlertId_key" ON "Alert"("ongoingMarchentAlertId");
+CREATE UNIQUE INDEX "Alert_ongoingMerchantAlertId_key" ON "Alert"("ongoingMerchantAlertId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Alert_transactionAlertId_key" ON "Alert"("transactionAlertId");
 
 -- AddForeignKey
-ALTER TABLE "OngoingMarchentAlert" ADD CONSTRAINT "OngoingMarchentAlert_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "OngoingMerchantAlert" ADD CONSTRAINT "OngoingMerchantAlert_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "Business"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "OngoingMarchentAlert" ADD CONSTRAINT "OngoingMarchentAlert_businessReportId_fkey" FOREIGN KEY ("businessReportId") REFERENCES "BusinessReport"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "OngoingMerchantAlert" ADD CONSTRAINT "OngoingMerchantAlert_businessReportId_fkey" FOREIGN KEY ("businessReportId") REFERENCES "BusinessReport"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "TransactionAlert" ADD CONSTRAINT "TransactionAlert_counterpartyId_fkey" FOREIGN KEY ("counterpartyId") REFERENCES "Counterparty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Alert" ADD CONSTRAINT "Alert_ongoingMarchentAlertId_fkey" FOREIGN KEY ("ongoingMarchentAlertId") REFERENCES "OngoingMarchentAlert"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Alert" ADD CONSTRAINT "Alert_ongoingMerchantAlertId_fkey" FOREIGN KEY ("ongoingMerchantAlertId") REFERENCES "OngoingMerchantAlert"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Alert" ADD CONSTRAINT "Alert_transactionAlertId_fkey" FOREIGN KEY ("transactionAlertId") REFERENCES "TransactionAlert"("id") ON DELETE SET NULL ON UPDATE CASCADE;
