09c87cd763874edaef838f424ff404959ac785cc
Tomer Shvadron <tomers@ballerine.com>
2024-05-01 14:29:32 +0300

    feat: added new columns in alerts table

--- apps/backoffice-v2/src/domains/alerts/fetchers.ts
+++ apps/backoffice-v2/src/domains/alerts/fetchers.ts
@@ -76,7 +76,12 @@ export const AlertsListSchema = z.array(
   ObjectWithIdSchema.extend({
     dataTimestamp: z.string().datetime(),
     updatedAt: z.string().datetime(),
-    subject: ObjectWithIdSchema.extend({ name: z.string() }),
+    subject: ObjectWithIdSchema.extend({
+      id: z.string(),
+      name: z.string(),
+      correlationId: z.string(),
+      type: z.enum(['business', 'counterparty']),
+    }),
     severity: z.enum(AlertSeverities),
     label: z.string(),
     alertDetails: z.string(),
