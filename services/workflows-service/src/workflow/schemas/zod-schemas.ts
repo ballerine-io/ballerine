7fb50ab34c3297aa17a558a8e3c1753be456cbb2
Omri Levy <omri.levy0197@gmail.com>
2024-05-07 19:36:19 +0300

    fix(workflows-service): fixed bad import in zod-schemas.ts

--- services/workflows-service/src/workflow/schemas/zod-schemas.ts
+++ services/workflows-service/src/workflow/schemas/zod-schemas.ts
@@ -1,6 +1,6 @@
 import { SubscriptionSchema } from '@/common/types';
 import { z } from 'zod';
-import { WorkflowDefinitionConfigThemeSchema } from '../../../../../packages/common/src';
+import { WorkflowDefinitionConfigThemeSchema } from '@ballerine/common';
 
 export const ConfigSchema = z
   .object({
