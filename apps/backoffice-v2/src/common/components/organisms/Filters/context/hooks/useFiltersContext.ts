ab45535391a1080bbd4b39d637c3d24c066011ba
Illia Rudniev <cheskmr@gmail.com>
2024-05-20 13:25:08 +0300

    feat: refactorted filters context

--- apps/backoffice-v2/src/common/components/organisms/Filters/context/hooks/useFiltersContext.ts
+++ apps/backoffice-v2/src/common/components/organisms/Filters/context/hooks/useFiltersContext.ts
@@ -1,4 +1,12 @@
 import { filtersContext } from '@/common/components/organisms/Filters/context/filters.context';
 import { useContext } from 'react';
 
-export const useFiltersContext = () => useContext(filtersContext);
+export const useFiltersContext = () => {
+  const values = useContext(filtersContext);
+
+  if (!values) {
+    throw new Error('useFiltersContext must be used within a FiltersContext.');
+  }
+
+  return values;
+};
