b44b1d9098511e3432811c6b0e99e1d9f5b62775
Illia Rudniev <cheskmr@gmail.com>
2024-05-22 11:48:29 +0300

    fix: fixed type

--- apps/backoffice-v2/src/pages/Entity/pdfs/case-information/components/CaseInformationPageContainer/CaseInformationPageContainer.tsx
+++ apps/backoffice-v2/src/pages/Entity/pdfs/case-information/components/CaseInformationPageContainer/CaseInformationPageContainer.tsx
@@ -1,15 +1,8 @@
+import { FunctionComponentWithChildren } from '@/common/types';
 import { tw } from '@ballerine/react-pdf-toolkit';
-import { AnyChildren } from '@ballerine/ui';
 import { Page, View } from '@react-pdf/renderer';
-import { FunctionComponent } from 'react';
 
-interface ICaseInformationPageContainer {
-  children: AnyChildren;
-}
-
-export const CaseInformationPageContainer: FunctionComponent<ICaseInformationPageContainer> = ({
-  children,
-}) => (
+export const CaseInformationPageContainer: FunctionComponentWithChildren = ({ children }) => (
   <Page wrap={false}>
     <View style={tw('flex flex-col p-5')}>{children}</View>
   </Page>
--- apps/backoffice-v2/src/pages/Entity/pdfs/case-information/components/CaseInformationPageSection/CaseInformationPageSection.tsx
+++ apps/backoffice-v2/src/pages/Entity/pdfs/case-information/components/CaseInformationPageSection/CaseInformationPageSection.tsx
@@ -1,12 +1,7 @@
+import { FunctionComponentWithChildren } from '@/common/types';
 import { tw } from '@ballerine/react-pdf-toolkit';
-import { AnyChildren } from '@ballerine/ui';
 import { View } from '@react-pdf/renderer';
-import { FunctionComponent } from 'react';
 
-interface ICaseInformationPageSectionProps {
-  children: AnyChildren;
-}
-
-export const CaseInformationPageSection: FunctionComponent<ICaseInformationPageSectionProps> = ({
-  children,
-}) => <View style={tw('flex flex-col p-6 rounded-[6px] border border-[#E5E7EB]')}>{children}</View>;
+export const CaseInformationPageSection: FunctionComponentWithChildren = ({ children }) => (
+  <View style={tw('flex flex-col p-6 rounded-[6px] border border-[#E5E7EB]')}>{children}</View>
+);
