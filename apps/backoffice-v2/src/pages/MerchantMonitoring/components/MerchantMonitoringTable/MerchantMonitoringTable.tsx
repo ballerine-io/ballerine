bcf39db7aa4638949d93154f3bd7157ca930113d
Omri Levy <omri.levy0197@gmail.com>
2024-05-29 15:48:50 +0300

    feat(*): added route and link to a single report and added report id

--- apps/backoffice-v2/src/pages/MerchantMonitoring/components/MerchantMonitoringTable/MerchantMonitoringTable.tsx
+++ apps/backoffice-v2/src/pages/MerchantMonitoring/components/MerchantMonitoringTable/MerchantMonitoringTable.tsx
@@ -2,18 +2,20 @@ import React, { FunctionComponent } from 'react';
 import { TBusinessReports } from '@/domains/business-reports/fetchers';
 import { DataTable, IDataTableProps } from '@/common/components/organisms/DataTable/DataTable';
 import { columns } from '@/pages/MerchantMonitoring/components/MerchantMonitoringTable/columns';
+import { Link } from 'react-router-dom';
+import { useLocale } from '@/common/hooks/useLocale/useLocale';
 
 export const MerchantMonitoringTable: FunctionComponent<{
   data: TBusinessReports;
 }> = ({ data }) => {
+  const locale = useLocale();
   const Cell: IDataTableProps<typeof data>['CellContentWrapper'] = ({ cell, children }) => {
+    const itemId = cell.id.replace(`_${cell.column.id}`, '');
+
     return (
-      <span
-        // to={`/${locale}/profiles/individuals/${itemId}${search}`}
-        className={`d-full flex p-4`}
-      >
+      <Link to={`/${locale}/merchant-monitoring/${itemId}`} className={`d-full flex p-4`}>
         {children}
-      </span>
+      </Link>
     );
   };
 
---
+++ apps/backoffice-v2/src/pages/MerchantMonitoringBusinessReport/MerchantMonitoringBusinessReport.page.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from 'react';
+
+export const MerchantMonitoringBusinessReport: FunctionComponent = () => {
+  return <div>MerchantMonitoringBusinessReport</div>;
+};
