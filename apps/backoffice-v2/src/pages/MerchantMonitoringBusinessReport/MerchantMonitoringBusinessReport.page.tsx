34303c9c3b5d55ea52da2a082db77664a9d583c2
Omri Levy <omri.levy0197@gmail.com>
2024-05-29 22:37:55 +0300

    feat(backoffice-v2): mocked all tabs of report view

--- apps/backoffice-v2/src/pages/MerchantMonitoringBusinessReport/MerchantMonitoringBusinessReport.page.tsx
+++ apps/backoffice-v2/src/pages/MerchantMonitoringBusinessReport/MerchantMonitoringBusinessReport.page.tsx
@@ -17,6 +17,9 @@ import { CardContent } from '@/common/components/atoms/Card/Card.Content';
 import { CardHeader } from '@/common/components/atoms/Card/Card.Header';
 import { WarningFilledSvg } from '@/common/components/atoms/icons';
 import { CheckCircle } from '@/common/components/atoms/CheckCircle/CheckCircle';
+import { DataTable } from '@/common/components/organisms/DataTable/DataTable';
+import { createColumnHelper } from '@tanstack/react-table';
+import { TextWithNAFallback } from '@/common/components/atoms/TextWithNAFallback/TextWithNAFallback';
 
 const RiskIndicator = ({
   title,
@@ -163,6 +166,352 @@ const BusinessReportSummary: FunctionComponent = () => {
   );
 };
 
+export const Analysis: FunctionComponent<{
+  companyActivityAnalysis: string[];
+  companyReputationAnalysis: string[];
+  violations: Array<{
+    label: string;
+    severity: string;
+  }>;
+}> = ({ companyActivityAnalysis, companyReputationAnalysis, violations }) => {
+  return (
+    <div className={'space-y-8'}>
+      <Card>
+        <CardHeader className={'pt-4 font-bold'}>Risk Indicators</CardHeader>
+        <CardContent>
+          <ul className="list-inside list-disc">
+            {!!violations.length &&
+              violations.map(violation => (
+                <li key={violation.label} className="flex list-none items-center text-slate-500">
+                  <WarningFilledSvg
+                    className={ctw('me-3 mt-px', {
+                      'text-slate-300 [&>:not(:first-child)]:stroke-background':
+                        violation.severity === 'low',
+                    })}
+                    width={'20'}
+                    height={'20'}
+                  />
+                  {violation.label}
+                </li>
+              ))}
+            {!violations.length && (
+              <li className="flex list-none items-center text-slate-500">
+                <CheckCircle
+                  size={18}
+                  className={`stroke-background`}
+                  containerProps={{
+                    className: 'me-3 bg-success',
+                  }}
+                />
+                No violations found
+              </li>
+            )}
+          </ul>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className={'pt-4 font-bold'}>Company Activity Assessment</CardHeader>
+        <CardContent>
+          <ol className={'ps-4'}>
+            {companyActivityAnalysis.map(warning => (
+              <li key={warning} className={'list-decimal'}>
+                {warning}
+              </li>
+            ))}
+          </ol>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className={'pt-4 font-bold'}>Company Reputation Analysis</CardHeader>
+        <CardContent>
+          <ol className={'ps-4'}>
+            {companyReputationAnalysis.map(warning => (
+              <li key={warning} className={'list-decimal'}>
+                {warning}
+              </li>
+            ))}
+          </ol>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export const WebsitesCompany: FunctionComponent = () => {
+  const companyActivityAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const companyReputationAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const violations = [
+    {
+      label: 'IP Infringement',
+      severity: 'high',
+    },
+  ] as const satisfies ReadonlyArray<{
+    label: string;
+    severity: string;
+  }>;
+
+  return (
+    <Analysis
+      companyActivityAnalysis={companyActivityAnalysis}
+      companyReputationAnalysis={companyReputationAnalysis}
+      violations={violations}
+    />
+  );
+};
+
+export const WebsiteLineOfBusiness: FunctionComponent = () => {
+  const companyActivityAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const companyReputationAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const violations = [
+    {
+      label: 'IP Infringement',
+      severity: 'high',
+    },
+  ] as const satisfies ReadonlyArray<{
+    label: string;
+    severity: string;
+  }>;
+
+  return (
+    <Analysis
+      companyActivityAnalysis={companyActivityAnalysis}
+      companyReputationAnalysis={companyReputationAnalysis}
+      violations={violations}
+    />
+  );
+};
+
+export const WebsiteCredibility: FunctionComponent = () => {
+  const companyActivityAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const companyReputationAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const violations = [
+    {
+      label: 'IP Infringement',
+      severity: 'high',
+    },
+  ] as const satisfies ReadonlyArray<{
+    label: string;
+    severity: string;
+  }>;
+
+  return (
+    <Analysis
+      companyActivityAnalysis={companyActivityAnalysis}
+      companyReputationAnalysis={companyReputationAnalysis}
+      violations={violations}
+    />
+  );
+};
+
+export const AdsAndSocialMedia: FunctionComponent = () => {
+  const companyActivityAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const companyReputationAnalysis = [
+    '"Adolescentm.com is a fraudulent website that lures victims through deceptive promotions."',
+    '"Adolescentm.com has a safety score of 0 out of 100."',
+    '"Highly unusual and suspicious lack of accounts on Adolescentm.com."',
+  ];
+  const violations = [
+    {
+      label: 'IP Infringement',
+      severity: 'high',
+    },
+  ] as const satisfies ReadonlyArray<{
+    label: string;
+    severity: string;
+  }>;
+
+  return (
+    <Analysis
+      companyActivityAnalysis={companyActivityAnalysis}
+      companyReputationAnalysis={companyReputationAnalysis}
+      violations={violations}
+    />
+  );
+};
+
+export const EcosystemAndTransactions: FunctionComponent = () => {
+  const violations = [
+    {
+      label: 'Illegal Substances',
+      severity: 'high',
+    },
+  ] as const satisfies ReadonlyArray<{
+    label: string;
+    severity: string;
+  }>;
+  const data = [
+    {
+      matchedName: 'Email Address',
+      relatedNodeType: 'info@3irdeyeplants.com',
+      relatedNode: 'info@3irdeyeplants.com',
+      indicators: {
+        label: 'Illegal Substances',
+        severity: 'high',
+      },
+    },
+    {
+      matchedName: 'adpey.com',
+      relatedNodeType: 'Phone Number',
+      relatedNode: '+86 15869188024',
+    },
+    {
+      matchedName: 'trypunk.com',
+      relatedNodeType: 'Phone Number',
+      relatedNode: '+86 15869188024',
+    },
+    {
+      matchedName: 'swimsuitmenu.com',
+      relatedNodeType: 'Phone Number',
+      relatedNode: '+86 15869188024',
+    },
+  ];
+  const columnHelper = createColumnHelper<{
+    matchedName: string;
+    relatedNodeType: string;
+    relatedNode: string;
+    indicators: {
+      label: string;
+      severity: string;
+    };
+  }>();
+  const columns = [
+    columnHelper.display({
+      id: 'index',
+      cell: info => {
+        const index = info.cell.row.index + 1;
+
+        return <TextWithNAFallback className={`ps-8`}>{index}</TextWithNAFallback>;
+      },
+      header: 'Match',
+    }),
+    columnHelper.accessor('matchedName', {
+      header: 'Matched Name',
+      cell: info => {
+        const matchedName = info.getValue();
+
+        return <TextWithNAFallback>{matchedName}</TextWithNAFallback>;
+      },
+    }),
+    columnHelper.accessor('relatedNodeType', {
+      header: 'Related Node Type',
+      cell: info => {
+        const relatedNodeType = info.getValue();
+
+        return <TextWithNAFallback>{relatedNodeType}</TextWithNAFallback>;
+      },
+    }),
+    columnHelper.accessor('relatedNode', {
+      header: 'Related Node',
+      cell: info => {
+        const relatedNode = info.getValue();
+
+        return <TextWithNAFallback>{relatedNode}</TextWithNAFallback>;
+      },
+    }),
+    columnHelper.accessor('indicators', {
+      header: 'Indicators',
+      cell: info => {
+        const indicators = info.getValue();
+
+        return (
+          <div className={'flex items-center'}>
+            {indicators && (
+              <WarningFilledSvg
+                className={ctw('me-3 mt-px', {
+                  'text-slate-300 [&>:not(:first-child)]:stroke-background':
+                    indicators?.severity === 'low',
+                })}
+                width={'20'}
+                height={'20'}
+              />
+            )}
+            <TextWithNAFallback>{indicators?.label}</TextWithNAFallback>
+          </div>
+        );
+      },
+    }),
+  ];
+
+  return (
+    <div className={'space-y-8'}>
+      <Card>
+        <CardHeader className={'pt-4 font-bold'}>Risk Indicators</CardHeader>
+        <CardContent>
+          <ul className="list-inside list-disc">
+            {!!violations.length &&
+              violations.map(violation => (
+                <li key={violation.label} className="flex list-none items-center text-slate-500">
+                  <WarningFilledSvg
+                    className={ctw('me-3 mt-px', {
+                      'text-slate-300 [&>:not(:first-child)]:stroke-background':
+                        violation.severity === 'low',
+                    })}
+                    width={'20'}
+                    height={'20'}
+                  />
+                  {violation.label}
+                </li>
+              ))}
+            {!violations.length && (
+              <li className="flex list-none items-center text-slate-500">
+                <CheckCircle
+                  size={18}
+                  className={`stroke-background`}
+                  containerProps={{
+                    className: 'me-3 bg-success',
+                  }}
+                />
+                No violations found
+              </li>
+            )}
+          </ul>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className={'pt-4 font-bold'}>Ecosystem</CardHeader>
+        <CardContent>
+          <DataTable
+            data={data}
+            columns={columns}
+            options={{
+              enableSorting: false,
+            }}
+            props={{ scroll: { className: 'h-full' }, cell: { className: '!p-0' } }}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
 export const MerchantMonitoringBusinessReport: FunctionComponent = () => {
   const tabs = [
     {
@@ -173,27 +522,27 @@ export const MerchantMonitoringBusinessReport: FunctionComponent = () => {
     {
       label: "Website's Company",
       value: 'websitesCompany',
-      content: 'Website content',
+      content: <WebsitesCompany />,
     },
     {
       label: 'Website Line of Business',
       value: 'lineOfBusiness',
-      content: 'Line of Business content',
+      content: <WebsiteLineOfBusiness />,
     },
     {
       label: 'Website Credibility',
       value: 'credibility',
-      content: 'Credibility content',
+      content: <WebsiteCredibility />,
     },
     {
       label: 'Ecosystem and Transactions',
       value: 'ecosystemAndTransactions',
-      content: 'Ecosystem and Transactions content',
+      content: <EcosystemAndTransactions />,
     },
     {
       label: 'Ads and Social Media',
       value: 'adsAndSocialMedia',
-      content: 'Ads and Social Media content',
+      content: <AdsAndSocialMedia />,
     },
   ] as const satisfies ReadonlyArray<{
     label: string;
@@ -262,7 +611,7 @@ export const MerchantMonitoringBusinessReport: FunctionComponent = () => {
         </div>
       </div>
       <Tabs defaultValue={activeTab} className="w-full">
-        <TabsList>
+        <TabsList className={'mb-4'}>
           {tabs.map(tab => (
             <TabsTrigger key={tab.value} value={tab.value} asChild>
               <Link
