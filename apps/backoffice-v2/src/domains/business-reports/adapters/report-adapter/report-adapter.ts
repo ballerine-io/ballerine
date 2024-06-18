c8dcb3302c7bde711a062f0fffb7fdb4650db82a
Omri Levy <omri.levy0197@gmail.com>
2024-06-18 16:33:32 +0300

    fix(backoffice-v2): fixed mapping for website credibility

--- apps/backoffice-v2/src/domains/business-reports/adapters/report-adapter/report-adapter.ts
+++ apps/backoffice-v2/src/domains/business-reports/adapters/report-adapter/report-adapter.ts
@@ -97,22 +97,20 @@ export const reportAdapter = {
       relatedAdsImages: Object.values(data?.report?.data?.socialMedia?.ads ?? {}).map(
         data => data?.pickedAd?.imageUrl,
       ),
-      onlineReputationAnalysis: data?.report?.data?.websiteScamAnalysis?.riskIndicators?.map(
-        ({ name, sourceUrl }) => ({ label: name, url: sourceUrl }),
-      ),
+      onlineReputationAnalysis: data?.report?.data?.transactionLaundering?.scamOrFraud?.indicators
+        ?.filter(({ violation }) => !!violation)
+        ?.map(({ violation, sourceUrl }) => ({ label: violation, url: sourceUrl })),
       summary: data?.report?.data?.summary?.summary,
       riskScore: data?.report?.data?.summary?.riskScore,
       riskLevels: normalizeRiskLevel(data?.report?.data?.summary?.riskLevels ?? {}),
-      companyReputationAnalysis:
-        data?.report?.data?.websiteCompanyAnalysis?.scamOrFraud?.indicators?.map(
-          ({ violation, sourceUrl }) => ({ label: violation, url: sourceUrl }),
-        ),
+      companyReputationAnalysis: data?.report?.data?.websiteCompanyAnalysis?.scamOrFraud?.indicators
+        ?.filter(({ violation }) => !!violation)
+        ?.map(({ violation, sourceUrl }) => ({ label: violation, url: sourceUrl })),
       lineOfBusinessDescription: data?.report?.data?.lineOfBusiness?.lobDescription,
       relatedAdsSummary: data?.report?.data?.socialMedia?.relatedAds?.summary,
-      pricingAnalysis: data?.report?.data?.pricing?.riskIndicators?.map(({ name }) => name),
-      websiteStructureAndContentEvaluation: data?.report?.data?.structure?.riskIndicators?.map(
-        ({ name }) => name,
-      ),
+      pricingAnalysis: data?.report?.data?.transactionLaundering?.pricingAnalysis?.indicators,
+      websiteStructureAndContentEvaluation:
+        data?.report?.data?.transactionLaundering?.websiteStructureEvaluation?.indicators,
       trafficAnalysis: [
         {
           label: 'Estimated Monthly Visits',
@@ -197,22 +195,20 @@ export const reportAdapter = {
       relatedAdsImages: Object.values(data?.report?.data?.socialMedia?.ads ?? {}).map(
         data => data?.pickedAd?.imageUrl,
       ),
-      onlineReputationAnalysis: data?.report?.data?.websiteScamAnalysis?.riskIndicators?.map(
-        ({ name, sourceUrl }) => ({ label: name, url: sourceUrl }),
-      ),
+      onlineReputationAnalysis: data?.report?.data?.transactionLaundering?.scamOrFraud?.indicators
+        ?.filter(({ violation }) => !!violation)
+        ?.map(({ violation, sourceUrl }) => ({ label: violation, url: sourceUrl })),
       summary: data?.report?.data?.summary?.summary,
       riskScore: data?.report?.data?.summary?.riskScore,
       riskLevels: normalizeRiskLevel(data?.report?.data?.summary?.riskLevels ?? {}),
-      companyReputationAnalysis:
-        data?.report?.data?.websiteCompanyAnalysis?.scamOrFraud?.indicators?.map(
-          ({ violation, sourceUrl }) => ({ label: violation, url: sourceUrl }),
-        ),
+      companyReputationAnalysis: data?.report?.data?.websiteCompanyAnalysis?.scamOrFraud?.indicators
+        ?.filter(({ violation }) => !!violation)
+        ?.map(({ violation, sourceUrl }) => ({ label: violation, url: sourceUrl })),
       lineOfBusinessDescription: data?.report?.data?.lineOfBusiness?.lobDescription,
       relatedAdsSummary: data?.report?.data?.socialMedia?.relatedAds?.summary,
-      pricingAnalysis: data?.report?.data?.pricing?.riskIndicators?.map(({ name }) => name),
-      websiteStructureAndContentEvaluation: data?.report?.data?.structure?.riskIndicators?.map(
-        ({ name }) => name,
-      ),
+      pricingAnalysis: data?.report?.data?.transactionLaundering?.pricingAnalysis?.indicators,
+      websiteStructureAndContentEvaluation:
+        data?.report?.data?.transactionLaundering?.websiteStructureEvaluation?.indicators,
       trafficAnalysis: [
         {
           label: 'Estimated Monthly Visits',
--- apps/backoffice-v2/src/pages/MerchantMonitoring/components/MerchantMonitoringTable/columns.tsx
+++ apps/backoffice-v2/src/pages/MerchantMonitoring/components/MerchantMonitoringTable/columns.tsx
@@ -91,7 +91,7 @@ export const columns = [
                 [severityToTextClassName[
                   (severity?.toUpperCase() as keyof typeof severityToClassName) ?? 'DEFAULT'
                 ]]: riskScore || riskScore === 0,
-                'text-destructive': severity === Severity.CRITICAL,
+                'text-destructive': severity === Severity.CRITICAL || severity === Severity.HIGH,
               },
               'py-0.5 font-bold',
             )}
