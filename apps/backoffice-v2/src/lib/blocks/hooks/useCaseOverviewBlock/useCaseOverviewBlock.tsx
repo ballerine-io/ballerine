41eff1a8796369f91035d92a0f8ffa51e2073c95
Omri Levy <omri.levy0197@gmail.com>
2024-06-26 19:19:23 +0300

    feat(*): added a feature flag for risk overview backoffice section

--- apps/backoffice-v2/src/lib/blocks/hooks/useCaseOverviewBlock/useCaseOverviewBlock.tsx
+++ apps/backoffice-v2/src/lib/blocks/hooks/useCaseOverviewBlock/useCaseOverviewBlock.tsx
@@ -1,89 +1,14 @@
-import { useCurrentCaseQuery } from '@/pages/Entity/hooks/useCurrentCaseQuery/useCurrentCaseQuery';
-import { useCasePlugins } from '@/pages/Entity/hooks/useCasePlugins/useCasePlugins';
-import { useProcessTrackerBlock } from '@/lib/blocks/hooks/useProcessTrackerBlock/useProcessTrackerBlock';
-import { DEFAULT_PROCESS_TRACKER_PROCESSES } from '@/common/components/molecules/ProcessTracker/constants';
 import { createBlocksTyped } from '@/lib/blocks/create-blocks-typed/create-blocks-typed';
-import { OverallRiskLevel } from '@/common/components/molecules/OverallRiskLevel/OverallRiskLevel';
-import { RiskIndicatorsSummary } from '@/common/components/molecules/RiskIndicatorsSummary/RiskIndicatorsSummary';
-import React, { useCallback } from 'react';
-import { titleCase } from 'string-ts';
-import { toRiskLabels } from '@/domains/business-reports/adapters/report-adapter/report-adapter';
-import { CaseTabs } from '@/common/hooks/useSearchParamsByEntity/validation-schemas';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { CaseOverview } from '@/pages/Entity/components/Case/components/CaseOverview/CaseOverview';
+import { DEFAULT_PROCESS_TRACKER_PROCESSES } from '@/common/components/molecules/ProcessTracker/constants';
 
 export const useCaseOverviewBlock = () => {
-  const { search } = useLocation();
-  const { data: workflow } = useCurrentCaseQuery();
-  const plugins = useCasePlugins({ workflow });
-  const processTrackerBlock = useProcessTrackerBlock({
-    workflow,
-    plugins,
-    processes: DEFAULT_PROCESS_TRACKER_PROCESSES,
-  });
-  const getUpdatedSearchParamsWithActiveTab = useCallback(
-    ({ tab }: { tab: string }) => {
-      const searchParams = new URLSearchParams(search);
-
-      searchParams.set('activeTab', tab);
-
-      return searchParams.toString();
-    },
-    [search],
-  );
-  const riskIndicators = Object.entries(
-    workflow?.context?.pluginsOutput?.risk?.data?.summary?.riskIndicatorsByDomain ?? {},
-  )?.map(([domain, riskIndicators]) => {
-    const isValidCaseTab = CaseTabs.includes(domain);
-
-    return {
-      title: titleCase(domain ?? ''),
-      search: isValidCaseTab
-        ? getUpdatedSearchParamsWithActiveTab({
-            tab: domain,
-          })
-        : undefined,
-      violations: toRiskLabels(riskIndicators),
-    };
-  }) satisfies Array<{
-    title: string;
-    search: string | undefined;
-    violations: Array<{
-      label: string;
-      severity: string;
-    }>;
-  }>;
-  const overallRiskLevelBlock = createBlocksTyped()
-    .addBlock()
-    .addCell({
-      type: 'node',
-      value: (
-        <OverallRiskLevel
-          riskScore={workflow?.context?.pluginsOutput?.risk?.data?.summary?.riskScore}
-          riskLevels={workflow?.context?.pluginsOutput?.risk?.data?.summary?.riskLevels}
-        />
-      ),
-    })
-    .build();
-  const riskIndicatorsBlock = createBlocksTyped()
-    .addBlock()
-    .addCell({
-      type: 'node',
-      value: <RiskIndicatorsSummary riskIndicators={riskIndicators} />,
-    })
-    .build();
-
   return createBlocksTyped()
     .addBlock()
     .addCell({
-      type: 'container',
-      props: {
-        className: 'grid grid-cols-4 gap-4',
-      },
-      value: [
-        ...overallRiskLevelBlock.flat(1),
-        ...processTrackerBlock.flat(1),
-        ...riskIndicatorsBlock.flat(1),
-      ],
+      type: 'node',
+      value: <CaseOverview processes={DEFAULT_PROCESS_TRACKER_PROCESSES} />,
     })
     .build();
 };
---
+++ apps/backoffice-v2/src/pages/Entity/components/Case/components/CaseOverview/CaseOverview.tsx
@@ -0,0 +1,67 @@
+import { useLocation } from 'react-router-dom';
+import { useCurrentCaseQuery } from '@/pages/Entity/hooks/useCurrentCaseQuery/useCurrentCaseQuery';
+import { useCasePlugins } from '@/pages/Entity/hooks/useCasePlugins/useCasePlugins';
+import React, { useCallback } from 'react';
+import { CaseTabs } from '@/common/hooks/useSearchParamsByEntity/validation-schemas';
+import { titleCase } from 'string-ts';
+import { toRiskLabels } from '@/domains/business-reports/adapters/report-adapter/report-adapter';
+import { OverallRiskLevel } from '@/common/components/molecules/OverallRiskLevel/OverallRiskLevel';
+import { ProcessTracker } from '@/common/components/molecules/ProcessTracker/ProcessTracker';
+import { RiskIndicatorsSummary } from '@/common/components/molecules/RiskIndicatorsSummary/RiskIndicatorsSummary';
+
+export const CaseOverview = ({ processes }: { processes: string[] }) => {
+  const { search } = useLocation();
+  const { data: workflow } = useCurrentCaseQuery();
+  const plugins = useCasePlugins({ workflow });
+  const getUpdatedSearchParamsWithActiveTab = useCallback(
+    ({ tab }: { tab: string }) => {
+      const searchParams = new URLSearchParams(search);
+
+      searchParams.set('activeTab', tab);
+
+      return searchParams.toString();
+    },
+    [search],
+  );
+  const riskIndicators = Object.entries(
+    workflow?.context?.pluginsOutput?.risk?.data?.summary?.riskIndicatorsByDomain ?? {},
+  )?.map(([domain, riskIndicators]) => {
+    const isValidCaseTab = CaseTabs.includes(domain);
+
+    return {
+      title: titleCase(domain ?? ''),
+      search: isValidCaseTab
+        ? getUpdatedSearchParamsWithActiveTab({
+            tab: domain,
+          })
+        : undefined,
+      violations: toRiskLabels(riskIndicators),
+    };
+  }) satisfies Array<{
+    title: string;
+    search: string | undefined;
+    violations: Array<{
+      label: string;
+      severity: string;
+    }>;
+  }>;
+
+  if (!workflow?.workflowDefinition?.config?.isCaseOverviewEnabled) {
+    return;
+  }
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {workflow?.workflowDefinition?.config?.isCaseRiskOverviewEnabled && (
+        <OverallRiskLevel
+          riskScore={workflow?.context?.pluginsOutput?.risk?.data?.summary?.riskScore}
+          riskLevels={workflow?.context?.pluginsOutput?.risk?.data?.summary?.riskLevels}
+        />
+      )}
+      <ProcessTracker workflow={workflow} plugins={plugins} processes={processes} />
+      {workflow?.workflowDefinition?.config?.isCaseRiskOverviewEnabled && (
+        <RiskIndicatorsSummary riskIndicators={riskIndicators} />
+      )}
+    </div>
+  );
+};
