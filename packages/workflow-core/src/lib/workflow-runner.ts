81c99ff8f5c489402fe0281d5338ee4f07e0714a
Lior Zamir <liorz@ballerine.com>
2024-07-22 15:42:19 +0300

    chore: fix import path

--- packages/workflow-core/src/lib/workflow-runner.ts
+++ packages/workflow-core/src/lib/workflow-runner.ts
@@ -56,7 +56,7 @@ import { KybPlugin } from './plugins/external-plugin/kyb-plugin';
 import { KycSessionPlugin } from './plugins/external-plugin/kyc-session-plugin';
 import { EmailPlugin } from './plugins/external-plugin/email-plugin';
 import { RiskRulePlugin } from './plugins/common-plugin/risk-rules-plugin';
-import { SanctionsScreeningPlugin } from './plugins/common-plugin/sanctions-screening.plugin';
+import { SanctionsScreeningPlugin } from './plugins/common-plugin/sanctions-screening-plugin';
 import {
   TransformerPlugin,
   TransformerPluginParams,
