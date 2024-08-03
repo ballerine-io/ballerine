c0d7e98a3d33ae63a541392ed171f68f51b53a44
Illia Rudniev <cheskmr@gmail.com>
2024-08-03 18:53:49 +0300

    feat: added error handling for xstate visualizer (#2601)
    Co-authored-by: Alon Peretz <8467965+alonp99@users.noreply.github.com>

--- apps/workflows-dashboard/src/components/organisms/XstateVisualizer/XstateVisualizer.tsx
+++ apps/workflows-dashboard/src/components/organisms/XstateVisualizer/XstateVisualizer.tsx
@@ -2,6 +2,7 @@ import { deserializeStateDefinition } from '@/components/organisms/XstateVisuali
 import { inspect } from '@xstate/inspect';
 import { useInterpret, useMachine } from '@xstate/react';
 import { memo, useLayoutEffect, useMemo, useRef } from 'react';
+import { withErrorBoundary } from 'react-error-boundary';
 import { createMachine } from 'xstate';
 
 interface Props {
@@ -9,38 +10,45 @@ interface Props {
   state?: string;
 }
 
-export const XstateVisualizer = memo(({ stateDefinition, state }: Props) => {
-  const _machine = useMemo(
-    () =>
-      createMachine(
-        deserializeStateDefinition({
-          ...stateDefinition,
-          initial: state || stateDefinition.initial,
-        }),
-      ),
-    [stateDefinition, state],
-  );
-  const [stateMachine] = useMachine(_machine);
+export const XstateVisualizer = memo(
+  withErrorBoundary(
+    ({ stateDefinition, state }: Props) => {
+      const _machine = useMemo(
+        () =>
+          createMachine(
+            deserializeStateDefinition({
+              ...stateDefinition,
+              initial: state || stateDefinition.initial,
+            }),
+          ),
+        [stateDefinition, state],
+      );
+      const [stateMachine] = useMachine(_machine);
 
-  useInterpret(_machine, { devTools: true });
+      useInterpret(_machine, { devTools: true });
 
-  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+      const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
-  useLayoutEffect(() => {
-    if (!iframeRef.current) return;
+      useLayoutEffect(() => {
+        if (!iframeRef.current) return;
 
-    inspect({ iframe: iframeRef.current });
-  }, [iframeRef, state, stateMachine.value]);
+        inspect({ iframe: iframeRef.current });
+      }, [iframeRef, state, stateMachine.value]);
 
-  return (
-    <div className="h-full w-full">
-      <iframe
-        ref={iframeRef}
-        data-xstate
-        style={{ width: 'calc(100% + clamp(40rem, 40rem + 0px, 100%))' }}
-        // width="100%"
-        height="100%"
-      />
-    </div>
-  );
-});
+      return (
+        <div className="h-full w-full">
+          <iframe
+            ref={iframeRef}
+            data-xstate
+            style={{ width: 'calc(100% + clamp(40rem, 40rem + 0px, 100%))' }}
+            // width="100%"
+            height="100%"
+          />
+        </div>
+      );
+    },
+    {
+      fallbackRender: ({ error }) => <span>{error.message}</span>,
+    },
+  ),
+);
