afa33f33acd4810e32b8f476eb32bb2a8338b85d
Alon Peretz <8467965+alonp99@users.noreply.github.com>
2024-08-05 00:31:45 +0300

    chore(sync): fix cross env keys for local

--- apps/workflows-dashboard/src/main.tsx
+++ apps/workflows-dashboard/src/main.tsx
@@ -3,6 +3,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import posthog from 'posthog-js';
+
+if (
+  // !window.location.host.includes('127.0.0.1') &&
+  // !window.location.host.includes('localhost') &&
+  import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY &&
+  import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST
+) {
+  posthog.init(import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY!, {
+    api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST!,
+    person_profiles: 'identified_only',
+    loaded: ph => {
+      ph.register_for_session({ environment: import.meta.env.VITE_ENVIRONMENT_NAME });
+    },
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
