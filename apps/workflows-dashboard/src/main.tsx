96c6b6505aa216836511ffdab56f7be9e99d362e
Omri Levy <61207713+Omri-Levy@users.noreply.github.com>
2024-08-06 12:32:35 +0300

    Added sentry to front-end projects (#2616)
    * feat(*): added sentry to front projects
    
    * fix(*): removed mistakes from pr
    
    * fix(backoffice-v2): removed localhost from sentry array
    
    * refactor(*): moved monitoring to a function

--- apps/workflows-dashboard/src/main.tsx
+++ apps/workflows-dashboard/src/main.tsx
@@ -3,24 +3,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import posthog from 'posthog-js';
+import { initializeMonitoring } from '@/initialize-monitoring/initialize-monitoring';
 
-if (
-  !window.location.host.includes('127.0.0.1') &&
-  !window.location.host.includes('localhost') &&
-  import.meta.env.VITE_POSTHOG_KEY &&
-  import.meta.env.VITE_POSTHOG_HOST
-) {
-  posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-    api_host: import.meta.env.VITE_POSTHOG_HOST,
-    person_profiles: 'identified_only',
-    loaded: ph => {
-      ph.register_for_session({ environment: import.meta.env.VITE_ENVIRONMENT_NAME });
-    },
-  });
-}
+initializeMonitoring();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
