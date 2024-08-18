1e3dbceb54c84293ec3d2cdbd2201fcc8200a0d0
Pratapa Lakshmi <gouthampratapa8@gmail.com>
2024-08-18 22:55:45 +0530

    update config.js

--- apps/backoffice-v2/public/config.js
+++ apps/backoffice-v2/public/config.js
@@ -1,3 +1,11 @@
 globalThis.env = {
   VITE_API_URL: 'http://google.com',
+  VITE_API_KEY: 'secret',
+  VITE_AUTH_ENABLED: true,
+  VITE_MOCK_SERVER: false,
+  VITE_POLLING_INTERVAL: 10,
+  VITE_ASSIGNMENT_POLLING_INTERVAL: 5,
+  VITE_FETCH_SIGNED_URL: false,
+  VITE_ENVIRONMENT_NAME: "local",
+  MODE: "production"
 };
