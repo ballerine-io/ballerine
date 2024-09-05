062753112c09e1369c640166803a469023ec48c3
Illia Rudniev <cheskmr@gmail.com>
2024-09-05 13:52:27 +0300

    feat: added clear field functionality

--- apps/kyb-app/src/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/FileUploaderField.tsx
+++ apps/kyb-app/src/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/FileUploaderField.tsx
@@ -2,8 +2,8 @@ import { useFileAssigner } from '@/components/organisms/UIRenderer/elements/JSON
 import { useFileRepository } from '@/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/hooks/useFileRepository';
 import { useFileUploading } from '@/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/hooks/useFileUploading';
 import { DocumentUploadFieldProps } from '@/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/types';
-import { Input } from '@ballerine/ui';
-import { Upload } from 'lucide-react';
+import { Button, Input } from '@ballerine/ui';
+import { Upload, XCircle } from 'lucide-react';
 import { useCallback, useRef } from 'react';
 
 export const FileUploaderField = ({
@@ -47,9 +47,23 @@ export const FileUploaderField = ({
     inputRef.current?.click();
   }, [inputRef]);
 
+  const handleClear = useCallback(
+    (event: React.SyntheticEvent) => {
+      event.stopPropagation();
+
+      if (!registeredFile || !fileId || !inputRef.current) return;
+
+      inputRef.current.value = '';
+
+      removeFile(fileId);
+      onChange(fileId, true);
+    },
+    [fileId, inputRef, registeredFile, removeFile, onChange],
+  );
+
   return (
     <div
-      className="flex h-[56px] flex-row items-center gap-3 rounded-[16px] border bg-white px-4"
+      className="relative flex h-[56px] flex-row items-center gap-3 rounded-[16px] border bg-white px-4"
       onClick={handdleContainerClick}
     >
       <div className="flex gap-3 text-[#007AFF]">
@@ -59,6 +73,18 @@ export const FileUploaderField = ({
       <span className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
         {registeredFile ? registeredFile.name : 'No File Choosen'}
       </span>
+      {registeredFile && (
+        <Button
+          variant="ghost"
+          size="icon"
+          className="top-[calc(50% - 14px)] absolute right-[-36px] h-[28px]  w-[28px] rounded-full"
+          onClick={handleClear}
+        >
+          <div className="rounded-full bg-white">
+            <XCircle />
+          </div>
+        </Button>
+      )}
       <Input
         data-testid={testId}
         type="file"
