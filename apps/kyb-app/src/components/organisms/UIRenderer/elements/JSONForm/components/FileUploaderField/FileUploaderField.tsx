778877c27f994366e446b484998ef2329f8dd93f
Illia Rudniev <cheskmr@gmail.com>
2024-09-05 12:53:36 +0300

    feat: reworked document field design

--- apps/kyb-app/src/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/FileUploaderField.tsx
+++ apps/kyb-app/src/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/FileUploaderField.tsx
@@ -3,6 +3,7 @@ import { useFileRepository } from '@/components/organisms/UIRenderer/elements/JS
 import { useFileUploading } from '@/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/hooks/useFileUploading';
 import { DocumentUploadFieldProps } from '@/components/organisms/UIRenderer/elements/JSONForm/components/FileUploaderField/types';
 import { Input } from '@ballerine/ui';
+import { Upload } from 'lucide-react';
 import { useCallback, useRef } from 'react';
 
 export const FileUploaderField = ({
@@ -41,16 +42,33 @@ export const FileUploaderField = ({
     [uploadFile, registerFile, onChange],
   );
 
+  const handdleContainerClick = useCallback(() => {
+    inputRef.current?.click();
+  }, [inputRef]);
+
   return (
-    <Input
-      data-testid={testId}
-      type="file"
-      placeholder={placeholder}
-      accept={acceptFileFormats}
-      disabled={disabled || isLoading || isUploading}
-      onChange={handleChange}
-      onBlur={onBlur}
-      ref={inputRef}
-    />
+    <div
+      className="flex h-[56px] flex-row items-center gap-3 rounded-[16px] border bg-white px-4"
+      onClick={handdleContainerClick}
+    >
+      <div className="flex gap-3 text-[#007AFF]">
+        <Upload />
+        <span className="select-none whitespace-nowrap text-base font-bold">Choose file</span>
+      </div>
+      <span className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+        {registeredFile ? registeredFile.name : 'No File Choosen'}
+      </span>
+      <Input
+        data-testid={testId}
+        type="file"
+        placeholder={placeholder}
+        accept={acceptFileFormats}
+        disabled={disabled || isLoading || isUploading}
+        onChange={handleChange}
+        onBlur={onBlur}
+        ref={inputRef}
+        className="hidden"
+      />
+    </div>
   );
 };
