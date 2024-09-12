6f34bde42f0675efae3bc9ff736b4331fb6164a1
Alon Peretz <8467965+alonp99@users.noreply.github.com>
2024-09-12 22:07:44 +0300

    Revert "Chatbot css fix (#2686)"
    This reverts commit 18f6d462fc83c7c2a417aafa0fd61e40187a797e.

--- apps/backoffice-v2/src/common/components/atoms/Portal/Portal.tsx
+++
@@ -1,8 +0,0 @@
-import { FunctionComponentWithChildren } from '@ballerine/ui';
-import { createPortal } from 'react-dom';
-
-export const Portal: FunctionComponentWithChildren<{
-  target: Parameters<typeof createPortal>[1];
-}> = ({ children, target }) => {
-  return createPortal(children, target);
-};
