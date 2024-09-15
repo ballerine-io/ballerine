2226e589b79a2f17d1f879da69158f68b45023ae
Tomer Shvadron <tomers@ballerine.com>
2024-09-15 17:45:10 +0300

    Blocks data sorting (#2709)
    * feat: blocks data sorting
    
    * feat: pR comments fix

--- apps/backoffice-v2/src/lib/blocks/components/ReadOnlyDetailsCell/ReadOnlyDetailsCell.tsx
+++ apps/backoffice-v2/src/lib/blocks/components/ReadOnlyDetailsCell/ReadOnlyDetailsCell.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from 'react';
-import { ctw } from '../../../../common/utils/ctw/ctw';
+import { ctw } from '@/common/utils/ctw/ctw';
 import { ExtractCellProps } from '@ballerine/blocks';
 import { ReadOnlyDetail } from '@/common/components/atoms/ReadOnlyDetail/ReadOnlyDetail';
 import { titleCase } from 'string-ts';
@@ -20,21 +20,24 @@ export const ReadOnlyDetailsCell: FunctionComponent<ExtractCellProps<'readOnlyDe
       {...restProps}
       className={ctw(`grid grid-cols-1 gap-4 p-4 md:grid-cols-2 xl:grid-cols-3`, className)}
     >
-      {value?.map(({ label, value }) => {
-        return (
-          <div key={label} className="flex flex-col">
-            <TextWithNAFallback as={'h4'} className={'mb-2 text-sm font-medium leading-none'}>
-              {titleCase(label ?? '')}
-            </TextWithNAFallback>
-            <ReadOnlyDetail
-              parse={parse}
-              className={'max-w-[35ch] justify-start break-all text-sm'}
-            >
-              {value}
-            </ReadOnlyDetail>
-          </div>
-        );
-      })}
+      {value
+        ?.slice()
+        ?.sort((a, b) => a.label.localeCompare(b.label))
+        .map(({ label, value }) => {
+          return (
+            <div key={label} className="flex flex-col">
+              <TextWithNAFallback as={'h4'} className={'mb-2 text-sm font-medium leading-none'}>
+                {titleCase(label ?? '')}
+              </TextWithNAFallback>
+              <ReadOnlyDetail
+                parse={parse}
+                className={'max-w-[35ch] justify-start break-all text-sm'}
+              >
+                {value}
+              </ReadOnlyDetail>
+            </div>
+          );
+        })}
     </div>
   );
 };
