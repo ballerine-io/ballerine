28d3b25441a3136bd7958795c9c2b0c289e87eba
Illia Rudniev <cheskmr@gmail.com>
2024-09-28 13:45:06 +0300

    feat: implemented visibleOn for nested inputs

--- apps/kyb-app/src/components/organisms/UIRenderer/hooks/useUIElementProps/useUIElementProps.ts
+++ apps/kyb-app/src/components/organisms/UIRenderer/hooks/useUIElementProps/useUIElementProps.ts
@@ -1,26 +1,38 @@
 import { useStateManagerContext } from '@/components/organisms/DynamicUI/StateManager/components/StateProvider';
 import { useDynamicUIContext } from '@/components/organisms/DynamicUI/hooks/useDynamicUIContext';
 import { useRuleExecutor } from '@/components/organisms/DynamicUI/hooks/useRuleExecutor';
+import { injectIndexesAtRulesPaths } from '@/components/organisms/UIRenderer/hooks/useUIElementProps/helpers';
 import { useUIElementState } from '@/components/organisms/UIRenderer/hooks/useUIElementState';
 import { UIElement } from '@/domains/collection-flow';
 import { AnyObject } from '@ballerine/ui';
 import { useMemo } from 'react';
 
-export const useUIElementProps = (definition: UIElement<AnyObject>) => {
+export const useUIElementProps = (
+  definition: UIElement<AnyObject>,
+  index: number | null = null,
+) => {
   const { payload } = useStateManagerContext();
   const { state } = useDynamicUIContext();
+  const availabilityRules = useMemo(() => {
+    return injectIndexesAtRulesPaths(definition.availableOn || [], index);
+  }, [definition, index]);
+
+  const visibilityRules = useMemo(() => {
+    return injectIndexesAtRulesPaths(definition.visibleOn || [], index);
+  }, [definition, index]);
+
   const [availabilityTestResulsts, visibilityTestResults] = [
     useRuleExecutor(
       payload,
       // @ts-ignore
-      definition.availableOn,
+      availabilityRules,
       definition,
       state,
     ),
     useRuleExecutor(
       payload,
       // @ts-ignore
-      definition.visibleOn,
+      visibilityRules,
       definition,
       state,
     ),
