9c217821e76566f9f8f2522195214650dd3440be
Illia Rudniev <cheskmr@gmail.com>
2024-10-02 13:31:46 +0300

    Illiar/feat/kyb theme customization (#2744)
    * feat: added stepper customization & updated pallete classes
    
    * feat: reworked document field design
    
    * feat: added clear field functionality
    
    * feat: implemented dynamic powered by logo & added white poweredby
    
    * fix: fixed & clean
    
    * feat: refactor
    
    * feat: added logo to theme definition
    
    * feat: implemented visibleOn for nested inputs
    
    * fix: fixed typos
    
    * fix: deleted theme
    
    * feat: added theme to ui def
    
    * fix: fixed types & theme provider & added migration for theme
    
    * fix: fixed lucide icons version
    
    * feat: refactor & updated favico

--- apps/kyb-app/src/utils/transform-theme-to-inline-styles.ts
+++ apps/kyb-app/src/utils/transform-theme-to-inline-styles.ts
@@ -1,20 +1,28 @@
 import { ITheme } from '@/common/types/settings';
 
-function createInlineVariable(key: string, value: string): string {
+const createInlineVariable = (key: string, value: string) => {
   return `--${key}: ${value};`;
-}
+};
 
 export const transformThemeToInlineStyles = (theme: ITheme): string => {
   let styles = '';
 
-  Object.entries(theme.pallete).forEach(([variableKey, value]) => {
+  Object.entries(theme.palette).forEach(([variableKey, value]) => {
     styles += createInlineVariable(variableKey, value.color);
     styles += createInlineVariable(`${variableKey}-foreground`, value.foreground);
   });
 
-  Object.entries(theme.elements).forEach(([variableKey, value]) => {
-    styles += createInlineVariable(variableKey, value);
-  });
+  const buildInlineVariableForElements = (elements: ITheme['elements'], path?: string) => {
+    Object.entries(elements).forEach(([variableKey, value]) => {
+      if (typeof value === 'string') {
+        styles += createInlineVariable(`${path ? `${path}-` : ''}${variableKey}`, value);
+      } else {
+        buildInlineVariableForElements(value, `${path ? `${path}-` : ''}${variableKey}`);
+      }
+    });
+  };
+
+  buildInlineVariableForElements(theme.elements);
 
   return styles;
 };
