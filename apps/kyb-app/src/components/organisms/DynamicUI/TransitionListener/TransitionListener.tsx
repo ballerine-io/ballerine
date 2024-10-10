5285de8e686178dbed52c42d11edf7a44380ed06
Alon Peretz <8467965+alonp99@users.noreply.github.com>
2024-10-10 17:31:58 +0300

    Revert "fix: reworked process tracking (#2765)"
    This reverts commit b1e17f5a26b914158eeceb23cd6ef6b0b3f85841.

--- apps/kyb-app/src/components/organisms/DynamicUI/TransitionListener/TransitionListener.tsx
+++ apps/kyb-app/src/components/organisms/DynamicUI/TransitionListener/TransitionListener.tsx
@@ -1,7 +1,6 @@
+import { usePageResolverContext } from '@/components/organisms/DynamicUI/PageResolver/hooks/usePageResolverContext';
 import { useStateManagerContext } from '@/components/organisms/DynamicUI/StateManager/components/StateProvider';
 import { useUIElementToolsLogic } from '@/components/organisms/DynamicUI/hooks/useUIStateLogic/hooks/useUIElementsStateLogic/hooks/useUIElementToolsLogic';
-import { UIPage } from '@/domains/collection-flow';
-import { useRefValue } from '@/hooks/useRefValue';
 import { AnyChildren } from '@ballerine/ui';
 import { useEffect, useRef } from 'react';
 
@@ -12,49 +11,37 @@ export interface TransitionListenerTools {
 export interface TransitionListenerProps {
   onPrevious?: (tools: TransitionListenerTools, prevState: string, currentState: string) => void;
   onNext?: (tools: TransitionListenerTools, prevState: string, currentState: string) => void;
-  onFinish?: (tools: TransitionListenerTools, currentState: string) => void;
-  pages: UIPage[];
-  children: AnyChildren | ((tools: TransitionListenerTools) => AnyChildren);
+  children: AnyChildren;
 }
 
-export const TransitionListener = ({
-  onPrevious,
-  onNext,
-  onFinish,
-  children,
-  pages,
-}: TransitionListenerProps) => {
+export const TransitionListener = ({ onPrevious, onNext, children }: TransitionListenerProps) => {
   const { state } = useStateManagerContext();
+  const { pages } = usePageResolverContext();
   const { setElementCompleted } = useUIElementToolsLogic('');
 
   const prevStateRef = useRef(state);
-  const helpersRef = useRefValue(setElementCompleted);
+  const helpersRef = useRef({ setElementCompleted });
+
+  useEffect(() => {
+    helpersRef.current = {
+      setElementCompleted,
+    };
+  }, [setElementCompleted]);
 
   useEffect(() => {
     const currentPageIndex = pages.findIndex(page => page.stateName === state);
     const prevPageIndex = pages.findIndex(page => page.stateName === prevStateRef.current);
 
     if (currentPageIndex < prevPageIndex) {
-      onPrevious &&
-        onPrevious({ setElementCompleted: helpersRef.current }, prevStateRef.current, state);
+      onPrevious && onPrevious(helpersRef.current, prevStateRef.current, state);
     }
 
     if (currentPageIndex > prevPageIndex) {
-      onNext && onNext({ setElementCompleted: helpersRef.current }, prevStateRef.current, state);
+      onNext && onNext(helpersRef.current, prevStateRef.current, state);
     }
 
-    if (currentPageIndex !== -1) {
-      prevStateRef.current = state;
-    }
+    prevStateRef.current = state;
   }, [prevStateRef, helpersRef, state, pages]);
 
-  useEffect(() => {
-    const currentPageIndex = pages.findIndex(page => page.stateName === state);
-
-    if (currentPageIndex === -1) {
-      onFinish && onFinish({ setElementCompleted: helpersRef.current }, prevStateRef.current);
-    }
-  }, [state, pages, helpersRef, prevStateRef]);
-
-  return <>{typeof children === 'function' ? children({ setElementCompleted }) : children}</>;
+  return <>{children}</>;
 };
