943b501d4f79c3d4ef82a8535251393178f2f061
Illia Rudniev <cheskmr@gmail.com>
2024-11-07 17:17:58 +0200

    fix: fixed loading screen loader color

--- apps/kyb-app/src/pages/CollectionFlow/components/atoms/LoadingScreen/LoadingScreen.tsx
+++ apps/kyb-app/src/pages/CollectionFlow/components/atoms/LoadingScreen/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 export const LoadingScreen = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <Loader2 className="text-primary-foreground animate-spin" size={'48'} />
+      <Loader2 className="text-black animate-spin" size={'48'} />
     </div>
   );
 };
