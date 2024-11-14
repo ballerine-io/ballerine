ddeb20ec171dadc1257d1061214895ed923b1728
Tomer Shvadron <tomers@ballerine.com>
2024-11-14 15:52:39 +0200

    feat: revert db cleanup fix

--- services/workflows-service/src/test/helpers/database-helper.ts
+++ services/workflows-service/src/test/helpers/database-helper.ts
@@ -11,21 +11,20 @@ const TEST_DATABASE_SCHEMA_NAME = z
 const __getTables = async (prisma: PrismaClient): Promise<string[]> => {
   const results: Array<{
     tablename: string;
-  }> = await prisma.$queryRawUnsafe(
-    `SELECT tablename from pg_tables where schemaname = '${TEST_DATABASE_SCHEMA_NAME}'`,
-  );
+  }> =
+    await prisma.$queryRaw`SELECT tablename from pg_tables where schemaname = '${TEST_DATABASE_SCHEMA_NAME}'`;
 
   return results.map(result => result.tablename);
 };
 
 const __removeAllTableContent = async (prisma: PrismaClient, tableNames: string[]) => {
-  await prisma.$executeRawUnsafe(`SET session_replication_role = replica;`);
+  // await prisma.$executeRawUnsafe(`SET session_replication_role = replica;`);
 
   for (const table of tableNames) {
     await prisma.$executeRawUnsafe(`DELETE FROM ${TEST_DATABASE_SCHEMA_NAME}."${table}" CASCADE;`);
   }
 
-  await prisma.$executeRawUnsafe(`SET session_replication_role = DEFAULT;`);
+  // await prisma.$executeRawUnsafe(`SET session_replication_role = DEFAULT;`);
 };
 
 //should be implemented in BeforeEach hook
