67df3720f97cacdadd890cb4ebf3d0f9aba0e20c
Tomer Shvadron <tomers@ballerine.com>
2024-11-14 15:05:15 +0200

    feat: added bad migration fix

--- services/workflows-service/prisma/migrations/20241114124909_fix_bad_migration/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "TransactionRecord_transactionDate_desc_idx";
-
--- DropIndex
-DROP INDEX "TransactionRecord_transactionDirection_idx";
--- services/workflows-service/prisma/migrations/20241114130413_added_transaction_direction_index/migration.sql
+++ services/workflows-service/prisma/migrations/20241114130413_added_transaction_direction_index/migration.sql
@@ -1,4 +1,2 @@
 -- CreateIndex
 CREATE INDEX "TransactionRecord_transactionDirection_idx" ON "TransactionRecord"("transactionDirection");
-
-CREATE INDEX "TransactionRecord_transactionDate_desc_idx" ON "TransactionRecord"("transactionDate" DESC);
