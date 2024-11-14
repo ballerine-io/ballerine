6808153e9c896e2e69aa8972c12c3acc1560a1df
Tomer Shvadron <tomers@ballerine.com>
2024-11-14 23:25:21 +0200

    feat: fix existing index issue

--- services/workflows-service/prisma/migrations/20241114130413_added_transaction_direction_index/migration.sql
+++ services/workflows-service/prisma/migrations/20241114130413_added_transaction_direction_index/migration.sql
@@ -1,2 +1,4 @@
+DROP INDEX "TransactionRecord_transactionDirection_idx" ON "TransactionRecord"("transactionDirection");
+
 -- CreateIndex
 CREATE INDEX "TransactionRecord_transactionDirection_idx" ON "TransactionRecord"("transactionDirection");
