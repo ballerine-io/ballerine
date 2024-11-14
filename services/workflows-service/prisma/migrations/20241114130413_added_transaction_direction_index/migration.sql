a2e4a5139ef9953a4a9887c9e7f3368a38d0aead
Tomer Shvadron <tomers@ballerine.com>
2024-11-15 00:57:54 +0200

    feat: fix bad db migration sql

--- services/workflows-service/prisma/migrations/20241114130413_added_transaction_direction_index/migration.sql
+++ services/workflows-service/prisma/migrations/20241114130413_added_transaction_direction_index/migration.sql
@@ -1,4 +1,4 @@
-DROP INDEX "TransactionRecord_transactionDirection_idx";
+DROP INDEX IF EXISTS "TransactionRecord_transactionDirection_idx";
 
 -- CreateIndex
 CREATE INDEX "TransactionRecord_transactionDirection_idx" ON "TransactionRecord"("transactionDirection");
