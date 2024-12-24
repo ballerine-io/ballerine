b498bc3015b0d18ad2690f9147e5041b83105052
Tomer Shvadron <tomers@ballerine.com>
2024-12-24 16:56:41 +0200

    fix(business-reports): simplify query enabled condition
    - Remove unnecessary string check for reportType
    - Simplify boolean conditions for enabling query
    
    (your code had more checks than a paranoid mother-in-law)

--- apps/backoffice-v2/src/domains/business-reports/hooks/queries/useBusinessReportsQuery/useBusinessReportsQuery.tsx
+++ apps/backoffice-v2/src/domains/business-reports/hooks/queries/useBusinessReportsQuery/useBusinessReportsQuery.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { isString } from '@/common/utils/is-string/is-string';
 import { MerchantReportType } from '@/domains/business-reports/constants';
 import { businessReportsQueryKey } from '@/domains/business-reports/query-keys';
 import { TReportStatusValue, TRiskLevel } from '@/pages/MerchantMonitoring/schemas';
@@ -47,14 +46,7 @@ export const useBusinessReportsQuery = ({
       from,
       to,
     }),
-    enabled:
-      isAuthenticated &&
-      isString(reportType) &&
-      !!reportType &&
-      !!sortBy &&
-      !!sortDir &&
-      !!page &&
-      !!pageSize,
+    enabled: isAuthenticated && !!sortBy && !!sortDir && !!page && !!pageSize,
     staleTime: 100_000,
     refetchInterval: 1_000_000,
   });
