8653622f15e970294ce17e224cc570514be7bf09
Illia Rudniev <cheskmr@gmail.com>
2025-01-30 12:18:26 +0200

    fix: added global types

--- apps/kyb-app/src/App.tsx
+++ apps/kyb-app/src/App.tsx
@@ -5,8 +5,16 @@ import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { version } from '../package.json';
 
-//@ts-ignore
 window.appVersion = version;
+
+window.toggleDevmode = () => {
+  const key = 'devmode';
+  const isDebug = localStorage.getItem(key);
+
+  isDebug ? localStorage.removeItem(key) : localStorage.setItem(key, 'true');
+
+  location.reload();
+};
 
 export const App = () => {
   return (
@@ -15,13 +23,4 @@ export const App = () => {
       <Toaster />
     </Sentry.ErrorBoundary>
   );
-};
-
-(window as any).toggleDevmode = () => {
-  const key = 'devmode';
-  const isDebug = localStorage.getItem(key);
-
-  isDebug ? localStorage.removeItem(key) : localStorage.setItem(key, 'true');
-
-  location.reload();
 };
