c40926dae9d5ca49d06705d33dfbedfe037cdc40
Shane <66246046+shanegrouber@users.noreply.github.com>
2025-02-12 11:00:22 +0200

    refactor(CaseOverview): simplify tab generation logic
    - Remove unnecessary getTab function
    - Use camelCase on the domain string directly

--- apps/backoffice-v2/src/pages/Entity/components/Case/components/CaseOverview/CaseOverview.tsx
+++ apps/backoffice-v2/src/pages/Entity/components/Case/components/CaseOverview/CaseOverview.tsx
@@ -28,14 +28,7 @@ export const CaseOverview = ({ processes }: { processes: string[] }) => {
       workflow?.context?.pluginsOutput?.risk_evaluation?.riskIndicatorsByDomain ??
       {},
   )?.map(([domain, riskIndicators]) => {
-    const getTab = (domain: string) => {
-      if (domain.toLowerCase() === 'ubos') {
-        return 'ubos';
-      }
-
-      return camelCase(domain);
-    };
-    const tab = getTab(domain);
+    const tab = camelCase(domain.toLowerCase());
     const isValidCaseTab = CaseTabs.includes(tab);
 
     return {
