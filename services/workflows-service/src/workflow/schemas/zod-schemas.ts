42c25bfeefe45a7eed31b444a74128cad59e32ac
ArtemLavrentii <lavrentiyartem@gmail.com>
2025-02-13 16:14:30 +0200

    chore: pr review fixes

--- services/workflows-service/src/workflow/schemas/zod-schemas.ts
+++ services/workflows-service/src/workflow/schemas/zod-schemas.ts
@@ -63,7 +63,7 @@ export const ConfigSchema = z
     maxBusinessReports: z.number().nonnegative().optional(),
     isMerchantMonitoringEnabled: z.boolean().optional(),
     showFullAccessPopup: z.boolean().optional(),
-    demoAccount: z.boolean().optional(),
+    isDemoAccount: z.boolean().optional(),
     uiOptions: z
       .object({
         redirectUrls: z
