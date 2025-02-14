7ca7cf6684b8ed5f1a7cb6367a5c12e3212b007e
Lior Zamir <liorz@ballerine.com>
2025-02-15 00:39:03 +0200

    fix: input data validation maximum size to 255

--- packages/common/src/schemas/documents/schemas/entity-schema.ts
+++ packages/common/src/schemas/documents/schemas/entity-schema.ts
@@ -22,7 +22,7 @@ export const IndividualDataSchema = Type.Object({
 export const BusinessDataSchema = Type.Object({
   correlationId: Type.Optional(Type.Union([Type.String(), Type.Null()])),
   businessType: Type.Optional(Type.Union([Type.String(), Type.Null()])),
-  companyName: Type.String(),
+  companyName: Type.String({ maxLength: 255 }),
   registrationNumber: Type.Optional(Type.Union([Type.String(), Type.Null()])),
   legalForm: Type.Optional(Type.Union([Type.String(), Type.Null()])),
   country: Type.Optional(
