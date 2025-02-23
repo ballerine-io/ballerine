b77375219b593b18750f233bb67ab02389c8dc6d
Sasha <sasham@ballerine.com>
2025-02-24 00:02:35 +0200

    welcome modal (#3066)

---
+++ apps/backoffice-v2/src/common/components/molecules/WelcomeModal/WelcomeModal.tsx
@@ -0,0 +1,73 @@
+import { DialogClose } from '@radix-ui/react-dialog';
+import { CircleCheck } from 'lucide-react';
+
+import { Button } from '@/common/components/atoms/Button/Button';
+import { Separator } from '@/common/components/atoms/Separator/Separator';
+import { Dialog } from '@/common/components/organisms/Dialog/Dialog';
+import { DialogContent } from '@/common/components/organisms/Dialog/Dialog.Content';
+import { DialogDescription } from '@/common/components/organisms/Dialog/Dialog.Description';
+import { DialogFooter } from '@/common/components/organisms/Dialog/Dialog.Footer';
+import { DialogHeader } from '@/common/components/organisms/Dialog/Dialog.Header';
+import { DialogTitle } from '@/common/components/organisms/Dialog/Dialog.Title';
+import { useToggle } from '@/common/hooks/useToggle/useToggle';
+import { BusinessReportsLeftCard } from '@/domains/business-reports/components/BusinessReportsLeftCard/BusinessReportsLeftCard';
+import { useCustomerQuery } from '@/domains/customer/hooks/queries/useCustomerQuery/useCustomerQuery';
+
+const benefits = [
+  'Spot potential risks and violations',
+  'Assess legitimacy and card scheme compliance',
+  'Gain actionable insights instantly',
+];
+
+export const WelcomeModal = () => {
+  const [open, toggleOpen] = useToggle(true);
+  const { data: customer, isLoading: isLoadingCustomer } = useCustomerQuery();
+
+  if (isLoadingCustomer || customer?.config?.demoAccessDetails?.seenWelcomeModal !== false) {
+    return null;
+  }
+
+  const { reportsLeft, demoDaysLeft } = customer?.config?.demoAccessDetails ?? {};
+
+  return (
+    <Dialog open={open} onOpenChange={toggleOpen}>
+      <DialogContent className="px-0 sm:max-w-xl">
+        <DialogHeader className="items-center px-6">
+          <DialogTitle className={`text-2xl`}>Welcome to Ballerine</DialogTitle>
+          <DialogDescription className={`text-md`}>
+            Welcome to Ballerine’s Web Presence Free Trial! 🚀
+          </DialogDescription>
+        </DialogHeader>
+
+        {/* Video element */}
+
+        <BusinessReportsLeftCard
+          reportsLeft={reportsLeft}
+          demoDaysLeft={demoDaysLeft}
+          className="mx-6"
+        />
+
+        <Separator />
+
+        <div className="space-y-4 px-6">
+          <p className="font-bold">Analyze any merchant&apos;s online presence</p>
+
+          {benefits.map(benefit => (
+            <div key={benefit} className="flex items-center gap-2">
+              <CircleCheck className="text-success d-5" />
+              <p>{benefit}</p>
+            </div>
+          ))}
+        </div>
+
+        <Separator />
+
+        <DialogFooter className="px-6">
+          <DialogClose asChild>
+            <Button className="text-md rounded-lg font-bold">Get Started</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
