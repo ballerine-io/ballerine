256f72d61cb684310a4664b00ac35a26a818befb
ArtemLavrentii <lavrentiyartem@gmail.com>
2025-02-26 15:13:32 +0200

    feat(bal-3619): added endpoint to create demo workflow

---
+++ services/workflows-service/src/workflow-defintion/workflow-definition.controller.internal.ts
@@ -0,0 +1,19 @@
+import * as common from '@nestjs/common';
+import * as swagger from '@nestjs/swagger';
+import { ApiExcludeController } from '@nestjs/swagger';
+import * as errors from '../errors';
+import { WorkflowDefinitionService } from '@/workflow-defintion/workflow-definition.service';
+import { CreateDemoWorkflowDefinitionDto } from '@/workflow-defintion/dtos/create-demo-workflow-definition-dto';
+
+@ApiExcludeController()
+@common.Controller('internal/workflow-definition')
+export class WorkflowControllerInternal {
+  constructor(protected readonly service: WorkflowDefinitionService) {}
+
+  @common.Post('/create-demo')
+  @swagger.ApiOkResponse()
+  @swagger.ApiForbiddenResponse({ type: errors.ForbiddenException })
+  async createDemoWorkflowDefinition(@common.Body() data: CreateDemoWorkflowDefinitionDto) {
+    return await this.service.createDemoWorkflowDefinition(data);
+  }
+}
