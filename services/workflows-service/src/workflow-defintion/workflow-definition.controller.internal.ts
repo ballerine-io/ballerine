f7681191c4185968fe3d16128f81419f0715e7b9
ArtemLavrentii <lavrentiyartem@gmail.com>
2025-02-27 14:43:09 +0200

    chore: added admin authentication

--- services/workflows-service/src/workflow-defintion/workflow-definition.controller.internal.ts
+++ services/workflows-service/src/workflow-defintion/workflow-definition.controller.internal.ts
@@ -4,6 +4,7 @@ import { ApiExcludeController } from '@nestjs/swagger';
 import * as errors from '../errors';
 import { WorkflowDefinitionService } from '@/workflow-defintion/workflow-definition.service';
 import { CreateDemoWorkflowDefinitionDto } from '@/workflow-defintion/dtos/create-demo-workflow-definition-dto';
+import { AdminAuthGuard } from '@/common/guards/admin-auth.guard';
 
 @ApiExcludeController()
 @common.Controller('internal/workflow-definition')
@@ -12,6 +13,7 @@ export class WorkflowControllerInternal {
 
   @common.Post('/create-demo')
   @swagger.ApiOkResponse()
+  @common.UseGuards(AdminAuthGuard)
   @swagger.ApiForbiddenResponse({ type: errors.ForbiddenException })
   async createDemoWorkflowDefinition(@common.Body() data: CreateDemoWorkflowDefinitionDto) {
     return await this.service.createDemoWorkflowDefinition(data);
