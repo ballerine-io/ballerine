731253c17eb7b4c7dda5c621adfcd49edfccf701
Tomer Shvadron <tomers@ballerine.com>
2025-03-02 12:41:34 +0200

    feat(case-creation): enhance button functionality with tooltip support
    - Update button state management with demo flag
    - Integrate tooltips to inform users on demo account limitations
    - Refactor withCaseCreation to use arrow function syntax for consistency

--- apps/backoffice-v2/src/pages/Entity/components/Case/components/CaseOptions/hooks/useCaseOptionsLogic/useCaseOptionsLogic.tsx
+++ apps/backoffice-v2/src/pages/Entity/components/Case/components/CaseOptions/hooks/useCaseOptionsLogic/useCaseOptionsLogic.tsx
@@ -22,5 +22,6 @@ export const useCaseOptionsLogic = () => {
     generateAndOpenPDFInNewTab,
     isCopyingCollectionFlowLink,
     copyCollectionFlowLink,
+    isDemo: customer?.config?.isDemo,
   };
 };
