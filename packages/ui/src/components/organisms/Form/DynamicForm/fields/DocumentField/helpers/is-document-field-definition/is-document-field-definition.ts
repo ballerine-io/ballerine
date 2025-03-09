888deefcb9368582d4840b4010e7ea2ea3b1ad25
Omri Levy <omri.levy0197@gmail.com>
2025-03-09 12:00:10 +0200

    fix(*): fixed wrong type-guard

--- packages/ui/src/components/organisms/Form/DynamicForm/fields/DocumentField/helpers/is-document-field-definition/is-document-field-definition.ts
+++ packages/ui/src/components/organisms/Form/DynamicForm/fields/DocumentField/helpers/is-document-field-definition/is-document-field-definition.ts
@@ -1,8 +1,8 @@
 import { IDocumentFieldParams } from '../../..';
-import { IFormElement, TBaseFields } from '../../../../types';
+import { IFormElement } from '../../../../types';
 
 export const isDocumentFieldDefinition = (
   element: IFormElement<any, any>,
-): element is IFormElement<TBaseFields, IDocumentFieldParams> => {
+): element is IFormElement<'documentfield', IDocumentFieldParams> => {
   return element.element === 'documentfield';
 };
