95a133322901719e3cfa9214f9a9bdf0ece386f3
Illia Rudniev <cheskmr@gmail.com>
2025-03-11 15:10:21 +0200

    feat: added minimumAge validator

---
+++ packages/ui/src/components/organisms/Form/Validator/validators/minimum-age/minimum-age-value-validator.ts
@@ -0,0 +1,41 @@
+import dayjs from 'dayjs';
+import { TValidator } from '../../types';
+import { formatErrorMessage } from '../../utils/format-error-message/format-error-message';
+import { IMinimumAgeValidatorParams } from './types';
+
+export const minimumAgeValueValidator: TValidator<string, IMinimumAgeValidatorParams> = (
+  value,
+  params,
+) => {
+  const { message = 'Minimum age is {minimumAge}.' } = params;
+  const isValid = dayjs(value).isValid();
+  console.log('isValid', isValid);
+
+  if (!isValid) {
+    throw new Error('Invalid date.');
+  }
+
+  // Default to 18 if not specified
+  const requiredAge = params?.value?.minimumAge;
+
+  if (!requiredAge) {
+    throw new Error('Minimum age is not specified.');
+  }
+
+  const today = dayjs();
+  const birthDate = dayjs(value);
+
+  let age = today.year() - birthDate.year();
+  const monthDiff = today.month() - birthDate.month();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.date() < birthDate.date())) {
+    age--;
+  }
+
+  console.log('age', age);
+  console.log('requiredAge', requiredAge);
+
+  if (age < requiredAge) {
+    throw new Error(formatErrorMessage(message, 'minimumAge', requiredAge.toString()));
+  }
+};
