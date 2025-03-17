132ce6c76b6faf2032bec8b521a2e2798f7bb438
Illia Rudniev <cheskmr@gmail.com>
2025-03-17 15:02:30 +0200

    feat: __id and __isGeneratedAutomatically is now being revoved after creation

--- packages/ui/src/components/organisms/Form/DynamicForm/fields/EntityFieldGroup/components/EntityFields/helpers/update-entities.ts
+++ packages/ui/src/components/organisms/Form/DynamicForm/fields/EntityFieldGroup/components/EntityFields/helpers/update-entities.ts
@@ -2,6 +2,9 @@ import { IEntity } from '../../../types';
 
 export const updateEntities = (entitiesList: IEntity[], createdEntityIds: string[]) => {
   return entitiesList.map((entity, index) => {
+    delete entity.__id;
+    delete entity.__isGeneratedAutomatically;
+
     return {
       ...entity,
       ballerineEntityId: createdEntityIds[index],
