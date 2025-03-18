9dc5181bb4b8ace28a8ba9eab8e74ffda9b195b3
Illia Rudniev <cheskmr@gmail.com>
2025-03-18 15:42:03 +0200

    chore: refactor

--- packages/ui/src/components/organisms/Form/DynamicForm/fields/EntityFieldGroup/components/EntityFields/helpers/update-entities.ts
+++ packages/ui/src/components/organisms/Form/DynamicForm/fields/EntityFieldGroup/components/EntityFields/helpers/update-entities.ts
@@ -1,10 +1,7 @@
 import { IEntity } from '../../../types';
 
 export const updateEntities = (entitiesList: IEntity[], createdEntityIds: string[]) => {
-  return entitiesList.map((entity, index) => {
-    delete entity.__id;
-    delete entity.__isGeneratedAutomatically;
-
+  return entitiesList.map(({ __id, __isGeneratedAutomatically, ...entity }, index) => {
     return {
       ...entity,
       ballerineEntityId: createdEntityIds[index],
