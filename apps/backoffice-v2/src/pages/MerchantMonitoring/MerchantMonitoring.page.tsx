4785163d51e2102ee18ac1ddf4b3b9c7ffc29d46
jonathan-shemer <jonathan-shemer@users.noreply.github.com>
2025-03-19 15:07:22 +0200

    feat(BAL-3438): tooltip text

--- apps/backoffice-v2/src/pages/MerchantMonitoring/MerchantMonitoring.page.tsx
+++ apps/backoffice-v2/src/pages/MerchantMonitoring/MerchantMonitoring.page.tsx
@@ -291,7 +291,7 @@ export const MerchantMonitoring: FunctionComponent = () => {
                 </Button>
               </TooltipTrigger>
               <TooltipContent side={'bottom'} align={'end'}>
-                <span>Export reports to CSV (filtered)</span>
+                <span>Export reports to a CSV file (filters applied)</span>
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
