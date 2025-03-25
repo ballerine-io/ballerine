cd9709be52480c6ac670d0e3ca4eb4ace898f79d
Alon Peretz <8467965+alonp99@users.noreply.github.com>
2025-03-26 01:20:50 +0200

    test(workflow): add WorkflowLogService to various integration tests
    - Import WorkflowLogService in integration test files for consistency
    - Include WorkflowLogService in the providers for dependency injection in tests
    - Ensure WorkflowLogService is referenced in all relevant test setups

--- services/workflows-service/src/workflow/workflow.controller.internal.intg.test.ts
+++ services/workflows-service/src/workflow/workflow.controller.internal.intg.test.ts
@@ -41,7 +41,7 @@ import { RiskRuleService } from '@/rule-engine/risk-rule.service';
 import { SentryService } from '@/sentry/sentry.service';
 import { SecretsManagerFactory } from '@/secrets-manager/secrets-manager.factory';
 import { MerchantMonitoringClient } from '@/merchant-monitoring/merchant-monitoring.client';
-
+import { WorkflowLogService } from '@/workflow/workflow-log.service';
 describe('/api/v1/internal/workflows #api #integration', () => {
   let app: INestApplication;
   let workflowService: WorkflowService;
@@ -92,6 +92,7 @@ describe('/api/v1/internal/workflows #api #integration', () => {
       SentryService,
       SecretsManagerFactory,
       MerchantMonitoringClient,
+      WorkflowLogService,
     ];
     workflowService = (await fetchServiceFromModule(WorkflowService, servicesProviders, [
       PrismaModule,
