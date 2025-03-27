ffcf2226645c4a1532dbaa906b305e8b9c44b4e0
Illia Rudniev <cheskmr@gmail.com>
2025-03-27 13:26:45 +0200

    fix: fixed output format serialization on date picker & fixed styles of multiselect

--- packages/ui/src/components/organisms/Form/DynamicForm/fields/MultiselectField/MultiselectFieldSelectedItem.tsx
+++ packages/ui/src/components/organisms/Form/DynamicForm/fields/MultiselectField/MultiselectFieldSelectedItem.tsx
@@ -12,11 +12,11 @@ export const MultiselectfieldSelectedItem: FunctionComponent<
   IMultiselectfieldSelectedItemProps
 > = ({ option, params }) => {
   return (
-    <Chip key={option.value} className="h-6">
-      <Chip.Label text={option.title} variant="secondary" />
+    <Chip key={option.value} className="h-6 bg-[#0F172A]">
+      <Chip.Label text={option.title} className="text-white text-sm" />
       <Chip.UnselectButton
         {...params.unselectButtonProps}
-        icon={<X className="hover:text-muted-foreground h-3 w-3 text-white" />}
+        icon={<X className="hover:text-white/80 h-3 w-3 text-white" />}
       />
     </Chip>
   );
