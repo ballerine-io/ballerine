1f4b381ccdfc2972a2f86831b382aa266f0daf20
Illia Rudniev <cheskmr@gmail.com>
2025-04-04 17:18:14 +0300

    feat: removed reduntant state mapping

--- apps/kyb-app/src/pages/CollectionFlow/versions/v2/CollectionFlowV2.tsx
+++ apps/kyb-app/src/pages/CollectionFlow/versions/v2/CollectionFlowV2.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { StepperProgress } from '@/common/components/atoms/StepperProgress';
@@ -15,11 +15,7 @@ import { useFlowContextQuery } from '@/hooks/useFlowContextQuery';
 import { useLanguageParam } from '@/hooks/useLanguageParam/useLanguageParam';
 import { withSessionProtected } from '@/hooks/useSessionQuery/hocs/withSessionProtected';
 import { useUISchemasQuery } from '@/hooks/useUISchemasQuery';
-import {
-  CollectionFlowStatusesEnum,
-  CollectionFlowStepStatesEnum,
-  getCollectionFlowState,
-} from '@ballerine/common';
+import { CollectionFlowStatusesEnum, getCollectionFlowState } from '@ballerine/common';
 import { LoadingScreen } from '../v1/components/atoms/LoadingScreen';
 import { Approved } from '../v1/components/pages/Approved';
 import { CompletedScreen } from '../v1/components/pages/CompletedScreen';
@@ -49,27 +45,6 @@ export const CollectionFlowV2 = withSessionProtected(() => {
   const elements = schema?.uiSchema?.elements as unknown as Array<UIPage<'v2'>>;
   const definition = schema?.definition.definition;
 
-  const isRevision = useMemo(
-    () =>
-      getCollectionFlowState(collectionFlowData?.context)?.status ===
-      CollectionFlowStatusesEnum.revision,
-    [collectionFlowData],
-  );
-
-  const initialContext: CollectionFlowContext = useMemo(() => {
-    const contextCopy = { ...collectionFlowContext };
-    const collectionFlow = getCollectionFlowState(contextCopy);
-    const firstRevisionStep = collectionFlow?.steps?.find(
-      step => step.state === CollectionFlowStepStatesEnum.revision,
-    );
-
-    if (isRevision && collectionFlow) {
-      collectionFlow.currentStep = firstRevisionStep?.stepName || collectionFlow.currentStep;
-    }
-
-    return contextCopy as CollectionFlowContext;
-  }, [isRevision, collectionFlowContext]);
-
   // Breadcrumbs now using scrollIntoView method to make sure that breadcrumb is always in viewport.
   // Due to dynamic dimensions of logo it doesnt work well if scroll happens before logo is loaded.
   // This workaround is needed to wait for logo to be loaded so scrollIntoView will work with correct dimensions of page.
@@ -84,26 +59,38 @@ export const CollectionFlowV2 = withSessionProtected(() => {
     setLogoLoaded(false);
   }, [customer?.logoImageUri]);
 
-  if (getCollectionFlowState(initialContext)?.status === CollectionFlowStatusesEnum.approved) {
+  if (
+    getCollectionFlowState(collectionFlowData?.context)?.status ===
+    CollectionFlowStatusesEnum.approved
+  ) {
     return <Approved />;
   }
 
-  if (getCollectionFlowState(initialContext)?.status === CollectionFlowStatusesEnum.rejected) {
+  if (
+    getCollectionFlowState(collectionFlowData?.context)?.status ===
+    CollectionFlowStatusesEnum.rejected
+  ) {
     return <Rejected />;
   }
 
-  if (getCollectionFlowState(initialContext)?.status === CollectionFlowStatusesEnum.completed) {
+  if (
+    getCollectionFlowState(collectionFlowData?.context)?.status ===
+    CollectionFlowStatusesEnum.completed
+  ) {
     return <CompletedScreen />;
   }
 
-  if (getCollectionFlowState(initialContext)?.status === CollectionFlowStatusesEnum.failed) {
+  if (
+    getCollectionFlowState(collectionFlowData?.context)?.status ===
+    CollectionFlowStatusesEnum.failed
+  ) {
     return <FailedScreen />;
   }
 
   return definition && collectionFlowContext ? (
     <DynamicUI>
       <DynamicUI.StateManager
-        initialContext={initialContext}
+        initialContext={collectionFlowData?.context as CollectionFlowContext}
         workflowId="1"
         definitionType={schema?.definition.definitionType}
         extensions={schema?.definition.extensions}
