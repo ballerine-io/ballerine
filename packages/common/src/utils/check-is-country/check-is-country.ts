aa1396dc901f63440b5ec82820b73eec4e654b33
Omri Levy <omri.levy0197@gmail.com>
2025-04-06 14:30:39 +0300

    refactor(*): updated check country util

--- packages/common/src/utils/check-is-country/check-is-country.ts
+++ packages/common/src/utils/check-is-country/check-is-country.ts
@@ -1,5 +1,5 @@
 import { getCountries } from '../get-countries';
 
 export const checkIsCountry = (locale: string, value: string) => {
-  return getCountries(locale).some((country) => country.const === value);
+  return getCountries(locale).some(country => country.const === value);
 };
