e1406e39cea540f4419991fa5cb105addb1edfa8
r4zendev <reactionnnnn@gmail.com>
2025-04-07 14:34:20 +0200

    pr comment

--- apps/backoffice-v2/src/domains/metrics/hooks/queries/useCaseDailyStats/useCaseDailyStats.tsx
+++ apps/backoffice-v2/src/domains/metrics/hooks/queries/useCaseDailyStats/useCaseDailyStats.tsx
@@ -3,15 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useIsAuthenticated } from '@/domains/auth/context/AuthProvider/hooks/useIsAuthenticated/useIsAuthenticated';
 import { metricsQueryKeys } from '@/domains/metrics/query-keys';
 
-export const useCaseDailyStats = (params: { from?: string; to?: string }) => {
+export const useCaseDailyStats = ({ from, to }: { from?: string; to?: string } = {}) => {
   const isAuthenticated = useIsAuthenticated();
-  const { from, to } = params;
 
   return useQuery({
-    ...metricsQueryKeys.caseDailyStats({
-      from: from ?? '',
-      to: to ?? '',
-    }),
+    ...metricsQueryKeys.caseDailyStats({ from: from!, to: to! }),
     enabled: !!from && !!to && isAuthenticated,
     staleTime: 100_000,
   });
