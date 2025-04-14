62d6670ebe30fcd5e555161f074b16bb7766ef8d
r4zendev <reactionnnnn@gmail.com>
2025-04-14 09:50:20 +0200

    remove redundant imports

--- apps/backoffice-v2/src/router.tsx
+++ apps/backoffice-v2/src/router.tsx
@@ -23,12 +23,10 @@ import { RootError } from '@/pages/Root/Root.error';
 import { rootLoader } from '@/pages/Root/Root.loader';
 import { Root } from '@/pages/Root/Root.page';
 import { SignIn } from '@/pages/SignIn/SignIn.page';
-import { Statistics } from '@/pages/Statistics/Statistics.page';
 import { TransactionMonitoring } from '@/pages/TransactionMonitoring/TransactionMonitoring';
 import { TransactionMonitoringAlerts } from '@/pages/TransactionMonitoringAlerts/TransactionMonitoringAlerts.page';
 import { TransactionMonitoringAlertsAnalysisPage } from '@/pages/TransactionMonitoringAlertsAnalysis/TransactionMonitoringAlertsAnalysis.page';
-import { Workflows } from '@/pages/Workflows/Workflows.page';
-import { FunctionComponent } from 'react';
+import type { FunctionComponent } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
