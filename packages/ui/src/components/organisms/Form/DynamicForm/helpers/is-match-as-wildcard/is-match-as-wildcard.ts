4d1289a2c376a837f77c7743613a403301be5727
Illia Rudniev <cheskmr@gmail.com>
2025-04-20 12:28:21 +0300

    chore:  refactor

--- packages/ui/src/components/organisms/Form/DynamicForm/helpers/is-match-as-wildcard/is-match-as-wildcard.ts
+++ packages/ui/src/components/organisms/Form/DynamicForm/helpers/is-match-as-wildcard/is-match-as-wildcard.ts
@@ -1,10 +1,6 @@
 import { formatWildcardId } from '@/components/organisms/Form/DynamicForm/helpers/format-wildcard-id/format-wildcard-id';
 
 export const isMatchAsWildcard = (id: string, priorityFieldId: string) => {
-  if (!priorityFieldId.endsWith('-*')) {
-    return false;
-  }
-
   const wildcardElementId = formatWildcardId(id);
 
   return priorityFieldId === wildcardElementId;
