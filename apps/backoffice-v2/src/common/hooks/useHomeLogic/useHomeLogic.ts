33ff0e19bc778ae22dc1a1eacb25ad67f4a3d125
r4zendev <reactionnnnn@gmail.com>
2025-04-23 14:29:12 +0200

    dont set default date for demo accounts

--- apps/backoffice-v2/src/common/hooks/useHomeLogic/useHomeLogic.ts
+++ apps/backoffice-v2/src/common/hooks/useHomeLogic/useHomeLogic.ts
@@ -69,12 +69,17 @@ export const useHomeLogic = () => {
 
     const toSet: Partial<z.infer<typeof HomeSearchSchema>> = {};
 
-    if (!mmFrom && !mmTo && isMerchantMonitoringEnabled) {
+    if (!mmFrom && !mmTo && isMerchantMonitoringEnabled && !customer?.config?.demoAccessDetails) {
       toSet.mmFrom = dayjs().subtract(1, 'month').format('YYYY-MM-DD');
       toSet.mmTo = dayjs().format('YYYY-MM-DD');
     }
 
-    if (!casesFrom && !casesTo && isCasesOnboardingEnabled) {
+    if (
+      !casesFrom &&
+      !casesTo &&
+      isCasesOnboardingEnabled &&
+      !customer?.config?.demoAccessDetails
+    ) {
       toSet.casesFrom = dayjs().subtract(1, 'month').format('YYYY-MM-DD');
       toSet.casesTo = dayjs().format('YYYY-MM-DD');
     }
