edd0b36db63ac3aa174e54e5e0cee5755af7686b
Illia Rudniev <cheskmr@gmail.com>
2025-04-25 13:13:40 +0300

    feat: added merchant monitoring report statuses to common

--- packages/common/src/consts/index.ts
+++ packages/common/src/consts/index.ts
@@ -179,8 +179,10 @@ export const MERCHANT_REPORT_STATUSES = [
   'quality-control',
   'pending-review',
   'under-review',
-  'completed',
   'failed',
+  'cleared',
+  'conditionally-approved',
+  'terminated',
 ] as const;
 
 export type MerchantReportStatus = (typeof MERCHANT_REPORT_STATUSES)[number];
@@ -190,14 +192,18 @@ export const MERCHANT_REPORT_STATUSES_MAP = Object.fromEntries(
 ) as { [K in MerchantReportStatus]: K };
 
 export type UpdateableReportStatus =
-  | (typeof MERCHANT_REPORT_STATUSES_MAP)['completed']
+  | (typeof MERCHANT_REPORT_STATUSES_MAP)['cleared']
+  | (typeof MERCHANT_REPORT_STATUSES_MAP)['conditionally-approved']
+  | (typeof MERCHANT_REPORT_STATUSES_MAP)['terminated']
   | (typeof MERCHANT_REPORT_STATUSES_MAP)['pending-review']
   | (typeof MERCHANT_REPORT_STATUSES_MAP)['under-review'];
 
 export const UPDATEABLE_REPORT_STATUSES = [
   MERCHANT_REPORT_STATUSES_MAP['pending-review'],
   MERCHANT_REPORT_STATUSES_MAP['under-review'],
-  MERCHANT_REPORT_STATUSES_MAP.completed,
+  MERCHANT_REPORT_STATUSES_MAP['cleared'],
+  MERCHANT_REPORT_STATUSES_MAP['conditionally-approved'],
+  MERCHANT_REPORT_STATUSES_MAP['terminated'],
 ] as const;
 
 export const MERCHANT_REPORT_TYPES = ['MERCHANT_REPORT_T1', 'ONGOING_MERCHANT_REPORT_T1'] as const;
