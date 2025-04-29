47c4a823c955f3bbd8aa486be2b6efd134828745
Illia Rudniev <cheskmr@gmail.com>
2025-04-29 12:05:02 +0300

    feat: added dom purify sanitization to notes

--- apps/backoffice-v2/src/pages/MerchantMonitoring/components/MerchantMonitoringReportStatus/helpers/get-note-content-for-unsubscribe.ts
+++ apps/backoffice-v2/src/pages/MerchantMonitoring/components/MerchantMonitoringReportStatus/helpers/get-note-content-for-unsubscribe.ts
@@ -1,7 +1,18 @@
-export const getNoteContentForUnsubscribe = (status: string, text: string) => `
+import DOMPurify from 'dompurify';
+
+export const getNoteContentForUnsubscribe = (status: string, text: string) => {
+  const sanitizedStatus = DOMPurify.sanitize(status);
+  const sanitizedText = DOMPurify.sanitize(text);
+
+  return `
   <div class="flex flex-col">
     <span class="text-xs leading-6 text-slate-500">
-      Status changed to <span class="font-semibold">"${status},"</span> and the merchant was <b>unsubscribed from ongoing monitoring</b>
-      ${text ? ` with details:</span><div class="text-sm">${text}</div>` : '</span>'}
+      Status changed to <span class="font-semibold">"${sanitizedStatus},"</span> and the merchant was <b>unsubscribed from ongoing monitoring</b>
+      ${
+        sanitizedText
+          ? ` with details:</span><div class="text-sm">${sanitizedText}</div>`
+          : '</span>'
+      }
   </div>
 `;
+};
