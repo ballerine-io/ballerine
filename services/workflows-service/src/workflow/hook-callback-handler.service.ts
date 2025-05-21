006b7a28dc41917a6847c3b203c32d226abb4784
Omri Levy <omri.levy0197@gmail.com>
2025-05-21 17:46:31 +0300

    fix(workflows-service): fixed type

--- services/workflows-service/src/workflow/hook-callback-handler.service.ts
+++ services/workflows-service/src/workflow/hook-callback-handler.service.ts
@@ -220,6 +220,7 @@ export class HookCallbackHandlerService {
     this.setNestedProperty(context, attributePath, result);
     // @ts-expect-error - we don't validate `context` is an object
     context.documents =
+      // @ts-expect-error - we don't validate `context` is an object
       context.documents?.filter(document => document.type !== 'identification_document') ?? [];
     // @ts-expect-error - we don't validate `context` is an object
     context.kycDocuments = persistedDocuments;
