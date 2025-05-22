cb50d90aa593eef5c41e72e772bd3edd67ab71e8
r4zendev <reactionnnnn@gmail.com>
2025-05-22 16:19:22 +0200

    fix: remove width/height constraints from process & document tracker blocks

--- apps/backoffice-v2/src/common/components/molecules/ProcessTracker/ProcessTracker.tsx
+++ apps/backoffice-v2/src/common/components/molecules/ProcessTracker/ProcessTracker.tsx
@@ -17,58 +17,56 @@ export const ProcessTracker: FunctionComponent<IProcessTrackerProps> = ({
   });
 
   return (
-    <div className={`max-w-xs`}>
-      <AccordionCard value={uncollapsedItemValue} onValueChange={onValueChange}>
-        <AccordionCard.Title
-          className={`flex-row items-center justify-between`}
-          rightChildren={
-            <HoverCard openDelay={0}>
-              <HoverCardTrigger className={`pb-1`}>
-                <HelpCircle size={18} className={`stroke-slate-400/70`} />
-              </HoverCardTrigger>
-              <HoverCardContent side={'top'} align={'start'}>
-                <ul className={`flex flex-col space-y-2`}>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.INDICATOR}
-                    Process not started
-                  </li>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.CLOCK}
-                    Process started
-                  </li>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.CHECK}
-                    Process complete
-                  </li>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.EDIT}
-                    Process being handled by Agent
-                  </li>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.MINUS}
-                    <span className={`text-slate-400/40 line-through`}>Process cancelled</span>
-                  </li>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.X}
-                    Process failed
-                  </li>
-                  <li className={`flex items-center gap-x-2`}>
-                    {Icon.REFRESH}
-                    Re-do process started
-                  </li>
-                </ul>
-              </HoverCardContent>
-            </HoverCard>
-          }
-        >
-          Processes
-        </AccordionCard.Title>
-        <AccordionCard.Content>
-          {trackedProcesses.map(({ name, Component }) => (
-            <Component key={name} workflow={workflow} plugins={plugins} processes={processes} />
-          ))}
-        </AccordionCard.Content>
-      </AccordionCard>
-    </div>
+    <AccordionCard value={uncollapsedItemValue} onValueChange={onValueChange}>
+      <AccordionCard.Title
+        className={`flex-row items-center justify-between`}
+        rightChildren={
+          <HoverCard openDelay={0}>
+            <HoverCardTrigger className={`pb-1`}>
+              <HelpCircle size={18} className={`stroke-slate-400/70`} />
+            </HoverCardTrigger>
+            <HoverCardContent side={'top'} align={'start'}>
+              <ul className={`flex flex-col space-y-2`}>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.INDICATOR}
+                  Process not started
+                </li>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.CLOCK}
+                  Process started
+                </li>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.CHECK}
+                  Process complete
+                </li>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.EDIT}
+                  Process being handled by Agent
+                </li>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.MINUS}
+                  <span className={`text-slate-400/40 line-through`}>Process cancelled</span>
+                </li>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.X}
+                  Process failed
+                </li>
+                <li className={`flex items-center gap-x-2`}>
+                  {Icon.REFRESH}
+                  Re-do process started
+                </li>
+              </ul>
+            </HoverCardContent>
+          </HoverCard>
+        }
+      >
+        Processes
+      </AccordionCard.Title>
+      <AccordionCard.Content>
+        {trackedProcesses.map(({ name, Component }) => (
+          <Component key={name} workflow={workflow} plugins={plugins} processes={processes} />
+        ))}
+      </AccordionCard.Content>
+    </AccordionCard>
   );
 };
