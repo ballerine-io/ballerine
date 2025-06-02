5e811c65a8f7758f5e8492c5caa07ac6051c35f5
Illia Rudniev <cheskmr@gmail.com>
2025-06-02 13:10:14 +0300

    chore: type fix

--- apps/kyb-app/src/hooks/useDocumentsQuery/useDocumentsQuery.ts
+++ apps/kyb-app/src/hooks/useDocumentsQuery/useDocumentsQuery.ts
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 export const useDocumentsQuery = () => {
   return useQuery({
     ...collectionFlowQuerykeys.getDocuments(),
+    //@ts-ignore
     staleTime: Infinity,
   });
 };
