db0a44942649aeda60f868d2b67848faa1030690
Tomer Shvadron <tomers@ballerine.com>
2025-06-11 14:32:24 +0300

    feat(ui): enhance SearchableDropdown and SelectField components
    - Improve accessibility by modifying state handling in SearchableDropdown
    - Refactor SelectField options mapping for clarity and update dependencies
    - Ensure SelectField does not disable by default and adds disablePortal attribute

--- packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/TextInputAdapter/components/SelectField/SelectField.tsx
+++ packages/ui/src/components/organisms/DynamicForm/components/RSJVInputAdaters/TextInputAdapter/components/SelectField/SelectField.tsx
@@ -16,15 +16,15 @@ export const SelectField = ({
 }: WithTestId<FieldProps<string>>) => {
   const options = useMemo((): ISearchableDropdownOption[] => {
     if (Array.isArray(schema.enum)) {
-      return schema.enum.map((value, index) => {
-        return {
-          label: schema.enumNames ? schema.enumNames[index] : value,
-          value: value as string,
-        };
-      });
+      return schema.enum.map((value, index) => ({
+        label: (uiSchema?.['ui:enumNames']?.[index] as string) ?? value,
+        value: value as string,
+      }));
     }
 
-    if (!Array.isArray(schema.oneOf)) return [];
+    if (!Array.isArray(schema.oneOf)) {
+      return [];
+    }
 
     return (schema.oneOf as TOneOfItem[]).map(item => {
       return {
@@ -32,7 +32,7 @@ export const SelectField = ({
         value: item.const as string,
       };
     }) as ISearchableDropdownOption[];
-  }, [schema.oneOf, schema.enumNames, schema.enum]);
+  }, [schema.enum, schema.oneOf, uiSchema]);
 
   const handleBlur = useCallback(() => {
     // @ts-ignore
@@ -46,8 +46,9 @@ export const SelectField = ({
       name={id}
       options={options}
       value={formData}
-      disabled={disabled}
+      disabled={false}
       testId={testId}
+      disablePortal
       textInputClassName="placeholder:text-gray-400"
       onChange={onChange}
       onBlur={handleBlur}
