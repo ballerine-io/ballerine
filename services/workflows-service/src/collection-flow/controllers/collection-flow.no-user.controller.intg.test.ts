345496b5f89c631040207ab56a6296c85df09bef
Omri Levy <omri.levy0197@gmail.com>
2025-06-11 14:27:54 +0300

    fixed test issues

--- services/workflows-service/src/collection-flow/controllers/collection-flow.no-user.controller.intg.test.ts
+++ services/workflows-service/src/collection-flow/controllers/collection-flow.no-user.controller.intg.test.ts
@@ -51,6 +51,7 @@ import { AssessmentsService } from '@/assessments/assessments.service';
 import { UnifiedApiClient } from '@/common/utils/unified-api-client/unified-api-client';
 import { KycService } from '@/kyc/kyc.service';
 import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 describe('CollectionFlowSignupController', () => {
   let app: INestApplication;
@@ -107,8 +108,7 @@ describe('CollectionFlowSignupController', () => {
         WorkflowRuntimeDataActorService,
         AssessmentsService,
         UnifiedApiClient,
-        KycService,
-        HttpService,
+        { provide: KycService, useValue: noop },
         mockClsService(),
         { provide: CollectionFlowStateService, useValue: noop },
       ],
