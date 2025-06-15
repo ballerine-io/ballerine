edd20b9fb21c5a8a28395813f3c1080bf85316cf
Tomer Shvadron <tomers@ballerine.com>
2025-06-15 13:25:40 +0300

    fix(workflows): update ongoing merchant report scheduling options
    - Change scheduling type from 'interval' to 'specific'
    - Add new fields for 'dayInMonth' and 'monthInterval'
    - Update relevant schemas and types for consistency

--- services/workflows-service/src/customer/types.ts
+++ services/workflows-service/src/customer/types.ts
@@ -21,18 +21,10 @@ export type TOngoingMerchantReportOptions = {
   proxyViaCountry: string;
   workflowVersion: MerchantReportVersion;
   reportType: 'ONGOING_MERCHANT_REPORT_T1';
-} & (
-  | {
-      scheduleType: 'specific';
-      specificDates: {
-        dayInMonth: number;
-      };
-    }
-  | {
-      scheduleType: 'interval';
-      intervalInDays: number;
-    }
-);
+  scheduleType: 'specific';
+  dayInMonth: number;
+  monthInterval: number;
+};
 
 type FeaturesOptions = TOngoingMerchantReportOptions;
 
@@ -48,8 +40,9 @@ export const CUSTOMER_FEATURES = {
     name: FEATURE_LIST.ONGOING_MERCHANT_REPORT,
     enabled: true,
     options: {
-      scheduleType: 'interval',
-      intervalInDays: 30,
+      scheduleType: 'specific',
+      dayInMonth: 1,
+      monthInterval: 1,
       runByDefault: true,
       workflowVersion: '2',
       proxyViaCountry: 'GB',
