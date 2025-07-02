bd78a0b806a56d251a604731842c6ed7a39b5646
Illia Rudniev <cheskmr@gmail.com>
2025-07-02 14:39:51 +0300

    fix: fixed type

--- apps/backoffice-v2/src/lib/blocks/components/MultiDocuments/interfaces.ts
+++ apps/backoffice-v2/src/lib/blocks/components/MultiDocuments/interfaces.ts
@@ -6,7 +6,7 @@ export interface IMultiDocumentsProps {
     isDocumentEditable: boolean;
     data: Array<{
       imageUrl: string;
-      base64: string;
+      base64?: string;
       title: string;
       fileType: string;
     }>;
