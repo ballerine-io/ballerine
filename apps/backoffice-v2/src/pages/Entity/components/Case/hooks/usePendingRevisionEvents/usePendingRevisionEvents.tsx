d659fe2814e12d4dd4d9945c0cce0a4a48f79256
Illia Rudniev <cheskmr@gmail.com>
2025-07-02 14:28:34 +0300

    feat: added toasts

--- apps/backoffice-v2/src/pages/Entity/components/Case/hooks/usePendingRevisionEvents/usePendingRevisionEvents.tsx
+++ apps/backoffice-v2/src/pages/Entity/components/Case/hooks/usePendingRevisionEvents/usePendingRevisionEvents.tsx
@@ -3,6 +3,8 @@ import { useCallback } from 'react';
 import { buildCollectionFlowUrl, CommonWorkflowEvent } from '@ballerine/common';
 import { checkIsKybExampleVariant } from '@/lib/blocks/variants/variant-checkers';
 import { useRevisionCaseMutation } from '@/domains/workflows/hooks/mutations/useRevisionCaseMutation/useRevisionCaseMutation';
+import { toast } from 'sonner';
+import { t } from 'i18next';
 
 export const usePendingRevisionEvents = ({
   mutateRevisionCase,
@@ -14,15 +16,31 @@ export const usePendingRevisionEvents = ({
   documentIds: string[];
 }) => {
   const onMutateRevisionCase = useCallback(() => {
-    if (!workflow?.nextEvents?.some(nextEvent => nextEvent === CommonWorkflowEvent.REVISION)) {
+    if (!workflow) {
+      console.error('Workflow not found.');
+      toast.error(t('toast:common.unexpected_error'));
       return;
     }
 
-    mutateRevisionCase({ workflowId: workflow?.id!, documentIds });
+    if (!workflow?.nextEvents?.some(nextEvent => nextEvent === CommonWorkflowEvent.REVISION)) {
+      console.error('Workflow does not have a revision event.');
+      toast.error(t('toast:common.unexpected_error'));
+      return;
+    }
+
+    if (!documentIds?.length) {
+      console.error('No document IDs provided.');
+      toast.error(t('toast:common.unexpected_error'));
+      return;
+    }
+
+    mutateRevisionCase({ workflowId: workflow.id, documentIds });
 
     const isKybExampleVariant = checkIsKybExampleVariant(workflow?.workflowDefinition);
 
     if (!isKybExampleVariant) {
+      console.error('Workflow is not a KYB example variant.');
+      toast.error(t('toast:common.unexpected_error'));
       return;
     }
 
